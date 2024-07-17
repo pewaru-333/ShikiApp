a776f08b9dd845a259885889772682861861318a
pav <pav@test.ru>
2024-07-17 16:43:59 +0300

    — Добавлены возможные нулевые поля в DTO

--- app/src/main/java/org/application/shikiapp/models/data/ExternalLink.kt
+++ app/src/main/java/org/application/shikiapp/models/data/ExternalLink.kt
@@ -5,13 +5,13 @@ import com.squareup.moshi.JsonClass
 
 @JsonClass(generateAdapter = true)
 data class ExternalLink(
-    @Json(name = "id") val id: Long,
+    @Json(name = "id") val id: Long?,
     @Json(name = "kind") val kind: String,
     @Json(name = "url") val url: String,
     @Json(name = "source") val source: String,
     @Json(name = "entry_id") val entryId: Long,
     @Json(name = "entry_type") val entryType: String,
-    @Json(name = "created_at") val createdAt: String,
-    @Json(name = "updated_at") val updatedAt: String,
-    @Json(name = "imported_at") val importedAt: String
+    @Json(name = "created_at") val createdAt: String?,
+    @Json(name = "updated_at") val updatedAt: String?,
+    @Json(name = "imported_at") val importedAt: String?
 )
