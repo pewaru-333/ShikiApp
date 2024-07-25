ff86f7625cddd7e52ec8ab5e7205dccd75b17e19
pav <pav@test.ru>
2024-07-25 13:26:30 +0300

    — Удалена библиотека AppStartup. Переопределён ImageLoader для библиотеки Coil.

---
+++ app/src/main/java/org/application/shikiapp/ShikiApp.kt
@@ -0,0 +1,37 @@
+package org.application.shikiapp
+
+import android.app.Application
+import coil.ImageLoader
+import coil.ImageLoaderFactory
+import coil.disk.DiskCache
+import coil.memory.MemoryCache
+import coil.request.CachePolicy
+import org.application.shikiapp.utils.Preferences
+
+class ShikiApp : Application(), ImageLoaderFactory {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        Preferences.getInstance(this)
+    }
+
+    override fun newImageLoader(): ImageLoader = ImageLoader(this).newBuilder()
+        .respectCacheHeaders(false)
+        .error(R.drawable.vector_bad)
+        .fallback(R.drawable.vector_bad)
+        .memoryCachePolicy(CachePolicy.ENABLED)
+        .memoryCache {
+            MemoryCache.Builder(this)
+                .maxSizePercent(0.2)
+                .strongReferencesEnabled(true)
+                .build()
+        }
+        .diskCachePolicy(CachePolicy.ENABLED)
+        .diskCache {
+            DiskCache.Builder()
+                .maxSizeBytes(Preferences.getCache() * 1048576L)
+                .directory(cacheDir)
+                .build()
+        }.build()
+}
