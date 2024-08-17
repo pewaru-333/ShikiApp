a662234dbb89c387a524af1c7d128b296b155bc1
pav <pav@test.ru>
2024-08-17 13:10:45 +0300

    — Добавлен номер и общее количество изображений при их просмотре

--- app/src/main/java/org/application/shikiapp/models/views/NewsDetailViewModel.kt
+++ app/src/main/java/org/application/shikiapp/models/views/NewsDetailViewModel.kt
@@ -4,16 +4,21 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 import org.application.shikiapp.models.data.News
 import org.application.shikiapp.models.views.NewsDetailViewModel.Response.Error
 import org.application.shikiapp.models.views.NewsDetailViewModel.Response.Loading
 import org.application.shikiapp.models.views.NewsDetailViewModel.Response.Success
 import org.application.shikiapp.network.NetworkClient
+import retrofit2.HttpException
 
 
 class NewsDetailViewModel(private val newsId: Long) : ViewModel() {
-    private val _state = MutableStateFlow<Response>(Loading)
+    private val _response = MutableStateFlow<Response>(Loading)
+    val response = _response.asStateFlow()
+
+    private val _state = MutableStateFlow(NewsDetailState())
     val state = _state.asStateFlow()
 
     init {
@@ -22,16 +27,34 @@ class NewsDetailViewModel(private val newsId: Long) : ViewModel() {
 
     fun getNews() {
         viewModelScope.launch {
-            _state.emit(Loading)
+            _response.emit(Loading)
 
             try {
-                val news = NetworkClient.client.getTopicById(newsId)
-
-                _state.emit(Success(news))
-            } catch (e: Exception) {
-                _state.emit(Error)
+                _response.emit(Success(NetworkClient.client.getTopicById(newsId)))
+            } catch (e: HttpException) {
+                _response.emit(Error)
             }
         }
+    }
+
+    fun showComments() {
+        viewModelScope.launch { _state.update { it.copy(showComments = true) } }
+    }
+
+    fun hideComments() {
+        viewModelScope.launch { _state.update { it.copy(showComments = false) } }
+    }
+
+    fun showImage(index: Int) {
+        viewModelScope.launch { _state.update { it.copy(showImage = true, image = index) } }
+    }
+
+    fun hideImage() {
+        viewModelScope.launch { _state.update { it.copy(showImage = false) } }
+    }
+
+    fun setImage(index: Int) {
+        viewModelScope.launch { _state.update { it.copy(image = index) } }
     }
 
     sealed interface Response {
@@ -40,3 +63,9 @@ class NewsDetailViewModel(private val newsId: Long) : ViewModel() {
         data class Success(val news: News) : Response
     }
 }
+
+data class NewsDetailState(
+    val showComments: Boolean = false,
+    val showImage: Boolean = false,
+    val image: Int = 0
+)
--- app/src/main/java/org/application/shikiapp/screens/NewsDetailScreen.kt
+++ app/src/main/java/org/application/shikiapp/screens/NewsDetailScreen.kt
@@ -1,41 +1,60 @@
 package org.application.shikiapp.screens
 
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.Arrangement.spacedBy
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.pager.HorizontalPager
+import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.ListItem
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.snapshotFlow
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.FilterQuality
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalUriHandler
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.compose.ui.window.Dialog
+import androidx.compose.ui.window.DialogProperties
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.paging.compose.collectAsLazyPagingItems
 import coil.compose.AsyncImage
 import com.ramcosta.composedestinations.annotation.Destination
 import com.ramcosta.composedestinations.annotation.RootGraph
+import com.ramcosta.composedestinations.generated.destinations.UserScreenDestination
 import com.ramcosta.composedestinations.navigation.DestinationsNavigator
+import kotlinx.coroutines.flow.collectLatest
 import org.application.shikiapp.R
+import org.application.shikiapp.R.drawable.vector_comments
+import org.application.shikiapp.R.string.text_image_of
 import org.application.shikiapp.models.views.CommentViewModel
+import org.application.shikiapp.models.views.NewsDetailState
 import org.application.shikiapp.models.views.NewsDetailViewModel
 import org.application.shikiapp.models.views.NewsDetailViewModel.Response.Error
 import org.application.shikiapp.models.views.NewsDetailViewModel.Response.Loading
@@ -49,23 +68,30 @@ import org.application.shikiapp.utils.getLinks
 @Composable
 fun NewsDetail(newsId: Long, navigator: DestinationsNavigator) {
     val model = viewModel<NewsDetailViewModel>(factory = factory { NewsDetailViewModel(newsId) })
-    val state by model.state.collectAsStateWithLifecycle()
+    val response by model.response.collectAsStateWithLifecycle()
 
-    when (val response = state) {
+    when (val data = response) {
         is Error -> ErrorScreen(model::getNews)
         is Loading -> LoadingScreen()
         is Success -> {
-            val comments = viewModel<CommentViewModel>(factory = factory {
-                CommentViewModel(newsId)
-            }).comments.collectAsLazyPagingItems()
-            val news = response.news
-            val poster = getLinks(news.htmlFooter).find { it.contains(".jpg") }
+            val state by model.state.collectAsStateWithLifecycle()
+            val comments = viewModel<CommentViewModel>(factory = factory { CommentViewModel(newsId) })
+                .comments.collectAsLazyPagingItems()
+            val news = data.news
+            val links = getLinks(news.htmlFooter)
+            val video = links.filter { it.contains("youtu") }
+            val images = links.filter { it.contains("original") }
 
             Scaffold(
                 topBar = {
                     TopAppBar(
                         title = { Text(stringResource(R.string.text_news_one)) },
-                        navigationIcon = { NavigationIcon(navigator::popBackStack) }
+                        navigationIcon = { NavigationIcon(navigator::popBackStack) },
+                        actions = {
+                            if (news.commentsCount > 0) IconButton(model::showComments) {
+                                Icon(painterResource(vector_comments), null)
+                            }
+                        }
                     )
                 }
             ) { values ->
@@ -75,7 +101,7 @@ fun NewsDetail(newsId: Long, navigator: DestinationsNavigator) {
                 ) {
                     item {
                         AsyncImage(
-                            model = poster,
+                            model = links.find { it.contains(".jpg") },
                             contentDescription = null,
                             modifier = Modifier
                                 .fillMaxWidth()
@@ -105,7 +131,8 @@ fun NewsDetail(newsId: Long, navigator: DestinationsNavigator) {
                                     contentDescription = null,
                                     modifier = Modifier
                                         .size(56.dp)
-                                        .clip(CircleShape),
+                                        .clip(CircleShape)
+                                        .clickable { navigator.navigate(UserScreenDestination(news.user.id)) },
                                     contentScale = ContentScale.Crop,
                                     filterQuality = FilterQuality.High
                                 )
@@ -113,44 +140,68 @@ fun NewsDetail(newsId: Long, navigator: DestinationsNavigator) {
                         )
                     }
 
-                    item {
-                        Text(
-                            text = fromHtml(news.htmlBody),
-                            style = MaterialTheme.typography.bodyLarge.copy(fontSize = 16.sp)
-                        )
-                    }
+                    item { Text(fromHtml(news.htmlBody), style = MaterialTheme.typography.bodyLarge) }
 
                     item {
-                        Row(Modifier.horizontalScroll(rememberScrollState()), spacedBy(16.dp)) {
-                            fromHtml(news.htmlFooter).getLinkAnnotations(0, news.htmlFooter.length)
-                                .forEach { link ->
-                                    val url = link.item.toString().substringAfterLast("=")
-                                        .substringBeforeLast(")")
-                                    if (!url.contains(".be"))
-//                                    AsyncImage(
-//                                        model = url,
-//                                        contentDescription = null,
-//                                        modifier = Modifier.size(160.dp, 75.dp),
-//                                        imageLoader = LocalContext.current.imageLoader.newBuilder()
-//                                            .components { add(VideoFrameDecoder.Factory()) }.build(),
-//                                        contentScale = ContentScale.Inside,
-//                                        fallback = painterResource(id = R.drawable.vector_home),
-//                                         error = painterResource(id = R.drawable.vector_home)
-//                                    )
-//                                else
-                                        AsyncImage(
-                                            model = url,
-                                            contentDescription = null,
-                                            modifier = Modifier.size(236.dp, 350.dp),
-                                            contentScale = ContentScale.FillBounds
-                                        )
-                                }
+                        Row(Modifier.horizontalScroll(rememberScrollState()), spacedBy(12.dp)) {
+                            if(video.isNotEmpty()) {
+                                val handler = LocalUriHandler.current
+                                AsyncImage(
+                                    model = video[1],
+                                    contentDescription = null,
+                                    modifier = Modifier
+                                        .size(172.dp, 130.dp)
+                                        .clip(MaterialTheme.shapes.small)
+                                        .clickable { handler.openUri(video[0]) }
+                                )
+                            }
+
+                            images.forEachIndexed { index, image ->
+                                AsyncImage(
+                                    model = image,
+                                    contentDescription = null,
+                                    modifier = Modifier
+                                        .size(172.dp, 130.dp)
+                                        .clip(MaterialTheme.shapes.small)
+                                        .clickable { model.showImage(index) }
+                                )
+                            }
                         }
                     }
-
-                    if (comments.itemCount > 0) comments(comments, navigator)
                 }
             }
+
+            when {
+                state.showComments -> Comments(model::hideComments, comments, navigator)
+                state.showImage -> DialogImage(model, state, images)
+            }
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+private fun DialogImage(model: NewsDetailViewModel, state: NewsDetailState, list: List<String>) {
+    val pagerState = rememberPagerState(state.image) { list.size }
+
+    LaunchedEffect(pagerState) {
+        snapshotFlow { pagerState.currentPage }.collectLatest(model::setImage)
+    }
+
+    Dialog(model::hideImage, DialogProperties(usePlatformDefaultWidth = false)) {
+        Scaffold(
+            topBar = {
+                TopAppBar(
+                    title = { Text(stringResource(text_image_of, state.image + 1, list.size)) },
+                    navigationIcon = { NavigationIcon(model::hideImage) }
+                )
+            }
+        ) { values ->
+            HorizontalPager(
+                state = pagerState,
+                modifier = Modifier.fillMaxSize(),
+                contentPadding = values
+            ) { Box(Modifier.fillMaxSize(), Alignment.Center) { AsyncImage(list[it], null) } }
         }
     }
 }
