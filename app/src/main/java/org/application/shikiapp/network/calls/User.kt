5a11bcfd1141f14dac355b778e613ee789ed2790
pav <pewaru@inbox.ru>
2024-09-02 16:29:46 +0300

    — Добавлены методы получения избранного и истории пользователя

--- app/src/main/java/org/application/shikiapp/network/calls/User.kt
+++ app/src/main/java/org/application/shikiapp/network/calls/User.kt
@@ -2,7 +2,10 @@ package org.application.shikiapp.network.calls
 
 import org.application.shikiapp.models.data.AnimeRate
 import org.application.shikiapp.models.data.Club
+import org.application.shikiapp.models.data.Favourites
+import org.application.shikiapp.models.data.HistoryAnime
 import org.application.shikiapp.models.data.User
+import org.application.shikiapp.models.data.UserShort
 import retrofit2.http.GET
 import retrofit2.http.Path
 import retrofit2.http.Query
@@ -27,7 +30,7 @@ interface User {
         @Path(value = "userId") userId: Long,
         @Query("page") page: Int = 1,
         @Query("limit") limit: Int = 5
-    ): List<User>
+    ): List<UserShort>
 
     @GET("users/{userId}/clubs")
     suspend fun getClubs(@Path(value = "userId") userId: Long): List<Club>
@@ -40,4 +43,15 @@ interface User {
         @Query("page") page: Int = 1,
         @Query("limit") limit: Int = 5000
     ): List<AnimeRate>
+
+    @GET("users/{userId}/favourites")
+    suspend fun getFavourites(@Path(value = "userId") userId: Long): Favourites
+
+    @GET("users/{userId}/history")
+    suspend fun getHistory(
+        @Path(value = "userId") userId: Long,
+        @Query("page") page: Int = 1,
+        @Query("limit") limit: Int = 20,
+        @Query("target_type") targetType: String = "Anime"
+    ): List<HistoryAnime>
 }
