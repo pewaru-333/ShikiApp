d3f41b9fa1d80de67375201c4a0356422290d260
pav <pewaru@inbox.ru>
2024-09-08 13:59:23 +0300

    — Изменение названия параметра. Изменение функции установки оценки.

--- app/src/main/java/org/application/shikiapp/network/paging/AnimePaging.kt
+++ app/src/main/java/org/application/shikiapp/network/paging/AnimePaging.kt
@@ -2,49 +2,44 @@ package org.application.shikiapp.network.paging
 
 import androidx.paging.PagingSource
 import androidx.paging.PagingState
-import org.application.AnimeListQuery
-import org.application.shikiapp.models.views.QueryMap
+import org.application.AnimeListQuery.Data.Anime
+import org.application.shikiapp.models.views.AnimeFilters
 import org.application.shikiapp.network.ApolloClient
-import org.application.shikiapp.utils.STATUSES
+import org.application.shikiapp.utils.setScore
 
-class AnimePaging(private val queryMap: QueryMap) : PagingSource<Int, AnimeListQuery.Anime>() {
+class AnimePaging(private val query: AnimeFilters) : PagingSource<Int, Anime>() {
 
-    override fun getRefreshKey(state: PagingState<Int, AnimeListQuery.Anime>): Int? =
+    override fun getRefreshKey(state: PagingState<Int, Anime>): Int? =
         state.anchorPosition?.let { anchorPosition ->
             val anchorPage = state.closestPageToPosition(anchorPosition)
             anchorPage?.prevKey?.plus(1) ?: anchorPage?.nextKey?.minus(1)
         }
 
-    override suspend fun load(params: LoadParams<Int>): LoadResult<Int, AnimeListQuery.Anime> {
-        return try {
-            val page = params.key ?: 1
-            val response = ApolloClient.getAnimeList(
-                page = page,
-                limit = params.loadSize,
-                order = queryMap.order,
-                kind = queryMap.kind.joinToString(","),
-                status = queryMap.status.joinToString(","),
-                season = queryMap.season.joinToString(","),
-                score = setScore(),
-                duration = queryMap.duration.joinToString(","),
-                rating = queryMap.rating.joinToString(","),
-                genre = queryMap.genre.joinToString(","),
-//                studio = queryMap.studio,
-//                franchise = queryMap.franchise,
-//                censored = queryMap.censored,
-//                myList = queryMap.myList,
-                search = queryMap.title
-            )
+    override suspend fun load(params: LoadParams<Int>): LoadResult<Int, Anime> = try {
+        val page = params.key ?: 1
+        val response = ApolloClient.getAnimeList(
+            page = page,
+            limit = params.loadSize,
+            order = query.order,
+            kind = query.kind.joinToString(","),
+            status = query.status.joinToString(","),
+            season = query.season.joinToString(","),
+            score = setScore(query.status, query.score),
+            duration = query.duration.joinToString(","),
+            rating = query.rating.joinToString(","),
+            genre = query.genre.joinToString(","),
+//          studio = queryMap.studio,
+//          franchise = queryMap.franchise,
+//          censored = queryMap.censored,
+//          myList = queryMap.myList,
+            search = query.title
+        )
 
-            val prevKey = if (page > 1) page.minus(1) else null
-            val nextKey = if (response.isNotEmpty()) page.plus(1) else null
+        val prevKey = if (page > 1) page.minus(1) else null
+        val nextKey = if (response.isNotEmpty()) page.plus(1) else null
 
-            LoadResult.Page(data = response, prevKey = prevKey, nextKey = nextKey)
-        } catch (e: Throwable) {
-            LoadResult.Error(e)
-        }
+        LoadResult.Page(data = response, prevKey = prevKey, nextKey = nextKey)
+    } catch (e: Throwable) {
+        LoadResult.Error(e)
     }
-
-    private fun setScore() = if (STATUSES.keys.elementAt(0) in queryMap.status) null
-    else queryMap.score.toInt()
 }
