4364dfec1558890d8886ba7bdb34ad3f232bae8b
pav <pewaru@inbox.ru>
2024-12-30 15:59:18 +0300

    — Рефакторинг кода

--- app/src/main/java/org/application/shikiapp/models/views/RanobeListViewModel.kt
+++ app/src/main/java/org/application/shikiapp/models/views/RanobeListViewModel.kt
@@ -1,6 +1,7 @@
 package org.application.shikiapp.models.views
 
 import androidx.lifecycle.viewModelScope
+import androidx.paging.LoadState
 import androidx.paging.Pager
 import androidx.paging.PagingConfig
 import androidx.paging.cachedIn
@@ -10,7 +11,9 @@ import kotlinx.coroutines.FlowPreview
 import kotlinx.coroutines.flow.debounce
 import kotlinx.coroutines.flow.flatMapLatest
 import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.onStart
 import kotlinx.coroutines.flow.retryWhen
+import org.application.shikiapp.network.ApolloClient
 import org.application.shikiapp.network.paging.RanobePaging
 import retrofit2.HttpException
 
@@ -23,6 +26,12 @@ class RanobeListViewModel : MangaListViewModel() {
         ).flow.map { list ->
             val set = mutableSetOf<String>()
             list.filter { if (it.id in set) false else set.add(it.id) }
+        }
+    }.onStart {
+        try {
+            genres.addAll(ApolloClient.getMangaGenres())
+        } catch (e: Throwable) {
+            LoadState.Error(e)
         }
     }.cachedIn(viewModelScope)
         .retryWhen { cause, attempt -> cause is HttpException || attempt <= 3 }
