c7db68ce6b4b4aa0d54e155b61ef597072083e59
pav <pewaru@inbox.ru>
2024-12-30 16:07:01 +0300

    — Рефакторинг кода

--- app/src/main/java/org/application/shikiapp/network/Comments.kt
+++ app/src/main/java/org/application/shikiapp/network/Comments.kt
@@ -1,21 +1,24 @@
-package org.application.shikiapp.models.views
+package org.application.shikiapp.network
 
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
 import androidx.paging.Pager
 import androidx.paging.PagingConfig
 import androidx.paging.cachedIn
 import androidx.paging.filter
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.flow.emptyFlow
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.retryWhen
 import org.application.shikiapp.network.paging.CommentsPaging
 import retrofit2.HttpException
 
-class CommentViewModel(id: Long) : ViewModel() {
-    val comments = Pager(PagingConfig(pageSize = 15, enablePlaceholders = false))
-    { CommentsPaging(id) }.flow.map { comment ->
+object Comments {
+    fun getComments(id: Long?, scope: CoroutineScope) = if (id == null) emptyFlow()
+    else Pager(
+        config = PagingConfig(pageSize = 15, enablePlaceholders = false),
+        pagingSourceFactory = { CommentsPaging(id) }
+    ).flow.map { comment ->
         val set = mutableSetOf<Long>()
         comment.filter { if (set.contains(it.id)) false else set.add(it.id) }
-    }.cachedIn(viewModelScope)
+    }.cachedIn(scope)
         .retryWhen { cause, attempt -> cause is HttpException || attempt <= 3 }
 }
