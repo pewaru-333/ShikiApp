33b6f48d298e38814b202b70b686b3a21dd07f31
pav <pewaru@inbox.ru>
2024-12-30 16:04:24 +0300

    — Удаление ненужных файлов

--- app/src/main/java/org/application/shikiapp/models/views/ViewModelFactoryHelper.kt
+++
@@ -1,9 +0,0 @@
-package org.application.shikiapp.models.views
-
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.ViewModelProvider
-
-fun <viewModel : ViewModel> factory(initializer: () -> viewModel): ViewModelProvider.Factory =
-    object : ViewModelProvider.Factory {
-        override fun <T : ViewModel> create(modelClass: Class<T>): T = initializer() as T
-    }
