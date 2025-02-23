4140b1e20d1f40327ddca3795d12af2753d6df3a
pav <pewaru@inbox.ru>
2025-02-23 11:37:06 +0300

    — Добавление функции "Потяни, чтобы обновить"

--- app/src/main/java/org/application/shikiapp/screens/NewsScreen.kt
+++ app/src/main/java/org/application/shikiapp/screens/NewsScreen.kt
@@ -1,3 +1,5 @@
+@file:OptIn(ExperimentalMaterial3Api::class)
+
 package org.application.shikiapp.screens
 
 import androidx.compose.foundation.layout.Arrangement
@@ -7,9 +9,17 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.ElevatedCard
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
+import androidx.compose.material3.pulltorefresh.PullToRefreshBox
+import androidx.compose.material3.pulltorefresh.rememberPullToRefreshState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.FilterQuality
@@ -21,6 +31,7 @@ import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.paging.LoadState
 import androidx.paging.compose.collectAsLazyPagingItems
 import coil.compose.AsyncImage
+import kotlinx.coroutines.launch
 import org.application.shikiapp.models.data.News
 import org.application.shikiapp.models.views.NewsViewModel
 import org.application.shikiapp.utils.convertDate
@@ -28,19 +39,33 @@ import org.application.shikiapp.utils.getPoster
 
 @Composable
 fun NewsScreen(toDetail: (Long) -> Unit) {
+    val scope = rememberCoroutineScope()
     val news = viewModel<NewsViewModel>()
     val list = news.newsList.collectAsLazyPagingItems()
+    val state = rememberPullToRefreshState()
+    var isRefreshing by remember { mutableStateOf(false) }
 
-    when (list.loadState.refresh) {
-        is LoadState.Error -> ErrorScreen(list::retry)
-        is LoadState.Loading -> LoadingScreen()
-        is LoadState.NotLoading -> LazyColumn(
-            contentPadding = PaddingValues(16.dp),
-            verticalArrangement = Arrangement.spacedBy(24.dp)
-        ) {
-            items(list.itemCount) { NewsCard(list[it]!!, toDetail) }
-            if (list.loadState.append == LoadState.Loading) item { LoadingScreen() }
-            if (list.loadState.hasError) item { ErrorScreen(list::retry) }
+    val onRefresh = {
+        list.refresh().also {
+            isRefreshing = false
+            scope.launch {
+                state.animateToHidden()
+            }
+        }
+    }
+
+    PullToRefreshBox(isRefreshing, onRefresh, Modifier, state) {
+        when (list.loadState.refresh) {
+            is LoadState.Error -> ErrorScreen(list::retry)
+            is LoadState.Loading -> LoadingScreen()
+            is LoadState.NotLoading -> LazyColumn(
+                contentPadding = PaddingValues(16.dp),
+                verticalArrangement = Arrangement.spacedBy(24.dp)
+            ) {
+                items(list.itemCount) { NewsCard(list[it]!!, toDetail) }
+                if (list.loadState.append == LoadState.Loading) item { LoadingScreen() }
+                if (list.loadState.hasError) item { ErrorScreen(list::retry) }
+            }
         }
     }
 }
