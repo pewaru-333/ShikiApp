d5fbb4cf0f45f88c4fe8c7b28bc85a5b4814f801
pav <pewaru@inbox.ru>
2025-02-23 11:12:19 +0300

    — Смена библиотеки сериализации

--- app/src/main/java/org/application/shikiapp/models/data/Ban.kt
+++ app/src/main/java/org/application/shikiapp/models/data/Ban.kt
@@ -1,17 +1,17 @@
 package org.application.shikiapp.models.data
 
-import com.squareup.moshi.Json
-import com.squareup.moshi.JsonClass
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
 
-@JsonClass(generateAdapter = true)
+@Serializable
 data class Ban(
-    @Json(name = "id") val id: Long,
-    @Json(name = "user_id") val userId: Long,
-    @Json(name = "comment") val comment: String,
-    @Json(name = "moderator_id") val moderatorId: Long,
-    @Json(name = "reason") val reason: String,
-    @Json(name = "created_at") val createdAt: String,
-    @Json(name = "duration_minutes") val durationMinutes: Int,
-    @Json(name = "user") val user: UserShort,
-    @Json(name = "moderator") val moderator: UserShort
+    @SerialName("id") val id: Long,
+    @SerialName("user_id") val userId: Long,
+    @SerialName("comment") val comment: String,
+    @SerialName("moderator_id") val moderatorId: Long,
+    @SerialName("reason") val reason: String,
+    @SerialName("created_at") val createdAt: String,
+    @SerialName("duration_minutes") val durationMinutes: Int,
+    @SerialName("user") val user: UserBasic,
+    @SerialName("moderator") val moderator: UserBasic
 )
