6688e79515ec200f380a4d85b24db5d6dfeb2f37
pav <pewaru@inbox.ru>
2025-02-23 11:22:34 +0300

    — Изменение получения предыдущего и следующего номера страницы при пагинации

--- app/src/main/java/org/application/shikiapp/network/paging/ClubCharactersPaging.kt
+++ app/src/main/java/org/application/shikiapp/network/paging/ClubCharactersPaging.kt
@@ -2,23 +2,23 @@ package org.application.shikiapp.network.paging
 
 import androidx.paging.PagingSource
 import androidx.paging.PagingState
-import org.application.shikiapp.models.data.Character
-import org.application.shikiapp.network.NetworkClient
+import org.application.shikiapp.models.data.BasicInfo
+import org.application.shikiapp.network.client.NetworkClient
 
-class ClubCharactersPaging(private val clubId: Long) : PagingSource<Int, Character>() {
+class ClubCharactersPaging(private val clubId: Long) : PagingSource<Int, BasicInfo>() {
 
-    override fun getRefreshKey(state: PagingState<Int, Character>): Int? =
+    override fun getRefreshKey(state: PagingState<Int, BasicInfo>) =
         state.anchorPosition?.let { anchorPosition ->
             val anchorPage = state.closestPageToPosition(anchorPosition)
             anchorPage?.prevKey?.plus(1) ?: anchorPage?.nextKey?.minus(1)
         }
 
-    override suspend fun load(params: LoadParams<Int>): LoadResult<Int, Character> = try {
+    override suspend fun load(params: LoadParams<Int>): LoadResult<Int, BasicInfo> = try {
         val page = params.key ?: 1
         val response = NetworkClient.clubs.getCharacters(clubId, page, params.loadSize)
 
-        val prevKey = if (page > 0) page.minus(1) else null
-        val nextKey = if (response.isNotEmpty()) page.plus(1) else null
+        val prevKey = if (page == 1) null else page - 1
+        val nextKey = if (response.isEmpty()) null else page + 1
 
         LoadResult.Page(response, prevKey, nextKey)
     } catch (e: Throwable) {
