100937ffcd998112093520a4c1d85abfa49e8d91
pav <pewaru@inbox.ru>
2025-02-23 11:12:54 +0300

    — Смена библиотеки сериализации

--- app/src/main/java/org/application/shikiapp/models/data/Character.kt
+++ app/src/main/java/org/application/shikiapp/models/data/Character.kt
@@ -1,34 +1,44 @@
 package org.application.shikiapp.models.data
 
-import com.squareup.moshi.Json
-import com.squareup.moshi.JsonClass
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+import org.application.shikiapp.utils.BLANK
 
-@JsonClass(generateAdapter = true)
-data class Character(
-    @Json(name = "id") val id: Long,
-    @Json(name = "name") val name: String,
-    @Json(name = "russian") val russian: String?,
-    @Json(name = "image") val image: Image,
-    @Json(name = "url") val url: String,
-    @Json(name = "altname") val altName: String?,
-    @Json(name = "japanese") val japanese: String?,
-    @Json(name = "description") val description: String?,
-    @Json(name = "description_html") val descriptionHTML: String?,
-    @Json(name = "description_source") val descriptionSource: String?,
-    @Json(name = "favoured") val favoured: Boolean,
-    @Json(name = "thread_id") val threadId: Long?,
-    @Json(name = "topic_id") val topicId: Long?,
-    @Json(name = "updated_at") val updatedAt: String?,
-    @Json(name = "seyu") val seyu: List<CharacterPerson> = emptyList(),
-    @Json(name = "animes") val animes: List<AnimeShort> = emptyList(),
-    @Json(name = "mangas") val mangas: List<MangaShort> = emptyList()
-)
+@Serializable
+class Character : BasicInfo() {
+    @SerialName("altname")
+    val altName: String? = null
 
-@JsonClass(generateAdapter = true)
-data class CharacterPerson(
-    @Json(name = "id") val id: Long,
-    @Json(name = "name") val name: String,
-    @Json(name = "russian") val russian: String?,
-    @Json(name = "image") val image: Image,
-    @Json(name = "url") val url: String,
-)
+    @SerialName("japanese")
+    val japanese: String? = null
+
+    @SerialName("description")
+    val description: String? = null
+
+    @SerialName("description_html")
+    val descriptionHTML: String? = null
+
+    @SerialName("description_source")
+    val descriptionSource: String? = null
+
+    @SerialName("favoured")
+    val favoured: Boolean = false
+
+    @SerialName("thread_id")
+    val threadId: Long = 0L
+
+    @SerialName("topic_id")
+    val topicId: Long = 0L
+
+    @SerialName("updated_at")
+    val updatedAt: String = BLANK
+
+    @SerialName("seyu")
+    val seyu: List<BasicInfo> = emptyList()
+
+    @SerialName("animes")
+    val animes: List<AnimeBasic> = emptyList()
+
+    @SerialName("mangas")
+    val mangas: List<MangaBasic> = emptyList()
+}
