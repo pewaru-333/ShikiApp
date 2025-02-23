e439c1fd8f3f3b2af8391c97449aa60b00a295f4
pav <pewaru@inbox.ru>
2025-02-23 11:13:28 +0300

    — Смена библиотеки сериализации

--- app/src/main/java/org/application/shikiapp/models/data/Comment.kt
+++ app/src/main/java/org/application/shikiapp/models/data/Comment.kt
@@ -1,20 +1,20 @@
 package org.application.shikiapp.models.data
 
-import com.squareup.moshi.Json
-import com.squareup.moshi.JsonClass
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
 
-@JsonClass(generateAdapter = true)
+@Serializable
 data class Comment(
-    @Json(name = "id") val id: Long,
-    @Json(name = "user_id") val userId: Long,
-    @Json(name = "commentable_id") val commentableId: Long,
-    @Json(name = "commentable_type") val commentableType: String,
-    @Json(name = "body") val body: String,
-    @Json(name = "html_body") val htmlBody: String,
-    @Json(name = "created_at") val createdAt: String,
-    @Json(name = "updated_at") val updatedAt: String,
-    @Json(name = "is_offtopic") val isOfftopic: Boolean,
-    @Json(name = "is_summary") val isSummary: Boolean,
-    @Json(name = "can_be_edited") val canBeEdited: Boolean,
-    @Json(name = "user") val user: UserShort
+    @SerialName("id") val id: Long,
+    @SerialName("user_id") val userId: Long,
+    @SerialName("commentable_id") val commentableId: Long,
+    @SerialName("commentable_type") val commentableType: String,
+    @SerialName("body") val body: String,
+    @SerialName("html_body") val htmlBody: String,
+    @SerialName("created_at") val createdAt: String,
+    @SerialName("updated_at") val updatedAt: String,
+    @SerialName("is_offtopic") val isOfftopic: Boolean,
+    @SerialName("is_summary") val isSummary: Boolean,
+    @SerialName("can_be_edited") val canBeEdited: Boolean,
+    @SerialName("user") val user: UserBasic
 )
