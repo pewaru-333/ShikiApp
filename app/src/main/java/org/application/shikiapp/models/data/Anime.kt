07ef70d72b8750245defb178ac625d8e7d97443c
pav <pewaru@inbox.ru>
2025-02-23 11:12:01 +0300

    — Смена библиотеки сериализации

--- app/src/main/java/org/application/shikiapp/models/data/Anime.kt
+++ app/src/main/java/org/application/shikiapp/models/data/Anime.kt
@@ -1,65 +1,65 @@
 package org.application.shikiapp.models.data
 
 
-import com.squareup.moshi.Json
-import com.squareup.moshi.JsonClass
+import kotlinx.serialization.Serializable
 
-@JsonClass(generateAdapter = true)
-data class Anime(
-    @Json(name = "id") val id: Long,
-    @Json(name = "name") val name: String,
-    @Json(name = "russian") val russian: String,
-    @Json(name = "image") val image: Image,
-    @Json(name = "url") val url: String,
-    @Json(name = "kind") val kind: String?,
-    @Json(name = "score") val score: String,
-    @Json(name = "status") val status: String,
-    @Json(name = "episodes") val episodes: Int,
-    @Json(name = "episodes_aired") val episodesAired: Int,
-    @Json(name = "aired_on") val airedOn: String?,
-    @Json(name = "released_on") val releasedOn: String?,
-    @Json(name = "rating") val rating: String?,
-    @Json(name = "english") val english: List<String>?,
-    @Json(name = "japanese") val japanese: List<String>?,
-    @Json(name = "synonyms") val synonyms: List<String>?,
-    @Json(name = "license_name_ru") val licenseNameRu: String?,
-    @Json(name = "duration") val duration: Int?,
-    @Json(name = "description") val description: String?,
-    @Json(name = "description_html") val descriptionHtml: String?,
-    @Json(name = "description_source") val descriptionSource: String?,
-    @Json(name = "franchise") val franchise: String?,
-    @Json(name = "favoured") val favoured: Boolean,
-    @Json(name = "anons") val anons: Boolean?,
-    @Json(name = "ongoing") val ongoing: Boolean?,
-    @Json(name = "thread_id") val threadId: Long?,
-    @Json(name = "topic_id") val topicId: Long?,
-    @Json(name = "myanimelist_id") val myAnimeListId: Long?,
-    @Json(name = "rates_scores_stats") val ratesScoresStats: List<RatesScores>?,
-    @Json(name = "rates_statuses_stats") val ratesStatusesStats: List<RatesStatuses>?,
-    @Json(name = "updated_at") val updatedAt: String?,
-    @Json(name = "next_episode_at") val nextEpisodeAt: String?,
-    @Json(name = "fansubbers") val fanSubbers: List<String> = emptyList(),
-    @Json(name = "fandubbers") val fanDubbers: List<String> = emptyList(),
-    @Json(name = "licensors") val licensors: List<String> = emptyList(),
-    // @Json(name = "genres") val genres: List<AnimeGenresQuery.Genre> = emptyList(),
-    @Json(name = "studios") val studios: List<Studio> = emptyList(),
-    @Json(name = "videos") val videos: List<Video> = emptyList(),
-    @Json(name = "screenshots") val screenshots: List<Screenshot> = emptyList(),
-    @Json(name = "user_rate") val userRate: UserRate?
-)
 
-@JsonClass(generateAdapter = true)
-data class AnimeShort(
-    @Json(name = "id") override val id: Long,
-    @Json(name = "name") override val name: String,
-    @Json(name = "russian") override val russian: String?,
-    @Json(name = "image") override val image: Image,
-    @Json(name = "url") override val url: String,
-    @Json(name = "kind") override val kind: String?,
-    @Json(name = "score") val score: String?,
-    @Json(name = "status") val status: String?,
-    @Json(name = "episodes") val episodes: Int?,
-    @Json(name = "episodes_aired") val episodesAired: Int?,
-    @Json(name = "aired_on") override val airedOn: String?,
-    @Json(name = "released_on") override val releasedOn: String?
-) : Content()
+@Serializable
+class Anime : Content() {
+//    @SerialName("kind")
+//    override val kind: Kind = AnimeKind.TV
+//
+//    @SerialName("score")
+//    val score: String = BLANK
+//
+//    @SerialName("status")
+//    val status: String = BLANK
+//
+//    @SerialName("episodes")
+//    val episodes: Int = 0
+//
+//    @SerialName("episodes_aired")
+//    val episodesAired: Int = 0
+//
+//    @SerialName("rating")
+//    val rating: String? = null
+//
+//    @SerialName("synonyms")
+//    val synonyms: List<String?>? = null
+//
+//    @SerialName("duration")
+//    val duration: Int? = null
+//
+//    @SerialName("updated_at")
+//    val updatedAt: String? = null
+//
+//    @SerialName("next_episode_at")
+//    val nextEpisodeAt: String? = null
+//
+//    @SerialName("fansubbers")
+//    val fanSubbers: List<String> = emptyList()
+//
+//    @SerialName("fandubbers")
+//    val fanDubbers: List<String> = emptyList()
+//
+//    @SerialName("studios")
+//    val studios: List<Studio> = emptyList()
+//
+//    @SerialName("videos")
+//    val videos: List<Video> = emptyList()
+//
+//    @SerialName("screenshots")
+//    val screenshots: List<Screenshot> = emptyList()
+//
+//    @SerialName("user_rate")
+//    val userRate: UserRate? = null
+}
+
+@Serializable
+class AnimeBasic : BasicContent() {
+    //@SerialName("episodes")
+    val episodes: Int? = null
+
+//    @SerialName("episodes_aired")
+//    val episodesAired: Int? = null
+}
