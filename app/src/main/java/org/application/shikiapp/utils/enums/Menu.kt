96fe5cfecb48aa5239d9eead4445379d415c84c5
pav <pewaru@inbox.ru>
2025-04-15 14:09:52 +0300

    — Изменение объекта на класс

--- app/src/main/java/org/application/shikiapp/utils/enums/Menu.kt
+++ app/src/main/java/org/application/shikiapp/utils/enums/Menu.kt
@@ -5,7 +5,7 @@ import org.application.shikiapp.R
 import org.application.shikiapp.utils.navigation.Screen
 
 enum class Menu(val route: Screen, @StringRes val title: Int, val icon: Int) {
-    CATALOG(Screen.Catalog, R.string.text_catalog, R.drawable.vector_home),
+    CATALOG(Screen.Catalog(), R.string.text_catalog, R.drawable.vector_home),
     NEWS(Screen.News, R.string.text_news, R.drawable.vector_news),
     CALENDAR(Screen.Calendar, R.string.text_calendar, R.drawable.vector_calendar),
     PROFILE(Screen.Profile, R.string.text_profile, R.drawable.vector_character),
