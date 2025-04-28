6b1dab8406992b9d406f9d02ac5a55578e9c5a0e
pav <pewaru@inbox.ru>
2025-04-28 13:54:03 +0300

    — Явное указание типа контента запроса

--- app/src/main/java/org/application/shikiapp/network/calls/UserRates.kt
+++ app/src/main/java/org/application/shikiapp/network/calls/UserRates.kt
@@ -1,22 +1,24 @@
 package org.application.shikiapp.network.calls
 
 import io.ktor.client.HttpClient
-import io.ktor.client.call.body
 import io.ktor.client.request.delete
 import io.ktor.client.request.patch
 import io.ktor.client.request.post
 import io.ktor.client.request.setBody
+import io.ktor.http.ContentType
+import io.ktor.http.contentType
 import org.application.shikiapp.models.data.NewRate
-import org.application.shikiapp.models.data.UserRate
 
 class UserRates(private val client: HttpClient) {
     suspend fun createRate(newRate: NewRate) = client.post("v2/user_rates") {
+        contentType(ContentType.Application.Json)
         setBody(newRate)
-    }.body<UserRate>()
+    }
 
     suspend fun updateRate(id: Long, newRate: NewRate) = client.patch("v2/user_rates/$id") {
+        contentType(ContentType.Application.Json)
         setBody(newRate)
-    }.body<UserRate>()
+    }
 
     suspend fun increment(id: Long) = client.post("v2/user_rates/$id/increment")
     suspend fun delete(id: Long) = client.delete("v2/user_rates/$id")
