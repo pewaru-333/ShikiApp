65982a6098d21e3e6acd41a2ae673c5ffac7460e
pav <pewaru@inbox.ru>
2025-04-28 13:52:43 +0300

    — Изменение метода

--- app/src/main/java/org/application/shikiapp/models/viewModels/NewsViewModel.kt
+++ app/src/main/java/org/application/shikiapp/models/viewModels/NewsViewModel.kt
@@ -6,6 +6,8 @@ import androidx.paging.Pager
 import androidx.paging.PagingConfig
 import androidx.paging.cachedIn
 import io.ktor.client.plugins.ClientRequestException
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.flowOn
 import kotlinx.coroutines.flow.retryWhen
 import org.application.shikiapp.models.data.News
 import org.application.shikiapp.network.client.NetworkClient
@@ -13,13 +15,14 @@ import org.application.shikiapp.network.paging.CommonPaging
 
 class NewsViewModel : ViewModel() {
     val newsList = Pager(
-        config = PagingConfig(pageSize = 10),
+        config = PagingConfig(pageSize = 10, enablePlaceholders = false),
         pagingSourceFactory = {
             CommonPaging<News>(News::id) { page, params ->
                 NetworkClient.topics.getNewsList(page, params.loadSize)
             }
         }
     ).flow
+        .flowOn(Dispatchers.IO)
         .cachedIn(viewModelScope)
         .retryWhen { cause, attempt -> cause is ClientRequestException || attempt <= 3 }
 }
