c53d8c77017eed71e866d29ec72b6cab853c8023
pav <pewaru@inbox.ru>
2025-05-11 16:58:12 +0300

    — Добавлена обработка ошибок сети при выходе из аккаунта.

--- app/src/main/java/org/application/shikiapp/models/viewModels/ProfileViewModel.kt
+++ app/src/main/java/org/application/shikiapp/models/viewModels/ProfileViewModel.kt
@@ -3,15 +3,13 @@ package org.application.shikiapp.models.viewModels
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.viewModelScope
 import androidx.navigation.toRoute
-import kotlinx.coroutines.Dispatchers
+import io.ktor.http.HttpStatusCode
 import kotlinx.coroutines.launch
 import org.application.shikiapp.models.data.Token
 import org.application.shikiapp.models.ui.User
+import org.application.shikiapp.network.client.Network
 import org.application.shikiapp.network.response.LoginResponse
-import org.application.shikiapp.network.client.NetworkClient
-import org.application.shikiapp.utils.BLANK
 import org.application.shikiapp.utils.Preferences
-import org.application.shikiapp.utils.TokenManager
 import org.application.shikiapp.utils.navigation.Screen.Login
 import java.net.UnknownHostException
 
@@ -19,39 +17,73 @@ class ProfileViewModel(saved: SavedStateHandle) : UserViewModel(saved) {
     val args = saved.toRoute<Login>()
 
     override val userId: Long
-        get() = Preferences.getUserId()
+        get() = Preferences.userId
 
     override fun loadData() {
-        viewModelScope.launch { emit(LoginResponse.NotLogged) }
-
-        if (Preferences.isTokenExists()) getProfile()
-
-        args.code?.let(::login)
+        viewModelScope.launch {
+            when {
+                Preferences.token != null -> getProfile()
+                args.code != null -> login(args.code)
+                else -> emit(LoginResponse.NotLogged)
+            }
+        }
     }
 
-    private fun getProfile() {
-        viewModelScope.launch {
-            emit(LoginResponse.Logging)
+    private suspend fun getProfile() {
+        emit(LoginResponse.Logging)
 
-            try {
-                emit(
-                    LoginResponse.Logged(
-                        User(
-                            user = user.await(),
-                            clubs = clubs.await(),
-                            comments = comments,
-                            friends = friends,
-                            history = history,
-                            favourites = favourites.await()
-                        )
+        try {
+            emit(
+                LoginResponse.Logged(
+                    User(
+                        user = user.await(),
+                        clubs = clubs.await(),
+                        comments = comments,
+                        friends = friends,
+                        history = history,
+                        favourites = favourites.await()
                     )
                 )
-            } catch (e: Throwable) {
-                when (e) {
-                    is UnknownHostException -> emit(LoginResponse.NetworkError)
-                    else -> emit(LoginResponse.NotLogged)
-                }
+            )
+        } catch (e: Throwable) {
+            when (e) {
+                is UnknownHostException -> emit(LoginResponse.NetworkError)
+                else -> emit(LoginResponse.NotLogged)
             }
+        }
+    }
+
+    private suspend fun login(code: String) {
+        if (response.value == LoginResponse.Logging) {
+            return
+        }
+
+        emit(LoginResponse.Logging)
+
+        try {
+            val token = Network.profile.getToken(code = code)
+            Preferences.saveToken(token)
+
+            val whoAmI = Network.profile.whoAmI()
+            Preferences.setUserId(whoAmI.id)
+
+            emit(
+                LoginResponse.Logged(
+                    User(
+                        user = user.await(),
+                        clubs = clubs.await(),
+                        comments = comments,
+                        friends = friends,
+                        history = history,
+                        favourites = favourites.await()
+                    )
+                )
+            )
+        } catch (_: Throwable) {
+            Preferences.saveToken(Token.empty)
+            Preferences.setUserId(0L)
+
+            emit(LoginResponse.NotLogged)
         }
     }
 
@@ -60,37 +92,18 @@ class ProfileViewModel(saved: SavedStateHandle) : UserViewModel(saved) {
             emit(LoginResponse.Logging)
 
             try {
-                NetworkClient.profile.signOut()
-                Preferences.saveToken(Token(BLANK, BLANK, 0L, BLANK, BLANK, 0L))
-                Preferences.setUserId(0L)
+                val request = Network.profile.signOut()
 
-                emit(LoginResponse.NotLogged)
-            } catch (e: Throwable) {
-                Preferences.saveToken(Token(BLANK, BLANK, 0L, BLANK, BLANK, 0L))
-                Preferences.setUserId(0L)
+                if (request.status == HttpStatusCode.OK) {
+                    Preferences.saveToken(Token.empty)
+                    Preferences.setUserId(0L)
 
-                emit(LoginResponse.NotLogged)
-            }
-        }
-    }
-
-    private fun login(code: String) {
-        if (response.value == LoginResponse.Logging)
-            return
-
-        viewModelScope.launch(Dispatchers.IO) {
-            emit(LoginResponse.Logging)
-
-            try {
-                TokenManager.getToken(code)
-                val user = TokenManager.getUser()
-                val clubs = NetworkClient.user.getClubs(user.id)
-                val comments = getComments(user.id)
-                val favourites = NetworkClient.user.getFavourites(user.id)
-
-                emit(LoginResponse.Logged(User(user, clubs, comments, friends, history, favourites)))
-            } catch (e: Throwable) {
-                emit(LoginResponse.NotLogged)
+                    emit(LoginResponse.NotLogged)
+                } else {
+                    emit(LoginResponse.NetworkError)
+                }
+            } catch (_: Throwable) {
+                emit(LoginResponse.NetworkError)
             }
         }
     }
