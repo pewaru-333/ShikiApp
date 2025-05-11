11edfc9bc9dcd308ee8877b31a2cad27e8276435
pav <pewaru@inbox.ru>
2025-05-11 16:43:16 +0300

    — Обновление логики маппинга аниме

--- app/src/main/java/org/application/shikiapp/models/ui/mappers/Anime.kt
+++ app/src/main/java/org/application/shikiapp/models/ui/mappers/Anime.kt
@@ -3,11 +3,11 @@ package org.application.shikiapp.models.ui.mappers
 import androidx.paging.PagingData
 import androidx.paging.map
 import kotlinx.coroutines.flow.Flow
-import org.application.AnimeAiringQuery
-import org.application.AnimeListQuery
-import org.application.AnimeQuery
-import org.application.AnimeStatsQuery
 import org.application.shikiapp.R
+import org.application.shikiapp.generated.AnimeAiringQuery
+import org.application.shikiapp.generated.AnimeListQuery
+import org.application.shikiapp.generated.AnimeQuery
+import org.application.shikiapp.generated.AnimeStatsQuery
 import org.application.shikiapp.models.data.AnimeBasic
 import org.application.shikiapp.models.data.Comment
 import org.application.shikiapp.models.data.ExternalLink
@@ -17,19 +17,22 @@ import org.application.shikiapp.models.ui.CharacterMain
 import org.application.shikiapp.models.ui.PersonMain
 import org.application.shikiapp.models.ui.Related
 import org.application.shikiapp.models.ui.Similar
+import org.application.shikiapp.models.ui.Statistics
 import org.application.shikiapp.models.ui.list.Content
 import org.application.shikiapp.models.ui.list.ShortContent
 import org.application.shikiapp.screens.fromHtml
 import org.application.shikiapp.utils.BLANK
 import org.application.shikiapp.utils.ROLES_RUSSIAN
+import org.application.shikiapp.utils.ResourceText
+import org.application.shikiapp.utils.ResourceText.StringResource
 import org.application.shikiapp.utils.enums.Kind
+import org.application.shikiapp.utils.enums.LinkedType
 import org.application.shikiapp.utils.enums.Rating
 import org.application.shikiapp.utils.enums.Status
 import org.application.shikiapp.utils.extensions.safeValueOf
 import org.application.shikiapp.utils.getFull
-import org.application.shikiapp.utils.getImage
 import org.application.shikiapp.utils.getSeason
-import org.application.shikiapp.utils.getStudio
+import org.application.shikiapp.utils.getWatchStatus
 
 fun AnimeQuery.Data.Anime.mapper(
     similar: List<AnimeBasic>,
@@ -49,7 +52,7 @@ fun AnimeQuery.Data.Anime.mapper(
         Status.RELEASED.name -> "$episodes / $episodes"
         else -> "$episodesAired / $episodes"
     },
-    studio = getStudio(studios),
+    studio = studios.map(AnimeQuery.Data.Anime.Studio::name).firstOrNull() ?: "Неизвестно",
     score = score.toString(),
     rating = Enum.safeValueOf<Rating>(rating?.rawValue).title,
     genres = genres,
@@ -96,14 +99,31 @@ fun AnimeQuery.Data.Anime.mapper(
         Similar(
             id = it.id.toString(),
             title = it.russian ?: it.name,
-            poster = getImage(it.image.original)
+            poster = it.image.original
         )
     },
     links = links,
     comments = comments,
     screenshots = screenshots.map(AnimeQuery.Data.Anime.Screenshot::originalUrl),
     videos = videos,
-    stats = stats,
+    stats = Pair(
+        first = stats.scoresStats?.let { scores ->
+            Statistics(
+                sum = scores.sumOf(AnimeStatsQuery.Data.Anime.ScoresStat::count),
+                scores = scores.associate {
+                    ResourceText.StaticString(it.score.toString()) to it.count.toString()
+                }
+            )
+        },
+        second = stats.statusesStats?.let { statuses ->
+            Statistics(
+                sum = statuses.sumOf(AnimeStatsQuery.Data.Anime.StatusesStat::count),
+                scores = statuses.associate {
+                    StringResource(getWatchStatus(it.status.rawValue, LinkedType.ANIME)) to it.count.toString()
+                }
+            )
+        }
+    ),
     userRate = userRate
 )
 
@@ -127,6 +147,6 @@ fun PagingData<Topic>.toAnimeContent() = map {
         title = it.linked.russian.orEmpty().ifEmpty(it.linked::name),
         kind = Enum.safeValueOf<Kind>(it.linked.kind).title,
         season = getSeason(it.linked.releasedOn, it.linked.kind),
-        poster = getImage(it.linked.image.original)
+        poster = it.linked.image.original
     )
 }
