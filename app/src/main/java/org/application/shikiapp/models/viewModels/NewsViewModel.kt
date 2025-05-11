d7255bb8cb57a7bd06e3c81c819bd393c424f297
pav <pewaru@inbox.ru>
2025-05-11 16:56:10 +0300

    — Изменена обработка в NewsViewModel

--- app/src/main/java/org/application/shikiapp/models/viewModels/NewsViewModel.kt
+++ app/src/main/java/org/application/shikiapp/models/viewModels/NewsViewModel.kt
@@ -5,12 +5,15 @@ import androidx.lifecycle.viewModelScope
 import androidx.paging.Pager
 import androidx.paging.PagingConfig
 import androidx.paging.cachedIn
+import androidx.paging.map
 import io.ktor.client.plugins.ClientRequestException
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.flowOn
+import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.retryWhen
 import org.application.shikiapp.models.data.News
-import org.application.shikiapp.network.client.NetworkClient
+import org.application.shikiapp.models.ui.mappers.mapper
+import org.application.shikiapp.network.client.Network
 import org.application.shikiapp.network.paging.CommonPaging
 
 class NewsViewModel : ViewModel() {
@@ -18,11 +21,12 @@ class NewsViewModel : ViewModel() {
         config = PagingConfig(pageSize = 10, enablePlaceholders = false),
         pagingSourceFactory = {
             CommonPaging<News>(News::id) { page, params ->
-                NetworkClient.topics.getNewsList(page, params.loadSize)
+                Network.topics.getNewsList(page, params.loadSize)
             }
         }
     ).flow
         .flowOn(Dispatchers.IO)
+        .map { list -> list.map(News::mapper) }
         .cachedIn(viewModelScope)
         .retryWhen { cause, attempt -> cause is ClientRequestException || attempt <= 3 }
 }
