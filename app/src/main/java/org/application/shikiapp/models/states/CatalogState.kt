18eaf5223088c1481eb9b754b1ff8130bdd202dd
pav <pewaru@inbox.ru>
2025-06-24 19:41:10 +0300

    — Изменение импорта

--- app/src/main/java/org/application/shikiapp/models/states/CatalogState.kt
+++ app/src/main/java/org/application/shikiapp/models/states/CatalogState.kt
@@ -5,16 +5,16 @@ import androidx.compose.foundation.lazy.grid.LazyGridState
 import androidx.compose.material3.DrawerState
 import androidx.compose.material3.DrawerValue
 import org.application.shikiapp.utils.BLANK
-import org.application.shikiapp.utils.enums.CatalogItems
+import org.application.shikiapp.utils.enums.CatalogItem
 
 data class CatalogState(
-    val menu: CatalogItems = CatalogItems.ANIME,
+    val menu: CatalogItem = CatalogItem.ANIME,
     val search: String = BLANK,
     val showFiltersA: Boolean = false,
     val showFiltersM: Boolean = false,
     val showFiltersR: Boolean = false,
     val showFiltersP: Boolean = false,
-    val listStates: Map<CatalogItems, LazyListState> = CatalogItems.entries.associateWith { LazyListState() },
-    val gridStates: Map<CatalogItems, LazyGridState> = CatalogItems.entries.associateWith { LazyGridState() },
+    val listStates: Map<CatalogItem, LazyListState> = CatalogItem.entries.associateWith { LazyListState() },
+    val gridStates: Map<CatalogItem, LazyGridState> = CatalogItem.entries.associateWith { LazyGridState() },
     val drawerState: DrawerState = DrawerState(DrawerValue.Closed)
 )
