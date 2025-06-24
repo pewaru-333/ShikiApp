d404478c80106654cea002318804433f153474d2
pav <pewaru@inbox.ru>
2025-06-24 19:51:03 +0300

    — Реализованы функции для управления друзьями и поиска пользователей

--- app/src/main/java/org/application/shikiapp/network/calls/User.kt
+++ app/src/main/java/org/application/shikiapp/network/calls/User.kt
@@ -2,8 +2,10 @@ package org.application.shikiapp.network.calls
 
 import io.ktor.client.HttpClient
 import io.ktor.client.call.body
+import io.ktor.client.request.delete
 import io.ktor.client.request.get
 import io.ktor.client.request.parameter
+import io.ktor.client.request.post
 import org.application.shikiapp.models.data.BaseRate
 import org.application.shikiapp.models.data.ClubBasic
 import org.application.shikiapp.models.data.Favourites
@@ -12,13 +14,11 @@ import org.application.shikiapp.models.data.User
 import org.application.shikiapp.models.data.UserBasic
 
 class User(private val client: HttpClient) {
-
-//    @GET("users")
-//    suspend fun getList(
-//        @Query("search") search: String? = null,
-//        @Query("page") page: Int = 1,
-//        @Query("limit") limit: Int = 5,
-//    ): List<User>
+    suspend fun getUsers(search: String, page: Int = 1, limit: Int = 30) = client.get("users") {
+        parameter("search", search)
+        parameter("page", page)
+        parameter("limit", limit)
+    }.body<List<UserBasic>>()
 
     suspend fun getUser(id: Long) = client.get("users/$id").body<User>()
 
@@ -68,4 +68,8 @@ class User(private val client: HttpClient) {
         parameter("limit", limit)
         parameter("target_type", targetType)
     }.body<List<History>>()
+
+    suspend fun addFriend(id: Long) = client.post("friends/$id")
+
+    suspend fun removeFriend(id: Long) = client.delete("friends/$id")
 }
