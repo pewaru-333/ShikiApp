6b90fc61b5e96213d77b2b5b3d700a991888c8ff
pav <pewaru@inbox.ru>
2025-06-24 19:45:02 +0300

    — Замена `SnapshotStateList` на `SnapshotStateSet`

--- app/src/main/java/org/application/shikiapp/models/states/FiltersState.kt
+++ app/src/main/java/org/application/shikiapp/models/states/FiltersState.kt
@@ -1,31 +1,28 @@
 package org.application.shikiapp.models.states
 
-import androidx.compose.runtime.mutableStateListOf
-import androidx.compose.runtime.snapshots.SnapshotStateList
+import androidx.compose.runtime.mutableStateSetOf
+import androidx.compose.runtime.snapshots.SnapshotStateSet
 import org.application.shikiapp.utils.BLANK
 import org.application.shikiapp.utils.enums.Order
-import org.application.shikiapp.utils.enums.PeopleFilterItems
+import org.application.shikiapp.utils.enums.PeopleFilterItem
 
 data class FiltersState(
     val order: Order = Order.RANKED,
-    val kind: SnapshotStateList<String> = mutableStateListOf(),
-    val status: SnapshotStateList<String> = mutableStateListOf(),
+    val kind: SnapshotStateSet<String> = mutableStateSetOf(),
+    val status: SnapshotStateSet<String> = mutableStateSetOf(),
     val seasonYS: String = BLANK,
     val seasonYF: String = BLANK,
-    val seasonS: SnapshotStateList<String> = mutableStateListOf(),
-    val season: SnapshotStateList<String> = mutableStateListOf(),
+    val seasonS: SnapshotStateSet<String> = mutableStateSetOf(),
+    val season: SnapshotStateSet<String> = mutableStateSetOf(),
     val score: Float = 6f,
-    val duration: SnapshotStateList<String> = mutableStateListOf(),
-    val rating: SnapshotStateList<String> = mutableStateListOf(),
-    val genres: SnapshotStateList<String> = mutableStateListOf(),
+    val duration: SnapshotStateSet<String> = mutableStateSetOf(),
+    val rating: SnapshotStateSet<String> = mutableStateSetOf(),
+    val genres: SnapshotStateSet<String> = mutableStateSetOf(),
     val studio: String? = null,
     val publisher: String? = null,
     val franchise: String? = null,
     val censored: Boolean? = null,
     val myList: String? = null,
-    val isSeyu: Boolean? = null,
-    val isProducer: Boolean? = null,
-    val isMangaka: Boolean? = null,
-    val roles: SnapshotStateList<PeopleFilterItems> = mutableStateListOf(),
+    val roles: SnapshotStateSet<PeopleFilterItem> = mutableStateSetOf(),
     val title: String = BLANK
 )
