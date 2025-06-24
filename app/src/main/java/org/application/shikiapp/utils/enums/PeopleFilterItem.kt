521ed4b1095e939250ccb7f26db82812c3a7db21
pav <pewaru@inbox.ru>
2025-06-24 19:47:48 +0300

    — Переименование enum класса PeopleFilterItems в PeopleFilterItem

--- app/src/main/java/org/application/shikiapp/utils/enums/PeopleFilterItem.kt
+++ app/src/main/java/org/application/shikiapp/utils/enums/PeopleFilterItem.kt
@@ -3,7 +3,7 @@ package org.application.shikiapp.utils.enums
 import androidx.annotation.StringRes
 import org.application.shikiapp.R
 
-enum class PeopleFilterItems(@StringRes val title: Int) {
+enum class PeopleFilterItem(@StringRes val title: Int) {
     SEYU(R.string.text_seyu),
     PRODUCER(R.string.text_producer),
     MANGAKA(R.string.text_mangaka)
