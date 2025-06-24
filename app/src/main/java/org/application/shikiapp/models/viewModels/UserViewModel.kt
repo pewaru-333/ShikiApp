b7a58e6d63de7f2b3c5d251f230fdb530b099aad
pav <pewaru@inbox.ru>
2025-06-24 19:52:26 +0300

    — Перенесены события из `UserDetailEvent` в `ContentDetailEvent` — Реализовано добавление/удаление пользователя из друзей — Обновлен способ получения данных о пользователе

--- app/src/main/java/org/application/shikiapp/models/viewModels/UserViewModel.kt
+++ app/src/main/java/org/application/shikiapp/models/viewModels/UserViewModel.kt
@@ -14,7 +14,7 @@ import kotlinx.coroutines.Deferred
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.retryWhen
 import kotlinx.coroutines.launch
-import org.application.shikiapp.events.UserDetailEvent
+import org.application.shikiapp.events.ContentDetailEvent
 import org.application.shikiapp.models.data.ClubBasic
 import org.application.shikiapp.models.data.Comment
 import org.application.shikiapp.models.data.Favourites
@@ -28,12 +28,15 @@ import org.application.shikiapp.network.paging.CommonPaging
 import org.application.shikiapp.network.response.Response
 import org.application.shikiapp.utils.navigation.Screen
 
-open class UserViewModel(private val saved: SavedStateHandle) : ContentDetailViewModel<User, UserState, UserDetailEvent>() {
+open class UserViewModel(private val saved: SavedStateHandle) : ContentDetailViewModel<User, UserState>() {
     open val userId: Long
         get() = saved.toRoute<Screen.User>().id
 
     protected val friends = Pager(
-        config = PagingConfig(pageSize = 10, enablePlaceholders = false),
+        config = PagingConfig(
+            pageSize = 10,
+            enablePlaceholders = false
+        ),
         pagingSourceFactory = {
             CommonPaging<UserBasic>(UserBasic::id) { page, params ->
                 Network.user.getFriends(userId, page, params.loadSize)
@@ -44,7 +47,10 @@ open class UserViewModel(private val saved: SavedStateHandle) : ContentDetailVie
         .retryWhen { _, attempt -> attempt <= 5 }
 
     protected val history = Pager(
-        config = PagingConfig(pageSize = 20, enablePlaceholders = false),
+        config = PagingConfig(
+            pageSize = 20,
+            enablePlaceholders = false
+        ),
         pagingSourceFactory = {
             CommonPaging<History>(History::id) { page, params ->
                 Network.user.getHistory(userId, page, params.loadSize)
@@ -74,10 +80,17 @@ open class UserViewModel(private val saved: SavedStateHandle) : ContentDetailVie
             emit(Response.Loading)
 
             try {
+                val userLoaded = user.await()
+
+                updateState {
+                    it.copy(
+                        isFriend = userLoaded.inFriends == true
+                    )
+                }
+
                 emit(
                     Response.Success(
-                        User(
-                            user = user.await(),
+                        userLoaded.mapper(
                             clubs = clubs.await(),
                             comments = comments,
                             friends = friends,
@@ -92,32 +105,35 @@ open class UserViewModel(private val saved: SavedStateHandle) : ContentDetailVie
         }
     }
 
-    override fun onEvent(event: UserDetailEvent) {
+    override fun onEvent(event: ContentDetailEvent) {
         when (event) {
-            is UserDetailEvent.ShowComments -> updateState { it.copy(showComments = !it.showComments) }
-            is UserDetailEvent.ShowSheet -> updateState { it.copy(showSheet = !it.showSheet) }
-            is UserDetailEvent.ShowFavourite -> updateState {
-                it.copy(
-                    showFavourite = !it.showFavourite,
-                    showSheet = !it.showSheet
-                )
+            ContentDetailEvent.ShowComments -> updateState { it.copy(showComments = !it.showComments) }
+
+            is ContentDetailEvent.User -> when (event) {
+                ContentDetailEvent.User.ToggleFriend -> toggleFriend()
+
+                ContentDetailEvent.User.ShowDialogToggleFriend -> updateState { it.copy(showDialogToggleFriend = !it.showDialogToggleFriend) }
+
+                is ContentDetailEvent.User.PickMenu -> updateState { it.copy(menu = event.menu) }
+
+                is ContentDetailEvent.User.PickFavouriteTab -> updateState { it.copy(favouriteTab = event.tab) }
             }
 
-            is UserDetailEvent.ShowHistory -> updateState {
-                it.copy(
-                    showHistory = !it.showHistory,
-                    showSheet = !it.showSheet
-                )
-            }
+            else -> Unit
+        }
+    }
 
-            is UserDetailEvent.PickMenu -> updateState {
-                it.copy(
-                    menu = event.menu,
-                    showDialog = !it.showDialog
-                )
-            }
+    fun toggleFriend() {
+        updateState { it.copy(showDialogToggleFriend = false) }
+        viewModelScope.launch {
+            try {
+                if (state.value.isFriend) Network.user.removeFriend(userId)
+                else Network.user.addFriend(userId)
+            } catch (_: Throwable) {
 
-            is UserDetailEvent.PickFavouriteTab -> updateState { it.copy(favouriteTab = event.tab) }
+            } finally {
+                loadData()
+            }
         }
     }
 }
