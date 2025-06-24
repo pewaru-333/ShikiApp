1144a2743a5a2687fa7936bda05125a2b2edcce8
pav <pewaru@inbox.ru>
2025-06-24 19:40:15 +0300

    — В CatalogItem добавлены новые элементы (пользователи и клубы)

--- app/src/main/java/org/application/shikiapp/utils/enums/CatalogItem.kt
+++ app/src/main/java/org/application/shikiapp/utils/enums/CatalogItem.kt
@@ -3,10 +3,12 @@ package org.application.shikiapp.utils.enums
 import androidx.annotation.StringRes
 import org.application.shikiapp.R
 
-enum class CatalogItems(@StringRes val title: Int, val icon: Int) {
+enum class CatalogItem(@StringRes val title: Int, val icon: Int) {
     ANIME(R.string.text_anime, R.drawable.vector_anime),
     MANGA(R.string.text_manga, R.drawable.vector_manga),
     RANOBE(R.string.text_ranobe, R.drawable.vector_ranobe),
     CHARACTERS(R.string.text_characters, R.drawable.vector_character),
-    PEOPLE(R.string.text_people, R.drawable.vector_person)
+    PEOPLE(R.string.text_people, R.drawable.vector_person),
+    USERS(R.string.text_users, R.drawable.vector_users),
+    CLUBS(R.string.text_clubs, R.drawable.vector_clubs)
 }
