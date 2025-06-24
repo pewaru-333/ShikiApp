05d9460f968191129004e200a24ca06a4f6222c7
pav <pewaru@inbox.ru>
2025-06-24 19:39:07 +0300

    — Переработана обработка событий

--- app/src/main/java/org/application/shikiapp/models/viewModels/AnimeViewModel.kt
+++ app/src/main/java/org/application/shikiapp/models/viewModels/AnimeViewModel.kt
@@ -7,7 +7,6 @@ import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.viewModelScope
 import androidx.navigation.toRoute
 import kotlinx.coroutines.launch
-import org.application.shikiapp.events.AnimeDetailEvent
 import org.application.shikiapp.events.ContentDetailEvent
 import org.application.shikiapp.models.states.AnimeState
 import org.application.shikiapp.models.ui.Anime
@@ -18,7 +17,7 @@ import org.application.shikiapp.network.response.Response
 import org.application.shikiapp.utils.enums.LinkedType
 import org.application.shikiapp.utils.navigation.Screen
 
-class AnimeViewModel(saved: SavedStateHandle) : ContentDetailViewModel<Anime, AnimeState, AnimeDetailEvent>() {
+class AnimeViewModel(saved: SavedStateHandle) : ContentDetailViewModel<Anime, AnimeState>() {
     private val animeId = saved.toRoute<Screen.Anime>().id
 
     override fun initState() = AnimeState()
@@ -29,11 +28,12 @@ class AnimeViewModel(saved: SavedStateHandle) : ContentDetailViewModel<Anime, An
 
             try {
                 val anime = asyncLoad { GraphQL.getAnime(animeId) }
-                val animeLoaded = anime.await()
                 val similar = asyncLoad { Network.anime.getSimilar(animeId) }
                 val links = asyncLoad { Network.anime.getLinks(animeId) }
                 val stats = asyncLoad { GraphQL.getAnimeStats(animeId) }
                 val favoured = Network.anime.getAnime(animeId).favoured
+
+                val animeLoaded = anime.await()
                 val comments = getComments(animeLoaded.topic?.id?.toLong())
 
                 emit(
@@ -53,68 +53,84 @@ class AnimeViewModel(saved: SavedStateHandle) : ContentDetailViewModel<Anime, An
         }
     }
 
-    override fun onEvent(event: AnimeDetailEvent) {
+    override fun onEvent(event: ContentDetailEvent) {
         when (event) {
-            AnimeDetailEvent.Reload -> viewModelScope.launch {
-                updateState {
+            ContentDetailEvent.ShowComments -> updateState { it.copy(showComments = !it.showComments) }
+            ContentDetailEvent.ShowSheet -> updateState { it.copy(showSheet = !it.showSheet) }
+
+            is ContentDetailEvent.Media -> when (event) {
+                ContentDetailEvent.Media.Reload -> viewModelScope.launch {
+                    updateState {
+                        it.copy(
+                            showRate = !it.showRate,
+                            showSheet = !it.showSheet
+                        )
+                    }
+                    loadData()
+                }
+
+                ContentDetailEvent.Media.ShowAuthors -> updateState { it.copy(showAuthors = !it.showAuthors) }
+                ContentDetailEvent.Media.ShowCharacters -> updateState { it.copy(showCharacters = !it.showCharacters) }
+                ContentDetailEvent.Media.ShowRelated -> updateState { it.copy(showRelated = !it.showRelated) }
+                ContentDetailEvent.Media.ShowSimilar -> updateState {
+                    it.copy(
+                        showSimilar = !it.showSimilar,
+                        showSheet = !it.showSheet
+                    )
+                }
+
+                ContentDetailEvent.Media.ShowStats -> updateState {
+                    it.copy(
+                        showStats = !it.showStats,
+                        showSheet = !it.showSheet
+                    )
+                }
+
+                ContentDetailEvent.Media.ShowLinks -> updateState {
+                    it.copy(
+                        showSheet = !it.showSheet,
+                        showLinks = !it.showLinks
+                    )
+                }
+
+                ContentDetailEvent.Media.ShowRate -> updateState {
                     it.copy(
                         showRate = !it.showRate,
                         showSheet = !it.showSheet
                     )
                 }
-                loadData()
+
+                is ContentDetailEvent.Media.ShowImage -> updateState {
+                    it.copy(
+                        showScreenshot = !it.showScreenshot,
+                        screenshot = event.index
+                    )
+                }
+
+                is ContentDetailEvent.Media.SetImage -> updateState {
+                    it.copy(
+                        screenshot = event.index
+                    )
+                }
             }
 
-            ContentDetailEvent.ShowComments -> updateState { it.copy(showComments = !it.showComments) }
-            ContentDetailEvent.ShowSheet -> updateState { it.copy(showSheet = !it.showSheet) }
-            ContentDetailEvent.ShowRelated -> updateState { it.copy(showRelated = !it.showRelated) }
-            ContentDetailEvent.ShowSimilar -> updateState {
-                it.copy(
-                    showSimilar = !it.showSimilar,
-                    showSheet = !it.showSheet
+            is ContentDetailEvent.Media.Anime -> when (event) {
+                ContentDetailEvent.Media.Anime.ShowScreenshots -> updateState {
+                    it.copy(showScreenshots = !it.showScreenshots)
+                }
+
+                ContentDetailEvent.Media.Anime.ShowVideo -> updateState {
+                    it.copy(showVideo = !it.showVideo)
+                }
+
+                is ContentDetailEvent.Media.Anime.ToggleFavourite -> toggleFavourite(
+                    id = animeId,
+                    type = LinkedType.ANIME,
+                    favoured = event.favoured
                 )
             }
 
-            AnimeDetailEvent.ShowCharacters -> updateState { it.copy(showCharacters = !it.showCharacters) }
-            AnimeDetailEvent.ShowAuthors -> updateState { it.copy(showAuthors = !it.showAuthors) }
-            AnimeDetailEvent.ShowScreenshots -> updateState { it.copy(showScreenshots = !it.showScreenshots) }
-            AnimeDetailEvent.ShowVideo -> updateState { it.copy(showVideo = !it.showVideo) }
-            AnimeDetailEvent.ShowRate, ContentDetailEvent.ShowRate -> updateState {
-                it.copy(
-                    showRate = !it.showRate,
-                    showSheet = !it.showSheet
-                )
-            }
-
-
-            ContentDetailEvent.ShowStats -> updateState {
-                it.copy(
-                    showStats = !it.showStats,
-                    showSheet = !it.showSheet
-                )
-            }
-
-            ContentDetailEvent.ShowLinks -> updateState {
-                it.copy(
-                    showSheet = !it.showSheet,
-                    showLinks = !it.showLinks
-                )
-            }
-
-            is AnimeDetailEvent.ShowScreenshot -> updateState {
-                it.copy(
-                    showScreenshot = !it.showScreenshot,
-                    screenshot = event.index
-                )
-            }
-
-            is AnimeDetailEvent.SetScreenshot -> updateState { it.copy(screenshot = event.index) }
-
-            is AnimeDetailEvent.ToggleFavourite -> toggleFavourite(
-                id = animeId,
-                type = LinkedType.ANIME,
-                favoured = event.favoured
-            )
+            else -> Unit
         }
     }
 }
