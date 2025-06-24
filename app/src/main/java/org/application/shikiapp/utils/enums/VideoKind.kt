5b3a56097f8d4113a9bcc52efada4ffeca497142
pav <pewaru@inbox.ru>
2025-06-24 19:52:35 +0300

    - Переименование VideoKinds в VideoKind

--- app/src/main/java/org/application/shikiapp/utils/enums/VideoKind.kt
+++ app/src/main/java/org/application/shikiapp/utils/enums/VideoKind.kt
@@ -1,6 +1,6 @@
 package org.application.shikiapp.utils.enums
 
-enum class VideoKinds(val title: String, val kinds: List<String>) {
+enum class VideoKind(val title: String, val kinds: List<String>) {
     VIDEO("Видео", listOf("pv", "ed", "op", "op_ed_clip", "other")),
     CHARACTER("Трейлеры персонажей", listOf("character_trailer")),
     EPISODE("Превью эпизодов", listOf("episode_preview"))
