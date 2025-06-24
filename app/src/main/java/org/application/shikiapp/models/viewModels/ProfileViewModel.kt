b7a85f33244357b9134e23d0d921e600f89188e1
pav <pewaru@inbox.ru>
2025-06-24 19:49:15 +0300

    — Реализован Mutex для функции входа в профиль

--- app/src/main/java/org/application/shikiapp/models/viewModels/ProfileViewModel.kt
+++ app/src/main/java/org/application/shikiapp/models/viewModels/ProfileViewModel.kt
@@ -5,8 +5,10 @@ import androidx.lifecycle.viewModelScope
 import androidx.navigation.toRoute
 import io.ktor.http.HttpStatusCode
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.sync.Mutex
+import kotlinx.coroutines.sync.withLock
 import org.application.shikiapp.models.data.Token
-import org.application.shikiapp.models.ui.User
+import org.application.shikiapp.models.ui.mappers.mapper
 import org.application.shikiapp.network.client.Network
 import org.application.shikiapp.network.response.LoginResponse
 import org.application.shikiapp.utils.Preferences
@@ -14,76 +16,79 @@ import org.application.shikiapp.utils.navigation.Screen.Login
 import java.net.UnknownHostException
 
 class ProfileViewModel(saved: SavedStateHandle) : UserViewModel(saved) {
+    private val mutex = Mutex()
     val args = saved.toRoute<Login>()
 
     override val userId: Long
         get() = Preferences.userId
 
     override fun loadData() {
+        when {
+            Preferences.token != null -> getProfile()
+            args.code != null -> login(args.code)
+            else -> viewModelScope.launch { emit(LoginResponse.NotLogged) }
+        }
+    }
+
+    private fun getProfile() {
         viewModelScope.launch {
-            when {
-                Preferences.token != null -> getProfile()
-                args.code != null -> login(args.code)
-                else -> emit(LoginResponse.NotLogged)
+            emit(LoginResponse.Logging)
+
+            try {
+                emit(
+                    LoginResponse.Logged(
+                        user.await().mapper(
+                            clubs = clubs.await(),
+                            comments = comments,
+                            friends = friends,
+                            history = history,
+                            favourites = favourites.await()
+                        )
+                    )
+                )
+            } catch (e: Throwable) {
+                when (e) {
+                    is UnknownHostException -> emit(LoginResponse.NetworkError)
+                    else -> emit(LoginResponse.NotLogged)
+                }
             }
         }
     }
 
-    private suspend fun getProfile() {
-        emit(LoginResponse.Logging)
+    private fun login(code: String) {
+        viewModelScope.launch {
+            emit(LoginResponse.Logging)
 
-        try {
-            emit(
-                LoginResponse.Logged(
-                    User(
-                        user = user.await(),
-                        clubs = clubs.await(),
-                        comments = comments,
-                        friends = friends,
-                        history = history,
-                        favourites = favourites.await()
-                    )
-                )
-            )
-        } catch (e: Throwable) {
-            when (e) {
-                is UnknownHostException -> emit(LoginResponse.NetworkError)
-                else -> emit(LoginResponse.NotLogged)
+            try {
+                mutex.withLock(this@ProfileViewModel) {
+                    try {
+                        val token = Network.profile.getToken(code = code)
+                        Preferences.saveToken(token)
+
+                        val whoAmI = Network.profile.whoAmI()
+                        Preferences.setUserId(whoAmI.id)
+
+                        emit(
+                            LoginResponse.Logged(
+                                user.await().mapper(
+                                    clubs = clubs.await(),
+                                    comments = comments,
+                                    friends = friends,
+                                    history = history,
+                                    favourites = favourites.await()
+                                )
+                            )
+                        )
+                    } catch (_: Throwable) {
+                        Preferences.saveToken(Token.empty)
+                        Preferences.setUserId(0L)
+
+                        emit(LoginResponse.NotLogged)
+                    }
+                }
+            } catch (_: IllegalStateException) {
+                // Already logging
             }
-        }
-    }
-
-    private suspend fun login(code: String) {
-        if (response.value == LoginResponse.Logging) {
-            return
-        }
-
-        emit(LoginResponse.Logging)
-
-        try {
-            val token = Network.profile.getToken(code = code)
-            Preferences.saveToken(token)
-
-            val whoAmI = Network.profile.whoAmI()
-            Preferences.setUserId(whoAmI.id)
-
-            emit(
-                LoginResponse.Logged(
-                    User(
-                        user = user.await(),
-                        clubs = clubs.await(),
-                        comments = comments,
-                        friends = friends,
-                        history = history,
-                        favourites = favourites.await()
-                    )
-                )
-            )
-        } catch (_: Throwable) {
-            Preferences.saveToken(Token.empty)
-            Preferences.setUserId(0L)
-
-            emit(LoginResponse.NotLogged)
         }
     }
 
