e09994f0d54f5957af77a751f71930e2d17cb278
pav <pewaru@inbox.ru>
2025-07-15 15:21:04 +0300

    — Добавления запросов для взаимодействия с диалогами и сообщениями пользователя

--- app/src/main/java/org/application/shikiapp/network/calls/Profile.kt
+++ app/src/main/java/org/application/shikiapp/network/calls/Profile.kt
@@ -6,7 +6,13 @@ import io.ktor.client.request.delete
 import io.ktor.client.request.forms.submitForm
 import io.ktor.client.request.get
 import io.ktor.client.request.post
+import io.ktor.client.request.setBody
+import io.ktor.http.ContentType
+import io.ktor.http.contentType
 import io.ktor.http.parameters
+import org.application.shikiapp.models.data.Dialog
+import org.application.shikiapp.models.data.FullMessage
+import org.application.shikiapp.models.data.MessageToSend
 import org.application.shikiapp.models.data.Token
 import org.application.shikiapp.models.data.UserBrief
 import org.application.shikiapp.utils.BLANK
@@ -37,4 +43,22 @@ class Profile(private val client: HttpClient) {
 
     suspend fun deleteFavourite(linkedType: String, linkedId: Any) =
         client.delete("favorites/$linkedType/$linkedId")
+
+    suspend fun getDialogs() = client.get("dialogs").body<List<Dialog>>()
+
+    suspend fun getUserDialog(nickname: String) = client.get("dialogs/$nickname").body<List<FullMessage>>()
+
+    suspend fun deleteUserDialog(nickname: String) = client.delete("dialogs/$nickname")
+
+    suspend fun sendMessage(message: MessageToSend) = client.post("messages") {
+        contentType(ContentType.Application.Json)
+        setBody(message)
+    }
+
+//    suspend fun editMessage(id: Long, message: MessageToSend) = client.patch("messages/$id") {
+//        contentType(ContentType.Application.Json)
+//        setBody(message)
+//    }
+
+//    suspend fun deleteMessage(id: Long) = client.delete("messages/$id")
 }
