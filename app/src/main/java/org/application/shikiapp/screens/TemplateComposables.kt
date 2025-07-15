11c0558bf7612f3a6f4ce6dd68f6af4669f46269
pav <pewaru@inbox.ru>
2025-07-15 15:17:13 +0300

    — Изменение интерфейса пользователя

--- app/src/main/java/org/application/shikiapp/screens/TemplateComposables.kt
+++ app/src/main/java/org/application/shikiapp/screens/TemplateComposables.kt
@@ -62,7 +62,6 @@ import androidx.compose.material.icons.outlined.Info
 import androidx.compose.material.icons.outlined.KeyboardArrowDown
 import androidx.compose.material.icons.outlined.KeyboardArrowUp
 import androidx.compose.material3.AlertDialog
-import androidx.compose.material3.AssistChip
 import androidx.compose.material3.Button
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.CircularProgressIndicator
@@ -70,6 +69,7 @@ import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ExposedDropdownMenuBox
 import androidx.compose.material3.ExposedDropdownMenuDefaults
+import androidx.compose.material3.FilterChip
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -1468,7 +1468,8 @@ fun UserMenuItems(setMenu: (UserMenu) -> Unit) =
         UserMenu.entries.chunked(2).forEach { row ->
             Row(Modifier.fillMaxWidth(), Arrangement.spacedBy(48.dp), Alignment.CenterVertically) {
                 row.forEach { entry ->
-                    AssistChip(
+                    FilterChip(
+                        selected = true,
                         label = { Text(stringResource(entry.title)) },
                         trailingIcon = { Icon(Icons.AutoMirrored.Outlined.KeyboardArrowRight, null) },
                         onClick = { setMenu(entry) },
