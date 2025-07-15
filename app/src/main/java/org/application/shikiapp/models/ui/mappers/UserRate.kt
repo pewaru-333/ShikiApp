3bf3d5e679c47dba6452b7c98af285bbf3323e20
pav <pewaru@inbox.ru>
2025-07-15 15:18:10 +0300

    — Изменение функции маппинга

--- app/src/main/java/org/application/shikiapp/models/ui/mappers/UserRate.kt
+++ app/src/main/java/org/application/shikiapp/models/ui/mappers/UserRate.kt
@@ -1,24 +1,37 @@
 package org.application.shikiapp.models.ui.mappers
 
-import org.application.shikiapp.models.data.BaseRate
+import org.application.shikiapp.generated.UserRatesQuery
+import org.application.shikiapp.generated.type.UserRateTargetTypeEnum
+import org.application.shikiapp.models.ui.UserRate
 import org.application.shikiapp.utils.enums.Kind
 import org.application.shikiapp.utils.extensions.safeValueOf
 import org.application.shikiapp.utils.getFull
 
-fun BaseRate.mapper() = org.application.shikiapp.models.ui.UserRate(
-    id = id,
-    contentId = (anime?.id ?: manga?.id!!).toString(),
-    title = anime?.russian ?: anime?.name ?: manga?.russian ?: manga?.name!!,
-    poster = anime?.image?.original ?: manga?.image?.original!!,
-    kind = Enum.safeValueOf<Kind>(anime?.kind ?: manga?.kind).title,
+fun UserRatesQuery.Data.UserRate.mapper(type: UserRateTargetTypeEnum) = UserRate(
+    id = id.toLong(),
+    contentId = anime?.id ?: manga?.id!!,
+    title = type.let {
+        if (it == UserRateTargetTypeEnum.Anime) anime?.russian ?: anime?.name!!
+        else manga?.russian ?: manga?.name!!
+    },
+    poster = type.let {
+        if (it == UserRateTargetTypeEnum.Anime) anime?.poster?.originalUrl!!
+        else manga?.poster?.originalUrl!!
+    },
+    kind = Enum.safeValueOf<Kind>(
+        type.let {
+            if (it == UserRateTargetTypeEnum.Anime) anime?.kind?.rawValue
+            else manga?.kind?.rawValue
+        }
+    ).title,
     score = score,
     scoreString = score.let { if (it != 0) it else '-' }.toString(),
-    status = status,
+    status = status.rawValue,
     text = text,
-    episodes = episodes ?: 0,
-    fullEpisodes = getFull(anime?.episodes, anime?.status),
-    volumes = volumes ?: 0,
-    chapters = chapters ?: 0,
+    episodes = episodes,
+    fullEpisodes = getFull(anime?.episodes, anime?.status?.rawValue),
+    volumes = volumes,
+    chapters = chapters,
     rewatches = rewatches,
-    fullChapters = getFull(manga?.chapters, manga?.status),
+    fullChapters = getFull(manga?.chapters, manga?.status?.rawValue),
 )
