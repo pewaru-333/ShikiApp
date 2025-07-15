20dbdc8e710318e5db53a97b73f702b081955b0f
pav <pewaru@inbox.ru>
2025-07-15 15:13:12 +0300

    — Функции расширения для маппинга сущностей

---
+++ app/src/main/java/org/application/shikiapp/models/ui/mappers/Message.kt
@@ -0,0 +1,23 @@
+package org.application.shikiapp.models.ui.mappers
+
+import org.application.shikiapp.models.data.FullMessage
+import org.application.shikiapp.models.ui.list.Dialog
+import org.application.shikiapp.utils.convertDate
+
+fun org.application.shikiapp.models.data.Dialog.toDialog() = Dialog(
+    id = targetUser.nickname,
+    userId = targetUser.id,
+    userNickname = targetUser.nickname,
+    userAvatar = targetUser.image.x160,
+    lastMessage = message.htmlBody,
+    lastDate = convertDate(message.createdAt)
+)
+
+fun FullMessage.toDialogMessage() = Dialog(
+    id = id.toString(),
+    userId = from.id,
+    userNickname = from.nickname,
+    userAvatar = from.image.x160,
+    lastMessage = htmlBody,
+    lastDate = convertDate(createdAt)
+)
