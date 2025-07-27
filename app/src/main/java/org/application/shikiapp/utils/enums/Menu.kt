48ac181f04e2372d8531ee319cbc4eb9ff56c5a5
pav <pewaru@inbox.ru>
2025-07-27 15:57:17 +0300

    — Изменение меню нижней панели навигации

--- app/src/main/java/org/application/shikiapp/utils/enums/Menu.kt
+++ app/src/main/java/org/application/shikiapp/utils/enums/Menu.kt
@@ -5,9 +5,9 @@ import org.application.shikiapp.R
 import org.application.shikiapp.utils.navigation.Screen
 
 enum class Menu(val route: Screen, @StringRes val title: Int, val icon: Int) {
-    CATALOG(Screen.Catalog(), R.string.text_catalog, R.drawable.vector_home),
-    NEWS(Screen.News, R.string.text_news, R.drawable.vector_news),
+    CATALOG(Screen.Catalog(), R.string.text_catalog, R.drawable.vector_compass),
     CALENDAR(Screen.Calendar, R.string.text_calendar, R.drawable.vector_calendar),
-    PROFILE(Screen.Profile, R.string.text_profile, R.drawable.vector_character),
-    SETTINGS(Screen.Settings, R.string.text_settings, R.drawable.vector_settings)
+    NEWS(Screen.News, R.string.text_news, R.drawable.vector_news),
+    LISTS(Screen.UserRates(editable = true), R.string.text_lists, R.drawable.vector_bookmark),
+    PROFILE(Screen.Profile, R.string.text_profile, R.drawable.vector_character)
 }
