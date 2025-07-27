1e6350d2195164bcd43827e1c5cef14798464946
pav <pewaru@inbox.ru>
2025-07-27 15:58:20 +0300

    — Добавление типа ответа

--- app/src/main/java/org/application/shikiapp/network/response/RatesResponse.kt
+++ app/src/main/java/org/application/shikiapp/network/response/RatesResponse.kt
@@ -5,6 +5,7 @@ import org.application.shikiapp.models.ui.UserRate
 sealed interface RatesResponse {
     data object Error : RatesResponse
     data object Loading : RatesResponse
+    data object Unlogged : RatesResponse
     data object NoAccess : RatesResponse
     data class Success(val rates: List<UserRate>) : RatesResponse
 }
