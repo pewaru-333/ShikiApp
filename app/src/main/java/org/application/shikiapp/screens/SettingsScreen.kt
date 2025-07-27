1e1fdf825b80b05c4e34832cdfa53bf4394eed43
pav <pewaru@inbox.ru>
2025-07-27 15:59:42 +0300

    — Изменение экрана настроек для их отображения не в первой очереди

--- app/src/main/java/org/application/shikiapp/screens/SettingsScreen.kt
+++ app/src/main/java/org/application/shikiapp/screens/SettingsScreen.kt
@@ -1,8 +1,19 @@
 package org.application.shikiapp.screens
 
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.activity.compose.BackHandler
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.slideInHorizontally
+import androidx.compose.animation.slideOutHorizontally
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.outlined.ArrowBack
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -14,6 +25,7 @@ import androidx.compose.ui.res.stringResource
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import me.zhanghai.compose.preference.ListPreference
 import me.zhanghai.compose.preference.ProvidePreferenceLocals
+import me.zhanghai.compose.preference.createPreferenceFlow
 import me.zhanghai.compose.preference.listPreference
 import me.zhanghai.compose.preference.preferenceCategory
 import me.zhanghai.compose.preference.switchPreference
@@ -27,66 +39,86 @@ import org.application.shikiapp.utils.PREF_GROUP_APP_VIEW
 import org.application.shikiapp.utils.Preferences
 import org.application.shikiapp.utils.enums.ListView
 import org.application.shikiapp.utils.enums.Theme
-import org.application.shikiapp.utils.extensions.getPreferenceFlow
 import org.application.shikiapp.utils.extensions.valueToText
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun SettingsScreen() {
+fun SettingsScreen(visible: Boolean, onBack: () -> Unit) {
     val context = LocalContext.current
 
-    ProvidePreferenceLocals(Preferences.app.getPreferenceFlow()) {
-        LazyColumn(Modifier.fillMaxSize()) {
-            preferenceCategory(
-                key = PREF_GROUP_APP_VIEW,
-                title = { Text(stringResource(R.string.preference_category_app_view)) }
-            )
-
-            item {
-                var value by remember { mutableStateOf(Preferences.listView) }
-
-                ListPreference(
-                    value = value,
-                    onValueChange = { value = it; Preferences.listView = it },
-                    values = ListView.entries,
-                    title = { Text(stringResource(R.string.preference_list_view)) },
-                    summary = { Text(stringResource(value.title)) },
-                    valueToText = { it.title.valueToText(context) }
+    BackHandler(onBack = onBack)
+    AnimatedVisibility(
+        visible = visible,
+        enter = slideInHorizontally(initialOffsetX = { it }),
+        exit = slideOutHorizontally(targetOffsetX = { it })
+    ) {
+        Scaffold(
+            topBar = {
+                TopAppBar(
+                    title = { Text(stringResource(R.string.text_settings)) },
+                    navigationIcon = {
+                        IconButton(onBack) {
+                            Icon(Icons.AutoMirrored.Outlined.ArrowBack, null)
+                        }
+                    }
                 )
             }
+        ) { values ->
+            ProvidePreferenceLocals(createPreferenceFlow(Preferences.app)) {
+                LazyColumn(Modifier.padding(values)) {
+                    preferenceCategory(
+                        key = PREF_GROUP_APP_VIEW,
+                        title = { Text(stringResource(R.string.preference_category_app_view)) }
+                    )
 
-            preferenceCategory(
-                key = PREF_GROUP_APP_SYSTEM,
-                title = { Text(stringResource(R.string.preference_category_system)) }
-            )
+                    item {
+                        var value by remember { mutableStateOf(Preferences.listView) }
 
-            item {
-                val value by Preferences.theme.collectAsStateWithLifecycle()
+                        ListPreference(
+                            value = value,
+                            onValueChange = { value = it; Preferences.listView = it },
+                            values = ListView.entries,
+                            title = { Text(stringResource(R.string.preference_list_view)) },
+                            summary = { Text(stringResource(value.title)) },
+                            valueToText = { it.title.valueToText(context) }
+                        )
+                    }
 
-                ListPreference(
-                    value = value,
-                    onValueChange = Preferences::setTheme,
-                    values = Theme.entries,
-                    title = { Text(stringResource(R.string.preference_theme)) },
-                    summary = { Text(stringResource(value.title)) },
-                    valueToText = { it.title.valueToText(context) }
-                )
+                    preferenceCategory(
+                        key = PREF_GROUP_APP_SYSTEM,
+                        title = { Text(stringResource(R.string.preference_category_system)) }
+                    )
+
+                    item {
+                        val value by Preferences.theme.collectAsStateWithLifecycle()
+
+                        ListPreference(
+                            value = value,
+                            onValueChange = Preferences::setTheme,
+                            values = Theme.entries,
+                            title = { Text(stringResource(R.string.preference_theme)) },
+                            summary = { Text(stringResource(value.title)) },
+                            valueToText = { it.title.valueToText(context) }
+                        )
+                    }
+
+                    switchPreference(
+                        key = PREF_DYNAMIC_COLORS,
+                        defaultValue = false,
+                        title = { Text(stringResource(R.string.preference_dynamic_colors)) },
+                        enabled = { isDynamicColorAvailable() }
+                    )
+
+                    listPreference(
+                        key = PREF_APP_CACHE,
+                        defaultValue = CACHE_LIST[0],
+                        values = CACHE_LIST,
+                        title = { Text(stringResource(R.string.preference_cache_size)) },
+                        summary = { Text(stringResource(R.string.preference_cache_size_mb, it)) },
+                        valueToText = { it.valueToText(context, R.string.preference_cache_size_mb) }
+                    )
+                }
             }
-
-            switchPreference(
-                key = PREF_DYNAMIC_COLORS,
-                defaultValue = false,
-                title = { Text(stringResource(R.string.preference_dynamic_colors)) },
-                enabled = { isDynamicColorAvailable() }
-            )
-
-            listPreference(
-                key = PREF_APP_CACHE,
-                defaultValue = CACHE_LIST[0],
-                values = CACHE_LIST,
-                title = { Text(stringResource(R.string.preference_cache_size)) },
-                summary = { Text(stringResource(R.string.preference_cache_size_mb, it)) },
-                valueToText = { it.valueToText(context, R.string.preference_cache_size_mb) }
-            )
         }
     }
 }
