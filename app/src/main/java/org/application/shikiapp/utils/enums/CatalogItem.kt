a80c5623a03e49c379a41295e6a5bae93c1b4f5a
pav <pewaru@inbox.ru>
2025-07-27 15:53:52 +0300

    — Добавление логики навигации

--- app/src/main/java/org/application/shikiapp/utils/enums/CatalogItem.kt
+++ app/src/main/java/org/application/shikiapp/utils/enums/CatalogItem.kt
@@ -2,13 +2,30 @@ package org.application.shikiapp.utils.enums
 
 import androidx.annotation.StringRes
 import org.application.shikiapp.R
+import org.application.shikiapp.utils.navigation.Screen
 
 enum class CatalogItem(@StringRes val title: Int, val icon: Int) {
-    ANIME(R.string.text_anime, R.drawable.vector_anime),
-    MANGA(R.string.text_manga, R.drawable.vector_manga),
-    RANOBE(R.string.text_ranobe, R.drawable.vector_ranobe),
-    CHARACTERS(R.string.text_characters, R.drawable.vector_character),
-    PEOPLE(R.string.text_people, R.drawable.vector_person),
-    USERS(R.string.text_users, R.drawable.vector_users),
-    CLUBS(R.string.text_clubs, R.drawable.vector_clubs)
+    ANIME(R.string.text_anime, R.drawable.vector_anime) {
+        override fun navigateTo(contentId: String) = Screen.Anime(contentId)
+    },
+    MANGA(R.string.text_manga, R.drawable.vector_manga) {
+        override fun navigateTo(contentId: String) = Screen.Manga(contentId)
+    },
+    RANOBE(R.string.text_ranobe, R.drawable.vector_ranobe) {
+        override fun navigateTo(contentId: String) = Screen.Manga(contentId)
+    },
+    CHARACTERS(R.string.text_characters, R.drawable.vector_character) {
+        override fun navigateTo(contentId: String) = Screen.Character(contentId)
+    },
+    PEOPLE(R.string.text_people, R.drawable.vector_person) {
+        override fun navigateTo(contentId: String) = Screen.Person(contentId.toLong())
+    },
+    USERS(R.string.text_users, R.drawable.vector_users) {
+        override fun navigateTo(contentId: String) = Screen.User(contentId.toLong())
+    },
+    CLUBS(R.string.text_clubs, R.drawable.vector_clubs) {
+        override fun navigateTo(contentId: String) = Screen.Club(contentId.toLong())
+    };
+
+    abstract fun navigateTo(contentId: String): Screen
 }
