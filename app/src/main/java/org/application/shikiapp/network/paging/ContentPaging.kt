8ef722ad9fcfc0097b6775cf957ddb108545d0b3
pav <pewaru@inbox.ru>
2025-07-28 15:02:04 +0300

    — Замена лямбды на простой объект

--- app/src/main/java/org/application/shikiapp/network/paging/ContentPaging.kt
+++ app/src/main/java/org/application/shikiapp/network/paging/ContentPaging.kt
@@ -5,14 +5,12 @@ import androidx.paging.PagingState
 import org.application.shikiapp.models.states.FiltersState
 
 class ContentPaging<T : Any>(
-    private val filters: () -> FiltersState,
+    private val filters: FiltersState,
     private val fetch: suspend (filters: FiltersState, page: Int, params: LoadParams<Int>) -> List<T>
 ) : PagingSource<Int, T>() {
     override suspend fun load(params: LoadParams<Int>): LoadResult<Int, T> = try {
         val currentPage = params.key ?: 1
-        val currentFilters = filters()
-
-        val data = fetch(currentFilters, currentPage, params)
+        val data = fetch(filters, currentPage, params)
 
         val prevKey = if (currentPage == 1) null else currentPage - 1
         val nextKey = if (data.isEmpty()) null else currentPage + 1
