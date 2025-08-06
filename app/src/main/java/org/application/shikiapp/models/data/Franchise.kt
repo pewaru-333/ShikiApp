f6574e900f03a3fb355da463f7933d7f7468062f
pav <pewaru@inbox.ru>
2025-08-06 13:33:42 +0300

    — Новый DTO

--- app/src/main/java/org/application/shikiapp/models/data/Franchise.kt
+++ app/src/main/java/org/application/shikiapp/models/data/Franchise.kt
@@ -1,31 +1,34 @@
 package org.application.shikiapp.models.data
 
-//@Serializable
-//data class Franchise(
-//    @SerialName("links") val links: List<Link>,
-//    @SerialName("nodes") val nodes: List<Node>,
-//    @SerialName("current_id") val currentId: Long
-//)
-//
-//@Serializable
-//data class Link(
-//    @SerialName("id") val id: Long,
-//    @SerialName("source_id") val sourceId: Long,
-//    @SerialName("target_id") val targetId: Long,
-//    @SerialName("source") val source: Long,
-//    @SerialName("target") val target: Long,
-//    @SerialName("weight") val weight: Int,
-//    @SerialName("relation") val relation: String
-//)
-//
-//@Serializable
-//data class Node(
-//    @SerialName("id") val id: Long,
-//    @SerialName("date") val date: Long,
-//    @SerialName("name") val name: String,
-//    @SerialName("image_url") val imageUrl: String,
-//    @SerialName("url") val url: String,
-//    @SerialName("year") val year: Int?,
-//    @SerialName("kind") val kind: String,
-//    @SerialName("weight") val weight: Int
-//)
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class Franchise(
+    val links: List<Link>,
+    val nodes: List<Node>,
+    @SerialName("current_id") val currentId: Long
+)
+
+@Serializable
+data class Link(
+    val id: Long,
+    @SerialName("source_id") val sourceId: Long,
+    @SerialName("target_id") val targetId: Long,
+    val source: Long,
+    val target: Long,
+    val weight: Int,
+    val relation: String
+)
+
+@Serializable
+data class Node(
+    val id: Long,
+    val date: Long,
+    val name: String,
+    @SerialName("image_url") val imageUrl: String,
+    val url: String,
+    val year: Int?,
+    val kind: String?,
+    val weight: Int
+)
