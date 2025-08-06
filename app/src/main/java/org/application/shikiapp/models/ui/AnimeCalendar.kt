718331e5aea9f5f709c0bd0b4f0683736273dbf9
pav <pewaru@inbox.ru>
2025-08-06 13:23:25 +0300

    — Изменение класс отображения интерфейса

--- app/src/main/java/org/application/shikiapp/models/ui/AnimeCalendar.kt
+++ app/src/main/java/org/application/shikiapp/models/ui/AnimeCalendar.kt
@@ -2,10 +2,10 @@ package org.application.shikiapp.models.ui
 
 import androidx.paging.PagingData
 import kotlinx.coroutines.flow.Flow
+import org.application.shikiapp.models.ui.list.BasicContent
 import org.application.shikiapp.models.ui.list.Content
-import org.application.shikiapp.models.ui.list.ShortContent
 
 data class AnimeCalendar(
-    val trending: List<ShortContent>,
+    val trending: List<BasicContent>,
     val updates: Flow<PagingData<Content>>
 )
