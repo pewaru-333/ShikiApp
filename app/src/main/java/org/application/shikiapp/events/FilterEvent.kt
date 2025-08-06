af3fcb21ba2c3b829341730186ae9d30dd468359
pav <pewaru@inbox.ru>
2025-08-06 13:31:45 +0300

    — Добавление метода сброса фильтров

--- app/src/main/java/org/application/shikiapp/events/FilterEvent.kt
+++ app/src/main/java/org/application/shikiapp/events/FilterEvent.kt
@@ -4,6 +4,8 @@ import org.application.shikiapp.utils.enums.Order
 import org.application.shikiapp.utils.enums.PeopleFilterItem
 
 sealed interface FilterEvent {
+    data object ClearFilters : FilterEvent
+
     data class SetOrder(val order: Order) : FilterEvent
     data class SetStatus(val status: String) : FilterEvent
     data class SetKind(val kind: String) : FilterEvent
@@ -16,7 +18,7 @@ sealed interface FilterEvent {
     data class SetRating(val rating: String) : FilterEvent
     data class SetGenre(val genre: String) : FilterEvent
     data class SetStudio(val studio: String) : FilterEvent
-    data class SetPublisher(val studio: String) : FilterEvent
+    data class SetPublisher(val publisher: String) : FilterEvent
     data class SetFranchise(val franchise: String) : FilterEvent
     data class SetCensored(val censored: Boolean) : FilterEvent
     data class SetMyList(val myList: String) : FilterEvent
