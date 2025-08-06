d48281392616d2753f98a9a3a83daa237576c5d5
pav <pewaru@inbox.ru>
2025-08-06 13:33:28 +0300

    — Переопределение метода equals()

--- app/src/main/java/org/application/shikiapp/models/states/FiltersState.kt
+++ app/src/main/java/org/application/shikiapp/models/states/FiltersState.kt
@@ -25,4 +25,57 @@ data class FiltersState(
     val myList: String? = null,
     val roles: SnapshotStateSet<PeopleFilterItem> = mutableStateSetOf(),
     val title: String = BLANK
-)
+) {
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+        if (other !is FiltersState) return false
+
+        if (order != other.order) return false
+        if (seasonYS != other.seasonYS) return false
+        if (seasonYF != other.seasonYF) return false
+        if (score != other.score) return false
+        if (studio != other.studio) return false
+        if (publisher != other.publisher) return false
+        if (franchise != other.franchise) return false
+        if (censored != other.censored) return false
+        if (myList != other.myList) return false
+        if (title != other.title) return false
+
+        // SnapshotStateSet имеет разные ссылки
+        if (kind.toSet() != other.kind.toSet()) return false
+        if (status.toSet() != other.status.toSet()) return false
+        if (seasonS.toSet() != other.seasonS.toSet()) return false
+        if (season.toSet() != other.season.toSet()) return false
+        if (duration.toSet() != other.duration.toSet()) return false
+        if (rating.toSet() != other.rating.toSet()) return false
+        if (genres.toSet() != other.genres.toSet()) return false
+        if (roles.toSet() != other.roles.toSet()) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = order.hashCode()
+
+        result = 31 * result + kind.toSet().hashCode()
+        result = 31 * result + status.toSet().hashCode()
+        result = 31 * result + seasonYS.hashCode()
+        result = 31 * result + seasonYF.hashCode()
+        result = 31 * result + seasonS.toSet().hashCode()
+        result = 31 * result + season.toSet().hashCode()
+        result = 31 * result + score.hashCode()
+        result = 31 * result + duration.toSet().hashCode()
+        result = 31 * result + rating.toSet().hashCode()
+        result = 31 * result + genres.toSet().hashCode()
+        result = 31 * result + (studio?.hashCode() ?: 0)
+        result = 31 * result + (publisher?.hashCode() ?: 0)
+        result = 31 * result + (franchise?.hashCode() ?: 0)
+        result = 31 * result + (censored?.hashCode() ?: 0)
+        result = 31 * result + (myList?.hashCode() ?: 0)
+        result = 31 * result + roles.toSet().hashCode()
+        result = 31 * result + title.hashCode()
+
+        return result
+    }
+}
