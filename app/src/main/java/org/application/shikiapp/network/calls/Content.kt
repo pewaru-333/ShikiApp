119f369ed7e2d3ecfb3cfc7afc612fae2942baf0
pav <pewaru@inbox.ru>
2025-08-06 13:31:57 +0300

    — Рефакторинг кода

--- app/src/main/java/org/application/shikiapp/network/calls/Content.kt
+++ app/src/main/java/org/application/shikiapp/network/calls/Content.kt
@@ -3,12 +3,11 @@ package org.application.shikiapp.network.calls
 import io.ktor.client.HttpClient
 import io.ktor.client.call.body
 import io.ktor.client.request.get
-import org.application.shikiapp.models.data.Calendar
 import org.application.shikiapp.models.data.Character
 import org.application.shikiapp.models.data.Person
 
 class Content(private val client: HttpClient) {
-    suspend fun getCalendar() = client.get("calendar").body<List<Calendar>>()
+    //suspend fun getCalendar() = client.get("calendar").body<List<Calendar>>()
     suspend fun getCharacter(id: Any) = client.get("characters/$id").body<Character>()
     suspend fun getPerson(id: Any) = client.get("people/$id").body<Person>()
 }
