54abbc82fc867ae04b2065f07ab71ccc7e10dcf7
pav <pewaru@inbox.ru>
2025-08-06 13:35:26 +0300

    — Добавление методов получения цветов

--- app/src/main/java/org/application/shikiapp/utils/enums/Status.kt
+++ app/src/main/java/org/application/shikiapp/utils/enums/Status.kt
@@ -1,6 +1,7 @@
 package org.application.shikiapp.utils.enums
 
 import androidx.annotation.StringRes
+import androidx.compose.ui.graphics.Color
 import org.application.shikiapp.R
 
 enum class Status(
@@ -32,5 +33,25 @@ enum class Status(
         types = listOf(LinkedType.MANGA, LinkedType.RANOBE),
         animeTitle = null,
         mangaTitle = R.string.status_paused_discontinued
-    )
+    );
+
+    @StringRes
+    fun getTitle(kind: Kind) = if (kind.linkedType == LinkedType.ANIME) animeTitle ?: R.string.text_unknown
+    else mangaTitle
 }
+
+val Status.backgroundColor: Color
+    get() = when (this) {
+        Status.ANONS -> Color(0xFFF28F3B)
+        Status.ONGOING -> Color(0xFFC6E0FF)
+        Status.RELEASED -> Color(0xFF0CF574)
+        Status.PAUSED -> Color(0xFFAEB7AF)
+        Status.DISCONTINUED -> Color(0xFFEF2D56)
+    }
+
+val Status.textColor: Color
+    get() = when (this) {
+        Status.ANONS, Status.RELEASED, Status.PAUSED -> Color.Black
+        Status.ONGOING -> Color(0xFF0E2239)
+        Status.DISCONTINUED -> Color(0xFFEFEFEF)
+    }
