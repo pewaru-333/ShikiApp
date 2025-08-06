5c593c2e3dee78b08805b4528756eb87b6b7b86d
pav <pewaru@inbox.ru>
2025-08-06 13:20:12 +0300

    — Добавление запроса на получение франшизы

--- app/src/main/java/org/application/shikiapp/network/calls/Anime.kt
+++ app/src/main/java/org/application/shikiapp/network/calls/Anime.kt
@@ -5,10 +5,10 @@ import io.ktor.client.call.body
 import io.ktor.client.request.get
 import org.application.shikiapp.models.data.Anime
 import org.application.shikiapp.models.data.AnimeBasic
-import org.application.shikiapp.models.data.ExternalLink
+import org.application.shikiapp.models.data.Franchise
 
 class Anime(private val client: HttpClient) {
     suspend fun getAnime(id: String) = client.get("animes/$id").body<Anime>()
     suspend fun getSimilar(id: String) = client.get("animes/$id/similar").body<List<AnimeBasic>>()
-    suspend fun getLinks(id: String) = client.get("animes/$id/external_links").body<List<ExternalLink>>()
+    suspend fun getFranchise(id: String) = client.get("animes/$id/franchise").body<Franchise>()
 }
--- app/src/main/java/org/application/shikiapp/network/calls/Manga.kt
+++ app/src/main/java/org/application/shikiapp/network/calls/Manga.kt
@@ -3,12 +3,12 @@ package org.application.shikiapp.network.calls
 import io.ktor.client.HttpClient
 import io.ktor.client.call.body
 import io.ktor.client.request.get
-import org.application.shikiapp.models.data.ExternalLink
+import org.application.shikiapp.models.data.Franchise
 import org.application.shikiapp.models.data.Manga
 import org.application.shikiapp.models.data.MangaBasic
 
 class Manga(private val client: HttpClient) {
     suspend fun getManga(id: Any) = client.get("mangas/$id").body<Manga>()
     suspend fun getSimilar(id: Any) = client.get("mangas/$id/similar").body<List<MangaBasic>>()
-    suspend fun getLinks(id: Any) = client.get("mangas/$id/external_links").body<List<ExternalLink>>()
+    suspend fun getFranchise(id: Any) = client.get("mangas/$id/franchise").body<Franchise>()
 }
