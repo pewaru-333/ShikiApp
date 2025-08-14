3948bfe0c1cc3e279f4635019fe821a0d19c84da
pav <pewaru@inbox.ru>
2025-08-14 15:16:21 +0300

    — Новый запросы для получения оценок пользователя

--- app/src/main/java/org/application/shikiapp/network/calls/UserRates.kt
+++ app/src/main/java/org/application/shikiapp/network/calls/UserRates.kt
@@ -1,15 +1,29 @@
 package org.application.shikiapp.network.calls
 
 import io.ktor.client.HttpClient
+import io.ktor.client.call.body
 import io.ktor.client.request.delete
+import io.ktor.client.request.get
+import io.ktor.client.request.parameter
 import io.ktor.client.request.patch
 import io.ktor.client.request.post
 import io.ktor.client.request.setBody
 import io.ktor.http.ContentType
 import io.ktor.http.contentType
+import org.application.shikiapp.models.data.BaseRate
 import org.application.shikiapp.models.data.NewRate
 
 class UserRates(private val client: HttpClient) {
+    suspend fun getAnimeRates(id: Long, page: Int, limit: Int = 5000) = client.get("users/$id/anime_rates") {
+        parameter("page", page)
+        parameter("limit", limit)
+    }.body<List<BaseRate>?>()
+
+    suspend fun getMangaRates(id: Long, page: Int, limit: Int = 5000) = client.get("users/$id/manga_rates") {
+        parameter("page", page)
+        parameter("limit", limit)
+    }.body<List<BaseRate>?>()
+
     suspend fun createRate(newRate: NewRate) = client.post("v2/user_rates") {
         contentType(ContentType.Application.Json)
         setBody(newRate)
