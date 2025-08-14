2b6a0541dec6c7cf52c7d2325a4441d5edf5c239
pav <pewaru@inbox.ru>
2025-08-14 15:12:44 +0300

    — Изменение DTO

--- app/src/main/java/org/application/shikiapp/models/data/Calendar.kt
+++ app/src/main/java/org/application/shikiapp/models/data/Calendar.kt
@@ -3,10 +3,10 @@ package org.application.shikiapp.models.data
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
-@Serializable
-data class Calendar(
-    @SerialName("next_episode") val nextEpisode: Int,
-    @SerialName("next_episode_at") val nextEpisodeAt: String,
-    @SerialName("duration") val duration: Int?,
-    @SerialName("anime") val anime: AnimeBasic
-)
+//@Serializable
+//data class Calendar(
+//    @SerialName("next_episode") val nextEpisode: Int,
+//    @SerialName("next_episode_at") val nextEpisodeAt: String,
+//    @SerialName("duration") val duration: Int?,
+//    @SerialName("anime") val anime: AnimeBasic
+//)
