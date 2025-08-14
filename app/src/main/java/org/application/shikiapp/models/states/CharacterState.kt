89733e1715a188f02047954954034498a81ea4ea
pav <pewaru@inbox.ru>
2025-08-14 15:13:11 +0300

    — Изменение класса хранения состояния экрана

--- app/src/main/java/org/application/shikiapp/models/states/CharacterState.kt
+++ app/src/main/java/org/application/shikiapp/models/states/CharacterState.kt
@@ -1,7 +1,15 @@
+@file:OptIn(ExperimentalMaterial3Api::class)
+
 package org.application.shikiapp.models.states
 
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.SheetState
+import androidx.compose.ui.unit.Density
+
 data class CharacterState(
+    val sheetState: SheetState = SheetState(false, Density(1f)),
     val showComments: Boolean = false,
     val showRelated: Boolean = false,
-    val showSeyu: Boolean = false
+    val showSeyu: Boolean = false,
+    val showSheet: Boolean = false,
 )
