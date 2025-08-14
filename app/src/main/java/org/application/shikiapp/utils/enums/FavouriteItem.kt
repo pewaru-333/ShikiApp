e1b0d0275ea98ae6268fa1e33b6f0e514f3dfa3e
pav <pewaru@inbox.ru>
2025-08-14 15:15:53 +0300

    — Изменение enum

--- app/src/main/java/org/application/shikiapp/utils/enums/FavouriteItem.kt
+++ app/src/main/java/org/application/shikiapp/utils/enums/FavouriteItem.kt
@@ -2,21 +2,14 @@ package org.application.shikiapp.utils.enums
 
 import androidx.annotation.StringRes
 import org.application.shikiapp.R
-import org.application.shikiapp.models.data.Favourite
-import org.application.shikiapp.models.data.Favourites
 
-enum class FavouriteItem(
-    @StringRes val title: Int,
-    private val getFavourite: (Favourites) -> List<Favourite>
-) {
-    ANIME(R.string.text_anime, Favourites::animes),
-    MANGA(R.string.text_manga, Favourites::mangas),
-    RANOBE(R.string.text_ranobe, Favourites::ranobe),
-    CHARACTERS(R.string.text_characters, Favourites::characters),
-    PEOPLE(R.string.text_people, Favourites::people),
-    MANGAKAS(R.string.text_mangakas, Favourites::mangakas),
-    SEYU(R.string.text_seyu, Favourites::seyu),
-    OTHERS(R.string.text_others, Favourites::producers);
-
-    fun getFavouriteList(favourites: Favourites) = getFavourite(favourites)
+enum class FavouriteItem(@StringRes val title: Int, val linkedType: LinkedType) {
+    ANIME(R.string.text_anime, LinkedType.ANIME),
+    MANGA(R.string.text_manga, LinkedType.MANGA),
+    RANOBE(R.string.text_ranobe, LinkedType.RANOBE),
+    CHARACTERS(R.string.text_characters, LinkedType.CHARACTER),
+    PEOPLE(R.string.text_people, LinkedType.PERSON),
+    MANGAKAS(R.string.text_mangakas, LinkedType.PERSON),
+    SEYU(R.string.text_seyu, LinkedType.PERSON),
+    OTHERS(R.string.text_others, LinkedType.PERSON)
 }
