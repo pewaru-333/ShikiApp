778d66dea589f8c5e15215eeead36fcf20398256
pav <pewaru@inbox.ru>
2025-08-14 15:12:30 +0300

    — Изменение данных для отображения на экране календаря

--- app/src/main/java/org/application/shikiapp/models/ui/AnimeCalendar.kt
+++ app/src/main/java/org/application/shikiapp/models/ui/AnimeCalendar.kt
@@ -2,10 +2,10 @@ package org.application.shikiapp.models.ui
 
 import androidx.paging.PagingData
 import kotlinx.coroutines.flow.Flow
-import org.application.shikiapp.models.ui.list.BasicContent
 import org.application.shikiapp.models.ui.list.Content
 
 data class AnimeCalendar(
-    val trending: List<BasicContent>,
+    val trending: List<Content>,
+    val random: List<Content>,
     val updates: Flow<PagingData<Content>>
 )
