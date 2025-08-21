d9f00742b23e7232a646e08df88c47acd21e1b93
pav <pewaru@inbox.ru>
2025-08-21 17:16:49 +0300

    — Замена текста на строковые ресурсы

--- app/src/main/java/org/application/shikiapp/utils/enums/VideoKind.kt
+++ app/src/main/java/org/application/shikiapp/utils/enums/VideoKind.kt
@@ -1,7 +1,10 @@
 package org.application.shikiapp.utils.enums
 
-enum class VideoKind(val title: String, val kinds: List<String>) {
-    VIDEO("Видео", listOf("pv", "ed", "op", "op_ed_clip", "other")),
-    CHARACTER("Трейлеры персонажей", listOf("character_trailer")),
-    EPISODE("Превью эпизодов", listOf("episode_preview"))
+import androidx.annotation.StringRes
+import org.application.shikiapp.R
+
+enum class VideoKind(@StringRes val title: Int, val kinds: List<String>) {
+    VIDEO(R.string.text_video, listOf("pv", "ed", "op", "op_ed_clip", "other")),
+    CHARACTER(R.string.text_video_character_trailers, listOf("character_trailer")),
+    EPISODE(R.string.text_video_episodes_preview, listOf("episode_preview"))
 }
