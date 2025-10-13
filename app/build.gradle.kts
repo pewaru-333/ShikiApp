22e384949a610776341088b801b4dab0d6afa6c1
pav <pewaru@inbox.ru>
2025-10-13 16:55:07 +0300

    — Версия alpha-0.4.4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,8 +24,8 @@ android {
         applicationId = "org.application.shikiapp"
         minSdk = 26
         targetSdk = 36
-        versionCode = 28
-        versionName = "alpha-0.4.3"
+        versionCode = 29
+        versionName = "alpha-0.4.4"
     }
 
     dependenciesInfo {
