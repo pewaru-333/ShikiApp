4cea4bf2b0d26aed9d4fad07e5db22a62acd30ee
pav <pewaru@inbox.ru>
2025-10-30 16:22:15 +0300

    — Обновление mapper'а

--- app/src/main/java/org/application/shikiapp/models/ui/mappers/Message.kt
+++ app/src/main/java/org/application/shikiapp/models/ui/mappers/Message.kt
@@ -1,24 +1,41 @@
 package org.application.shikiapp.models.ui.mappers
 
+import org.application.shikiapp.di.Preferences
 import org.application.shikiapp.models.data.FullMessage
 import org.application.shikiapp.models.ui.list.Dialog
+import org.application.shikiapp.models.ui.list.Message
+import org.application.shikiapp.network.response.AsyncData
 import org.application.shikiapp.utils.HtmlParser
 import org.application.shikiapp.utils.convertDate
 
 fun org.application.shikiapp.models.data.Dialog.toDialog() = Dialog(
-    id = targetUser.nickname,
+    id = message.id,
     userId = targetUser.id,
     userNickname = targetUser.nickname,
     userAvatar = targetUser.image.x160,
     lastMessage = HtmlParser.parseComment(message.htmlBody),
-    lastDate = convertDate(message.createdAt)
+    lastDate = convertDate(message.createdAt),
+    accountUser = true
 )
 
 fun FullMessage.toDialogMessage() = Dialog(
-    id = id.toString(),
+    id = id,
     userId = from.id,
     userNickname = from.nickname,
     userAvatar = from.image.x160,
     lastMessage = HtmlParser.parseComment(htmlBody),
-    lastDate = convertDate(createdAt)
+    lastDate = convertDate(createdAt),
+    accountUser = from.id == Preferences.userId
+)
+
+fun FullMessage.toNewsMessage() = Message(
+    id = id,
+    kind = kind,
+    read = AsyncData.Success(read),
+    body = HtmlParser.parseComment(htmlBody),
+    linked = linked?.toRelated(),
+    from = from,
+    to = to,
+    createdAt = convertDate(createdAt),
+    isDeleting = AsyncData.Success(false)
 )
