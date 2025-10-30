8789879e1ea39649d303ab358774c354daa974bd
pav <pewaru@inbox.ru>
2025-10-30 16:24:07 +0300

    — Обновление модели состояния почты пользователя

--- app/src/main/java/org/application/shikiapp/models/states/UserMessagesState.kt
+++ app/src/main/java/org/application/shikiapp/models/states/UserMessagesState.kt
@@ -1,13 +1,13 @@
 package org.application.shikiapp.models.states
 
-import org.application.shikiapp.utils.BLANK
+import org.application.shikiapp.network.response.AsyncData
+import org.application.shikiapp.utils.enums.MessageType
 
 data class UserMessagesState(
-    val dialogId: String? = null,
-    val toDeleteId: String? = null,
-    val userId: Long = 0L,
-    val text: String = BLANK
+    val messageType: MessageType = MessageType.INBOX,
+    val userId: Long = -1L,
+    val userNickname: AsyncData<String> = AsyncData.Loading,
+    val userAvatar: AsyncData<String> = AsyncData.Loading,
+    val isFromList: Boolean = false,
+    val showDeleteAll: Boolean = false
 )
-
-val UserMessagesState.showDialogDelete: Boolean
-    get() = toDeleteId != null
