2627f5ebfb283cdd6a32959e182c783a2c81304f
pav <pewaru@inbox.ru>
2025-10-30 16:21:48 +0300

    — Обновление модели сообщения

--- app/src/main/java/org/application/shikiapp/models/ui/list/Dialog.kt
+++ app/src/main/java/org/application/shikiapp/models/ui/list/Dialog.kt
@@ -3,10 +3,13 @@ package org.application.shikiapp.models.ui.list
 import org.application.shikiapp.utils.CommentContent
 
 data class Dialog(
-    val id: String,
+    val id: Long,
     val userId: Long,
     val userNickname: String,
     val userAvatar: String,
     val lastMessage: List<CommentContent>,
-    val lastDate: String
+    val lastDate: String,
+    val accountUser: Boolean,
+    val isSending: Boolean = false,
+    val isError: Boolean = false
 )
