e133a4f5611eae000f6b3dd708299cd379f4425d
pav <pewaru@inbox.ru>
2025-11-19 12:33:47 +0300

    — Добавленеи функции расширения

--- app/src/main/java/org/application/shikiapp/utils/extensions/Flow.kt
+++ app/src/main/java/org/application/shikiapp/utils/extensions/Flow.kt
@@ -1,11 +1,14 @@
 package org.application.shikiapp.utils.extensions
 
+import androidx.paging.PagingData
+import androidx.paging.filter
 import com.apollographql.apollo.api.ApolloResponse
 import com.apollographql.apollo.api.Operation
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.filterNotNull
 import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.mapLatest
 
 fun <T> Flow<T>.pairwise(): Flow<Pair<T, T>> = flow {
@@ -13,6 +16,16 @@ fun <T> Flow<T>.pairwise(): Flow<Pair<T, T>> = flow {
     collect { value ->
         previous?.let { emit(it to value) }
         previous = value
+    }
+}
+
+fun <T : Any, K> Flow<PagingData<T>>.distinctBy(selector: (T) -> K): Flow<PagingData<T>> {
+    val set = hashSetOf<K>()
+
+    return map { pagingData ->
+        pagingData.filter { item ->
+            set.add(selector(item))
+        }
     }
 }
 
