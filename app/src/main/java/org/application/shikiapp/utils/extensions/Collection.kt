2bb62929fa922e4b9f63d2e2c66dcd9433b7c41e
pav <pewaru@inbox.ru>
2025-11-19 12:33:34 +0300

    — Добавленеи функции расширения

--- app/src/main/java/org/application/shikiapp/utils/extensions/Collection.kt
+++ app/src/main/java/org/application/shikiapp/utils/extensions/Collection.kt
@@ -1,4 +1,6 @@
 package org.application.shikiapp.utils.extensions
 
+fun <T> Collection<T>.commaJoin() = joinToString(",")
+
 fun <E> MutableCollection<E>.toggle(element: E) = if (element in this) remove(element)
 else add(element)
