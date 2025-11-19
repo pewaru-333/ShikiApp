48437966fbb9e5b8c085a66ac8f147eabf3bc81d
pav <pewaru@inbox.ru>
2025-11-19 12:37:53 +0300

    — Изменение логики получения информации и фильтрации тем

--- app/src/main/java/org/application/shikiapp/models/viewModels/CalendarViewModel.kt
+++ app/src/main/java/org/application/shikiapp/models/viewModels/CalendarViewModel.kt
@@ -5,19 +5,26 @@ import androidx.paging.Pager
 import androidx.paging.PagingConfig
 import androidx.paging.PagingData
 import androidx.paging.cachedIn
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.async
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.flow.flowOn
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.retryWhen
 import kotlinx.coroutines.launch
 import org.application.shikiapp.models.data.Topic
 import org.application.shikiapp.models.ui.AnimeCalendar
+import org.application.shikiapp.models.ui.list.Content
 import org.application.shikiapp.models.ui.mappers.toAnimeContent
 import org.application.shikiapp.network.client.GraphQL
 import org.application.shikiapp.network.client.Network
 import org.application.shikiapp.network.paging.CommonPaging
 import org.application.shikiapp.network.response.Response
+import org.application.shikiapp.utils.extensions.distinctBy
 
 class CalendarViewModel : BaseViewModel<AnimeCalendar, Unit, Unit>() {
+    override val contentId = Any()
+
     override fun initState() = Unit
 
     override fun onEvent(event: Unit) = Unit
@@ -31,9 +38,12 @@ class CalendarViewModel : BaseViewModel<AnimeCalendar, Unit, Unit>() {
             }
 
             try {
-                val trending = async { GraphQL.getTrending() }
-                val random = async { GraphQL.getRandom() }
-                val topicsUpdates = getTopicsUpdates()
+                val (trending, random) = coroutineScope {
+                    val trending = async { GraphQL.getTrending() }
+                    val random = async { GraphQL.getRandom() }
+
+                    Pair(trending, random)
+                }
 
                 emit(
                     Response.Success(
@@ -51,14 +61,11 @@ class CalendarViewModel : BaseViewModel<AnimeCalendar, Unit, Unit>() {
     }
 
     fun reload() {
-        viewModelScope.launch {
-            emit(Response.Loading)
-
-            loadData()
-        }
+        tryEmit(Response.Loading)
+        loadData()
     }
 
-    private fun getTopicsUpdates() = Pager(
+    private val topicsUpdates = Pager(
         config = PagingConfig(
             pageSize = 20,
             enablePlaceholders = false
@@ -70,6 +77,8 @@ class CalendarViewModel : BaseViewModel<AnimeCalendar, Unit, Unit>() {
         }
     ).flow
         .map(PagingData<Topic>::toAnimeContent)
+        .distinctBy(Content::id)
+        .flowOn(Dispatchers.Default)
         .cachedIn(viewModelScope)
         .retryWhen { _, attempt -> attempt <= 3 }
 }
