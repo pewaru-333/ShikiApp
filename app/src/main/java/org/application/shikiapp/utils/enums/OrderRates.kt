19ac8b353b2ff86c716d007e4cbfedaddc00aa84
pav <email@inbox.ru>
2026-01-22 10:06:10 +0300

    — Исправление опечатки в названии перечисления

--- app/src/main/java/org/application/shikiapp/utils/enums/OrderRates.kt
+++ app/src/main/java/org/application/shikiapp/utils/enums/OrderRates.kt
@@ -9,5 +9,5 @@ enum class OrderRates(@StringRes val title: Int, @StringRes val titleManga: Int?
     EPISODES(R.string.order_episodes, R.string.order_chapters),
     KIND(R.string.order_kind),
     CREATED_AT(R.string.order_created_at),
-    UPDATE_AT(R.string.order_updated_at)
+    UPDATED_AT(R.string.order_updated_at)
 }
