15463e78cc3b4c1fcd49148cbdba6308040a1340
pav <email@inbox.ru>
2026-01-22 10:11:48 +0300

    — Обновление Gradle до 9-й версии

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,5 @@ plugins {
     alias(libs.plugins.android) apply false
     alias(libs.plugins.apollo) apply false
     alias(libs.plugins.compose) apply false
-    alias(libs.plugins.kotlin) apply false
     alias(libs.plugins.kotlin.serialization) apply false
 }
