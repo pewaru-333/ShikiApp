b19541c01f80fb8a9b8a93e930994651c794b923
pav <email@inbox.ru>
2026-01-26 09:41:49 +0300

    — Изменение UI-модели

--- app/src/main/java/org/application/shikiapp/models/ui/AnimeCalendar.kt
+++ app/src/main/java/org/application/shikiapp/models/ui/AnimeCalendar.kt
@@ -1,11 +1,19 @@
 package org.application.shikiapp.models.ui
 
+import androidx.compose.runtime.Stable
 import androidx.paging.PagingData
 import kotlinx.coroutines.flow.Flow
 import org.application.shikiapp.models.ui.list.Content
 
+@Stable
 data class AnimeCalendar(
     val trending: List<Content>,
     val random: List<Content>,
+    val schedule: List<Schedule>,
     val updates: Flow<PagingData<Content>>
-)
+) {
+    data class Schedule(
+        val date: String,
+        val animes: List<Content>
+    )
+}
