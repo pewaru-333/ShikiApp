97f2146470144fa15d2225d1214ca9c892499238
pav <email@inbox.ru>
2026-01-26 09:42:08 +0300

    — Добавление запроса

--- app/src/main/java/org/application/shikiapp/network/calls/Content.kt
+++ app/src/main/java/org/application/shikiapp/network/calls/Content.kt
@@ -1,12 +1,15 @@
 package org.application.shikiapp.network.calls
 
 import io.ktor.client.HttpClient
+import io.ktor.client.call.body
+import io.ktor.client.request.get
+import org.application.shikiapp.models.data.Calendar
 import org.application.shikiapp.models.data.Character
 import org.application.shikiapp.models.data.Person
 import org.application.shikiapp.utils.extensions.requestWithCache
 
 class Content(private val client: HttpClient) {
-    //suspend fun getCalendar() = client.get("calendar").body<List<Calendar>>()
+    suspend fun getCalendar() = client.get("calendar").body<List<Calendar>>()
 
     suspend fun getCharacter(id: Any) = client.requestWithCache<Character>(
         cacheKey = "character:$id",
