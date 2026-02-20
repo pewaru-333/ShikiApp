b78c6cee0d4c52738d04ff44a39f2cb95beb3144
pav <email@inbox.ru>
2026-02-20 16:06:19 +0300

    — Рефакторинг кода

--- app/src/main/java/org/application/shikiapp/ui/templates/Icons.kt
+++ app/src/main/java/org/application/shikiapp/ui/templates/Icons.kt
@@ -17,38 +17,55 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.vectorResource
-import androidx.paging.LoadState
-import androidx.paging.compose.LazyPagingItems
 import org.application.shikiapp.R
-import org.application.shikiapp.models.ui.Comment
 
 @Composable
-fun IconComment(comments: LazyPagingItems<Comment>, onEvent: () -> Unit) {
-    val enabledIcon = IconButtonDefaults.iconButtonColors().contentColor
-    val disabledIcon = IconButtonDefaults.iconButtonColors().disabledContentColor
-
-    val isLoading = comments.loadState.refresh is LoadState.Loading
-    val isEnabled = !isLoading && comments.itemCount > 0
+fun IconComment(onLoadState: () -> Pair<Boolean, Int>, onEvent: () -> Unit) {
+    val (isLoading, itemCount) = onLoadState()
+    val isEnabled = itemCount > 0
 
     val infiniteTransition = rememberInfiniteTransition()
-    val fillPercentage by infiniteTransition.animateFloat(
-        initialValue = 0f,
+    val alpha by infiniteTransition.animateFloat(
+        initialValue = 0.4f,
         targetValue = 1f,
         animationSpec = infiniteRepeatable(
-            animation = tween(durationMillis = 1500, easing = LinearEasing),
-            repeatMode = RepeatMode.Restart
+            animation = tween(durationMillis = 1000, easing = LinearEasing),
+            repeatMode = RepeatMode.Reverse
         )
     )
 
-    IconButton(onEvent, Modifier, isEnabled) {
-        VectorIcon(
-            resId = R.drawable.vector_comments,
-            tint = when {
-                isLoading -> enabledIcon.copy(alpha = fillPercentage)
-                !isLoading && isEnabled -> enabledIcon
-                else -> disabledIcon
+    when {
+        isLoading -> {
+            IconButton(
+                onClick = {},
+                enabled = false,
+                content = {
+                    VectorIcon(
+                        resId = R.drawable.vector_comments,
+                        tint = LocalContentColor.current.copy(alpha = alpha)
+                    )
+                }
+            )
+        }
+
+        isEnabled -> {
+            IconButton(onEvent) {
+                VectorIcon(R.drawable.vector_comments)
             }
-        )
+        }
+
+        else -> {
+            IconButton(
+                onClick = {},
+                enabled = false,
+                content = {
+                    VectorIcon(
+                        resId = R.drawable.vector_comments,
+                        tint = IconButtonDefaults.iconButtonColors().disabledContentColor
+                    )
+                }
+            )
+        }
     }
 }
 
