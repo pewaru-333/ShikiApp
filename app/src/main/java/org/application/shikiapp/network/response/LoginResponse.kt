8ec2faff407903f3e6a66fe069420b05c1ce294a
pav <email@inbox.ru>
2026-02-20 15:59:29 +0300

    — Рефакторинг кода

--- app/src/main/java/org/application/shikiapp/network/response/LoginResponse.kt
+++ app/src/main/java/org/application/shikiapp/network/response/LoginResponse.kt
@@ -2,7 +2,7 @@ package org.application.shikiapp.network.response
 
 import org.application.shikiapp.models.ui.User
 
-sealed interface LoginResponse : Response<User, Throwable> {
+sealed interface LoginResponse : Response<User, Exception> {
     data object NotLogged : LoginResponse
     data object Logging : LoginResponse
     data object NetworkError : LoginResponse
