0465e45f2c6ba973e34a4252cd48e49deeea65cb
pav <email@inbox.ru>
2026-02-20 16:02:41 +0300

    — Изменение метода обновления токена

--- app/src/main/java/org/application/shikiapp/network/calls/Profile.kt
+++ app/src/main/java/org/application/shikiapp/network/calls/Profile.kt
@@ -2,6 +2,7 @@ package org.application.shikiapp.network.calls
 
 import io.ktor.client.HttpClient
 import io.ktor.client.call.body
+import io.ktor.client.request.HttpRequestBuilder
 import io.ktor.client.request.delete
 import io.ktor.client.request.forms.submitForm
 import io.ktor.client.request.get
@@ -9,8 +10,15 @@ import io.ktor.client.request.parameter
 import io.ktor.client.request.post
 import io.ktor.client.request.setBody
 import io.ktor.http.ContentType
+import io.ktor.http.HttpStatusCode
 import io.ktor.http.contentType
+import io.ktor.http.isSuccess
 import io.ktor.http.parameters
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.sync.Mutex
+import kotlinx.coroutines.sync.withLock
+import kotlinx.io.IOException
+import org.application.shikiapp.di.Preferences
 import org.application.shikiapp.models.data.Dialog
 import org.application.shikiapp.models.data.FullMessage
 import org.application.shikiapp.models.data.MessageToSend
@@ -22,11 +30,14 @@ import org.application.shikiapp.utils.CLIENT_ID
 import org.application.shikiapp.utils.CLIENT_SECRET
 import org.application.shikiapp.utils.GRANT_TYPE
 import org.application.shikiapp.utils.REDIRECT_URI
+import org.application.shikiapp.utils.REFRESH_TOKEN
 import org.application.shikiapp.utils.TOKEN_URL
 import org.application.shikiapp.utils.enums.MessageType
 import org.application.shikiapp.utils.extensions.requestWithCache
 
 class Profile(private val client: HttpClient) {
+    private val mutex = Mutex()
+
     suspend fun getToken(code: String) = client.submitForm(
         url = TOKEN_URL,
         formParameters = parameters {
@@ -37,6 +48,46 @@ class Profile(private val client: HttpClient) {
             append("redirect_uri", REDIRECT_URI)
         }
     ).body<Token>()
+
+    suspend fun refreshToken(refreshToken: String, builder: HttpRequestBuilder.() -> Unit): Token? = mutex.withLock {
+        with(Preferences.token) {
+            if (this != null && this.refreshToken != refreshToken) {
+                return@withLock this
+            }
+        }
+
+        repeat(3) { attempt ->
+            try {
+                val response = client.submitForm(
+                    url = TOKEN_URL,
+                    block = builder,
+                    formParameters = parameters {
+                        append("grant_type", REFRESH_TOKEN)
+                        append("client_id", CLIENT_ID)
+                        append("client_secret", CLIENT_SECRET)
+                        append("refresh_token", refreshToken)
+                    }
+                )
+
+                if (response.status.isSuccess()) {
+                    val newToken = response.body<Token>()
+                    Preferences.saveToken(newToken)
+                    return@withLock newToken
+                } else if (response.status == HttpStatusCode.BadRequest || response.status == HttpStatusCode.Unauthorized) {
+                    Preferences.saveToken(Token.empty)
+                    return@withLock null
+                }
+            } catch (e: Exception) {
+                if (e !is IOException || attempt == 2) {
+                    return@withLock null
+                }
+
+                delay(1000L * (attempt + 1))
+            }
+        }
+
+        return@withLock null
+    }
 
     suspend fun whoAmI() = client.get("users/whoami").body<UserBrief>()
 
