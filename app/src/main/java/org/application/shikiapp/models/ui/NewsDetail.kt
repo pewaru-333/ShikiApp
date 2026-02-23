bdc208722af750efa72654f94bed426040e30f57
pav <email@inbox.ru>
2026-02-23 13:39:08 +0300

    — Изменение класса модели

--- app/src/main/java/org/application/shikiapp/models/ui/NewsDetail.kt
+++ app/src/main/java/org/application/shikiapp/models/ui/NewsDetail.kt
@@ -3,17 +3,18 @@ package org.application.shikiapp.models.ui
 import androidx.compose.ui.text.AnnotatedString
 import androidx.paging.PagingData
 import kotlinx.coroutines.flow.Flow
+import org.application.shikiapp.utils.CommentContent
 
 data class NewsDetail(
     val title: String,
     val newsBody: AnnotatedString,
-    val poster: String?,
+    val poster: CommentContent?,
     val userId: Long,
     val userNickname: String,
     val userImage: String,
     val date: String,
     val images: List<String>,
-    val videos: List<String>,
+    val videos: List<CommentContent.VideoContent>,
     val commentsCount: Int,
     val comments: Flow<PagingData<Comment>>
 )
