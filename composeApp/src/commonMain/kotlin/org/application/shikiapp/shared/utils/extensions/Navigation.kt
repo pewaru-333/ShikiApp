b8e4e6fe698190d31355309e3c8ee42e888647fe
pav <email@inbox.ru>
2026-03-14 15:55:19 +0300

    — Обновление функций расширения для навигации

--- composeApp/src/commonMain/kotlin/org/application/shikiapp/shared/utils/extensions/Navigation.kt
+++ composeApp/src/commonMain/kotlin/org/application/shikiapp/shared/utils/extensions/Navigation.kt
@@ -3,19 +3,71 @@ package org.application.shikiapp.shared.utils.extensions
 import androidx.navigation.NavBackStackEntry
 import androidx.navigation.NavDestination.Companion.hasRoute
 import androidx.navigation.NavDestination.Companion.hierarchy
+import androidx.navigation.NavGraph.Companion.findStartDestination
 import androidx.navigation.NavHostController
-import kotlin.reflect.KClass
+import androidx.navigation.toRoute
+import org.application.shikiapp.shared.utils.navigation.Screen
 
-fun NavHostController.toBottomBarItem(route: Any) =
-    currentBackStackEntry?.destination?.route?.let {
-        if (!route.toString().contains(it)) {
-            navigate(route) {
-                launchSingleTop = true
-                restoreState = true
-                popBackStack(route, true)
+fun NavHostController.onNavigationItemClick(route: Screen) {
+    if (currentBackStackEntry.isSameRoute(route)) return
+
+    val backStack = currentBackStack.value
+    val index = backStack.indexOfLast { it.destination.hasRoute(route::class) }
+
+    if (index != -1) {
+        if (index == backStack.lastIndex) {
+            return
+        }
+
+        navigate(route) {
+            popUpTo(route::class) { inclusive = false }
+            launchSingleTop = true
+        }
+    } else {
+        navigate(route) {
+            popUpTo(graph.findStartDestination().id) {
+                saveState = true
             }
+
+            launchSingleTop = true
+            restoreState = true
         }
     }
+}
 
-fun NavBackStackEntry?.isCurrentRoute(route: KClass<*>) =
-    this?.destination?.hierarchy?.any { it.hasRoute(route) } == true
+
+fun NavBackStackEntry?.isTopLevelRouteSelected(route: Screen): Boolean {
+    this ?: return false
+
+    val hasRoute = destination.hierarchy.any { it.hasRoute(route::class) }
+    if (!hasRoute) return false
+
+    return when (route) {
+        is Screen.UserRates -> {
+            val args = runCatching { toRoute<Screen.UserRates>() }.getOrNull()
+            args?.editable == route.editable
+        }
+
+        else -> true
+    }
+}
+
+fun NavBackStackEntry?.isSameRoute(route: Screen): Boolean {
+    this ?: return false
+
+    if (!destination.hasRoute(route::class)) return false
+
+    return when (route) {
+        is Screen.Catalog -> {
+            val args = runCatching { toRoute<Screen.Catalog>() }.getOrNull()
+            args == route
+        }
+
+        is Screen.UserRates -> {
+            val args = runCatching { toRoute<Screen.UserRates>() }.getOrNull()
+            args?.editable == route.editable
+        }
+
+        else -> true
+    }
+}
