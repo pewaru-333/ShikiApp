40eb1f401f01d2999a00ee458359d651e0e06eba
pav <email@inbox.ru>
2026-03-14 16:03:40 +0300

    — Удаление неиспользуемых файлов

--- composeApp/src/androidMain/kotlin/org/application/shikiapp/shared/utils/Activity.kt
+++
@@ -1,8 +0,0 @@
-package org.application.shikiapp.shared.utils
-
-import androidx.activity.ComponentActivity
-import androidx.compose.runtime.staticCompositionLocalOf
-
-val LocalActivity = staticCompositionLocalOf<ComponentActivity> {
-    error("ERROR!")
-}
