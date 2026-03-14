151f2f2ee2b1e57f2b8432a76bd60cb54e9926af
pav <email@inbox.ru>
2026-03-14 16:00:30 +0300

    — Правильная логика получения полного Uri

--- composeApp/src/commonMain/kotlin/org/application/shikiapp/shared/utils/extensions/Uri.kt
+++ composeApp/src/commonMain/kotlin/org/application/shikiapp/shared/utils/extensions/Uri.kt
@@ -3,12 +3,13 @@ package org.application.shikiapp.shared.utils.extensions
 import io.ktor.http.URLBuilder
 import io.ktor.http.Url
 import org.application.shikiapp.shared.utils.BASE_URL
+import org.application.shikiapp.shared.utils.URL_MIRROR
 
 fun String.toFullUri(): Url {
-    val fullUri = if (!contains(BASE_URL)) {
-        BASE_URL + this
-    } else {
+    val fullUri = if (contains(BASE_URL) || contains(URL_MIRROR)) {
         this
+    } else {
+        BASE_URL + this
     }
 
     return URLBuilder(fullUri).build()
