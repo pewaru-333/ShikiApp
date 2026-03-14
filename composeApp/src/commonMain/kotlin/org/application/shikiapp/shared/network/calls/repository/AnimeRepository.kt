543a0c6ab8d48c7573ce25c8e306c29728499b38
pav <email@inbox.ru>
2026-03-14 15:54:51 +0300

    — Оптимизация импорта

--- composeApp/src/commonMain/kotlin/org/application/shikiapp/shared/network/calls/repository/AnimeRepository.kt
+++ composeApp/src/commonMain/kotlin/org/application/shikiapp/shared/network/calls/repository/AnimeRepository.kt
@@ -3,10 +3,10 @@ package org.application.shikiapp.shared.network.calls.repository
 import androidx.paging.PagingData
 import kotlinx.coroutines.flow.Flow
 import org.application.shikiapp.shared.models.data.AnimeBasic
-import org.application.shikiapp.shared.models.ui.Comment
 import org.application.shikiapp.shared.models.data.Franchise
 import org.application.shikiapp.shared.models.ui.Anime
 import org.application.shikiapp.shared.models.ui.AnimeT
+import org.application.shikiapp.shared.models.ui.Comment
 
 interface AnimeRepository {
     fun getAnimeRawData(id: String): Flow<AnimeT>
