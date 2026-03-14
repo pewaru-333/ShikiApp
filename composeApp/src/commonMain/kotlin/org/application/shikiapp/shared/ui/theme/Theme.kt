d2b6b453412eb704e446bac2cf2eab728458562f
pav <email@inbox.ru>
2026-03-14 16:00:05 +0300

    — Обновление импорта

--- composeApp/src/commonMain/kotlin/org/application/shikiapp/shared/ui/theme/Theme.kt
+++ composeApp/src/commonMain/kotlin/org/application/shikiapp/shared/ui/theme/Theme.kt
@@ -3,7 +3,6 @@
 package org.application.shikiapp.shared.ui.theme
 
 import androidx.compose.foundation.isSystemInDarkTheme
-import androidx.compose.material3.ColorScheme
 import androidx.compose.material3.ExperimentalMaterial3ExpressiveApi
 import androidx.compose.material3.MaterialExpressiveTheme
 import androidx.compose.material3.darkColorScheme
