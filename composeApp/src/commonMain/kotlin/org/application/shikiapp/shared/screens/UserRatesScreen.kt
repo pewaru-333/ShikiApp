e257ae5aa5807449a84ad085d78dc3e3f75ccd3d
pav <email@inbox.ru>
2026-03-14 16:01:53 +0300

    — Обновление экрана пользовательского списка

--- composeApp/src/commonMain/kotlin/org/application/shikiapp/shared/screens/UserRatesScreen.kt
+++ composeApp/src/commonMain/kotlin/org/application/shikiapp/shared/screens/UserRatesScreen.kt
@@ -13,9 +13,11 @@ import androidx.compose.foundation.combinedClickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -26,11 +28,18 @@ import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyListState
 import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyGridState
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.foundation.lazy.grid.rememberLazyGridState
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FilledIconToggleButton
@@ -38,14 +47,12 @@ import androidx.compose.material3.FilledTonalButton
 import androidx.compose.material3.FilledTonalIconButton
 import androidx.compose.material3.FilterChip
 import androidx.compose.material3.HorizontalDivider
-import androidx.compose.material3.Icon
 import androidx.compose.material3.LinearProgressIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.PrimaryScrollableTabRow
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Tab
 import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
@@ -60,6 +67,7 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.launch
@@ -69,27 +77,26 @@ import org.application.shikiapp.shared.models.states.UserRateUiEvent
 import org.application.shikiapp.shared.models.states.rememberRateState
 import org.application.shikiapp.shared.models.ui.UserRate
 import org.application.shikiapp.shared.models.viewModels.UserRateViewModel
-import org.application.shikiapp.shared.network.response.RatesResponse.Error
-import org.application.shikiapp.shared.network.response.RatesResponse.Loading
-import org.application.shikiapp.shared.network.response.RatesResponse.NoAccess
-import org.application.shikiapp.shared.network.response.RatesResponse.Success
-import org.application.shikiapp.shared.network.response.RatesResponse.Unlogged
+import org.application.shikiapp.shared.network.response.RatesResponse
 import org.application.shikiapp.shared.ui.templates.AnimatedAsyncImage
 import org.application.shikiapp.shared.ui.templates.DialogEditRate
 import org.application.shikiapp.shared.ui.templates.ErrorScreen
 import org.application.shikiapp.shared.ui.templates.LoadingScreen
 import org.application.shikiapp.shared.ui.templates.NavigationIcon
+import org.application.shikiapp.shared.ui.templates.SearchAppBar
 import org.application.shikiapp.shared.ui.templates.VectorIcon
 import org.application.shikiapp.shared.utils.enums.LinkedType
 import org.application.shikiapp.shared.utils.enums.OrderDirection
 import org.application.shikiapp.shared.utils.enums.OrderRates
 import org.application.shikiapp.shared.utils.enums.WatchStatus
+import org.application.shikiapp.shared.utils.enums.WindowSize
 import org.application.shikiapp.shared.utils.extensions.pairwise
 import org.application.shikiapp.shared.utils.navigation.LocalBarVisibility
 import org.application.shikiapp.shared.utils.navigation.Screen
 import org.application.shikiapp.shared.utils.rememberToastState
+import org.application.shikiapp.shared.utils.ui.IWindowSize
+import org.application.shikiapp.shared.utils.ui.rememberWindowSize
 import org.application.shikiapp.shared.utils.viewModel
-import org.jetbrains.compose.resources.painterResource
 import org.jetbrains.compose.resources.stringResource
 import shikiapp.composeapp.generated.resources.Res
 import shikiapp.composeapp.generated.resources.text_empty
@@ -120,16 +127,16 @@ fun UserRates(onNavigate: (Screen) -> Unit, onBack: () -> Unit) {
     val scope = rememberCoroutineScope()
     val pagerState = rememberPagerState(pageCount = WatchStatus.entries::size)
     val listStates = WatchStatus.entries.map { rememberLazyListState() }
+    val gridStates = WatchStatus.entries.map { rememberLazyGridState() }
 
     val rateState = rememberRateState()
     val rates by model.rates.collectAsStateWithLifecycle()
     val newRate by model.newRate.collectAsStateWithLifecycle()
 
     val type by model.type.collectAsStateWithLifecycle()
+    val search by model.search.collectAsStateWithLifecycle()
 
-    LaunchedEffect(Unit) {
-        barVisibility.toggle(!model.editable)
-    }
+    val windowSize = rememberWindowSize()
 
     LaunchedEffect(type) {
         snapshotFlow { type }
@@ -147,6 +154,7 @@ fun UserRates(onNavigate: (Screen) -> Unit, onBack: () -> Unit) {
             .collectLatest { (old, new) ->
                 if (new != old) {
                     listStates[pagerState.currentPage].requestScrollToItem(0)
+                    gridStates[pagerState.currentPage].requestScrollToItem(0)
                 }
             }
     }
@@ -169,68 +177,76 @@ fun UserRates(onNavigate: (Screen) -> Unit, onBack: () -> Unit) {
     }
 
     when (response) {
-        Unlogged -> UnloggedScreen(onNavigate)
-        Error -> ErrorScreen(model::loadRates)
-        is Success, Loading, NoAccess -> Scaffold(
+        RatesResponse.Unlogged -> UnloggedScreen(onNavigate)
+        RatesResponse.Error -> ErrorScreen(model::loadRates)
+        is RatesResponse.Success, RatesResponse.Loading, RatesResponse.NoAccess -> Scaffold(
             topBar = {
-                TopAppBar(
+                SearchAppBar(
+                    search = search,
+                    onSearch = model::setSearch,
                     navigationIcon = { if (!model.editable) NavigationIcon(onBack) },
-                    title = { Text(stringResource(type.getListTitle())) },
                     actions = {
                         if (model.editable) {
                             FilledIconToggleButton(
                                 checked = type == LinkedType.ANIME,
                                 onCheckedChange = { model.setLinkedType(LinkedType.ANIME) },
-                                content = { Icon(painterResource(Res.drawable.vector_anime), null) }
+                                content = { VectorIcon(Res.drawable.vector_anime) }
                             )
                             FilledIconToggleButton(
                                 checked = type == LinkedType.MANGA,
                                 onCheckedChange = { model.setLinkedType(LinkedType.MANGA) },
-                                content = { Icon(painterResource(Res.drawable.vector_manga), null) }
+                                content = { VectorIcon(Res.drawable.vector_manga) }
                             )
                         }
                     }
                 )
             }
         ) { values ->
-            if (response is Loading) LoadingScreen()
-            else if (response is NoAccess) Box(Modifier.fillMaxSize(), Alignment.Center) { Text(stringResource(Res.string.text_profile_closed)) }
-            else Column(Modifier.padding(values)) {
-                PrimaryScrollableTabRow(pagerState.currentPage, edgePadding = 8.dp) {
-                    WatchStatus.entries.forEach { status ->
-                        Tab(
-                            selected = pagerState.targetPage == status.ordinal,
-                            onClick = { scope.launch { pagerState.animateScrollToPage(status.ordinal) } },
-                            text = { Text(stringResource(type.getWatchStatusTitle(status))) }
-                        )
-                    }
+            when (response) {
+                is RatesResponse.Loading -> LoadingScreen()
+                is RatesResponse.NoAccess -> Box(Modifier.fillMaxSize(), Alignment.Center) {
+                    Text(stringResource(Res.string.text_profile_closed))
                 }
 
-                LazyRow(
-                    contentPadding = PaddingValues(horizontal = 16.dp),
-                    horizontalArrangement = Arrangement.spacedBy(8.dp)
-                ) {
-                    items(OrderRates.entries) { order ->
-                        SortChip(
-                            order = order,
-                            state = orderState,
+                else -> Column(Modifier.padding(values)) {
+                    PrimaryScrollableTabRow(pagerState.currentPage, edgePadding = 8.dp) {
+                        WatchStatus.entries.forEach { status ->
+                            Tab(
+                                selected = pagerState.targetPage == status.ordinal,
+                                onClick = { scope.launch { pagerState.animateScrollToPage(status.ordinal) } },
+                                text = { Text(stringResource(type.getWatchStatusTitle(status))) }
+                            )
+                        }
+                    }
+
+                    LazyRow(
+                        contentPadding = PaddingValues(horizontal = 16.dp),
+                        horizontalArrangement = Arrangement.spacedBy(8.dp)
+                    ) {
+                        items(OrderRates.entries, OrderRates::name) { order ->
+                            SortChip(
+                                order = order,
+                                state = orderState,
+                                type = type,
+                                onClick = { model.onSortChanged(order) }
+                            )
+                        }
+                    }
+
+                    HorizontalPager(pagerState) { page ->
+                        UserRateList(
+                            rates = rates.getOrDefault(WatchStatus.entries[page], emptyList()),
+                            listState = listStates[page],
+                            gridState = gridStates[page],
                             type = type,
-                            onClick = { model.onSortChanged(order) }
+                            getRate = model::getRate,
+                            editable = model.editable,
+                            windowSize = windowSize,
+                            rateState = rateState,
+                            increment = model::increment,
+                            onNavigate = onNavigate
                         )
                     }
-                }
-
-                HorizontalPager(pagerState) { page ->
-                    UserRateList(
-                        rates = rates.getOrDefault(WatchStatus.entries[page], emptyList()),
-                        listState = listStates[page],
-                        type = type,
-                        getRate = model::getRate,
-                        editable = model.editable,
-                        rateState = rateState,
-                        increment = model::increment,
-                        onNavigate = onNavigate
-                    )
                 }
             }
         }
@@ -261,22 +277,19 @@ private fun UnloggedScreen(onNavigate: (Screen) -> Unit) =
             horizontalAlignment = Alignment.CenterHorizontally,
             verticalArrangement = Arrangement.spacedBy(8.dp)
         ) {
-            Icon(
-                modifier = Modifier.size(96.dp),
-                painter = painterResource(Res.drawable.vector_no_profile),
-                contentDescription = null
-            )
+            VectorIcon(Res.drawable.vector_no_profile, Modifier.size(96.dp))
             Text(
                 text = stringResource(Res.string.text_login_to_modify_lists),
                 textAlign = TextAlign.Center,
                 style = MaterialTheme.typography.bodyLarge
             )
             FilledTonalButton(
-                onClick = { onNavigate(Screen.Profile) }
-            ) {
-                Text(stringResource(Res.string.text_to_profile))
-                VectorIcon(Res.drawable.vector_keyboard_arrow_right)
-            }
+                onClick = { onNavigate(Screen.Profile) },
+                content = {
+                    Text(stringResource(Res.string.text_to_profile))
+                    VectorIcon(Res.drawable.vector_keyboard_arrow_right)
+                }
+            )
         }
     }
 
@@ -345,11 +358,12 @@ private fun UserRateCard(
     onGetRate: (UserRate) -> Unit,
     onIncrement: (Long) -> Unit,
     onNavigate: (Screen) -> Unit
-) = Box(modifier.height(175.dp), Alignment.Center) {
+) = Box(modifier.fillMaxWidth()) {
     Row(
         horizontalArrangement = Arrangement.spacedBy(16.dp),
         modifier = Modifier
             .fillMaxSize()
+            .height(IntrinsicSize.Min)
             .combinedClickable(
                 enabled = !rateState.isEditing(rate.id),
                 onClick = { onNavigate(type.navigateTo(rate.contentId)) },
@@ -361,30 +375,35 @@ private fun UserRateCard(
             model = rate.poster,
             contentScale = ContentScale.Crop,
             modifier = Modifier
-                .width(100.dp)
-                .fillMaxHeight()
+                .width(110.dp)
+                .aspectRatio(2f / 3f)
                 .clip(MaterialTheme.shapes.medium)
                 .border((0.5).dp, MaterialTheme.colorScheme.onSurface, MaterialTheme.shapes.medium)
         )
 
-        Column(Modifier.fillMaxHeight()) {
+        Column(
+            modifier = Modifier
+                .weight(1f)
+                .fillMaxHeight()
+        ) {
             Text(
                 text = rate.title,
                 maxLines = 2,
                 overflow = TextOverflow.Ellipsis,
                 style = MaterialTheme.typography.titleMedium.copy(
-                    fontWeight = FontWeight.SemiBold,
+                    fontWeight = FontWeight.Bold,
+                    lineHeight = 22.sp
                 )
             )
 
             Spacer(Modifier.height(4.dp))
 
-            Row(Modifier, Arrangement.spacedBy(8.dp), Alignment.CenterVertically) {
-                Text(
-                    text = stringResource(rate.kind),
-                    style = MaterialTheme.typography.bodySmall
+            Text(
+                text = stringResource(rate.kindString),
+                style = MaterialTheme.typography.bodySmall.copy(
+                    color = MaterialTheme.colorScheme.onSurfaceVariant
                 )
-            }
+            )
 
             Spacer(Modifier.height(8.dp))
 
@@ -427,19 +446,22 @@ private fun UserRateList(
     rates: List<UserRate>,
     rateState: UserRateState,
     listState: LazyListState,
+    gridState: LazyGridState,
     getRate: (UserRate) -> Unit,
     type: LinkedType,
     editable: Boolean,
+    windowSize: IWindowSize,
     increment: (Long) -> Unit,
     onNavigate: (Screen) -> Unit
-) = LazyColumn(Modifier.fillMaxSize(), listState) {
-    if (rates.isEmpty()) {
-        item {
-            Box(Modifier.fillParentMaxSize(), Alignment.Center) {
-                Text(stringResource(Res.string.text_empty))
-            }
-        }
-    } else {
+) = when {
+    rates.isEmpty() -> Box(Modifier.fillMaxSize(), Alignment.Center) {
+        Text(
+            text = stringResource(Res.string.text_empty),
+            color = MaterialTheme.colorScheme.onSurfaceVariant
+        )
+    }
+
+    windowSize.isCompact -> LazyColumn(Modifier.fillMaxSize(), listState) {
         items(rates, UserRate::id) { rate ->
             UserRateCard(
                 rate = rate,
@@ -453,6 +475,42 @@ private fun UserRateList(
 
             if (rate != rates.lastOrNull()) {
                 HorizontalDivider(Modifier.padding(horizontal = 12.dp))
+            }
+        }
+    }
+
+    else -> {
+        val minSize = when (windowSize.windowSize) {
+            WindowSize.MEDIUM -> 220.dp
+            else -> 300.dp
+        }
+
+        LazyVerticalGrid(
+            columns = GridCells.Adaptive(minSize),
+            modifier = Modifier.fillMaxSize(),
+            state = gridState,
+            contentPadding = PaddingValues(16.dp),
+            horizontalArrangement = Arrangement.spacedBy(16.dp),
+            verticalArrangement = Arrangement.spacedBy(16.dp)
+        ) {
+            items(rates, UserRate::id) { rate ->
+                Card(
+                    shape = MaterialTheme.shapes.medium,
+                    elevation = CardDefaults.cardElevation(2.dp),
+                    colors = CardDefaults.cardColors(
+                        containerColor = MaterialTheme.colorScheme.surfaceContainerLow
+                    )
+                ) {
+                    UserRateCard(
+                        rate = rate,
+                        type = type,
+                        editable = editable,
+                        rateState = rateState,
+                        onNavigate = onNavigate,
+                        onGetRate = getRate,
+                        onIncrement = increment
+                    )
+                }
             }
         }
     }
@@ -473,14 +531,11 @@ private fun SortChip(order: OrderRates, state: SortingState, type: LinkedType, o
         },
         leadingIcon = {
             if (state.order == order) {
-                Icon(
-                    contentDescription = null,
-                    painter = painterResource(
-                        when (state.direction) {
-                            OrderDirection.ASCENDING -> Res.drawable.vector_arrow_up
-                            OrderDirection.DESCENDING -> Res.drawable.vector_arrow_down
-                        }
-                    )
+                VectorIcon(
+                    resId = when (state.direction) {
+                        OrderDirection.ASCENDING -> Res.drawable.vector_arrow_up
+                        OrderDirection.DESCENDING -> Res.drawable.vector_arrow_down
+                    }
                 )
             }
         }
