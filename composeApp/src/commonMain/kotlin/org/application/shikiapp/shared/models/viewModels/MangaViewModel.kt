97812a62de2555e1dc44307e7c13c7e79d3a0d33
pav <email@inbox.ru>
2026-04-01 13:58:59 +0300

    — Рефакторинг кода

--- composeApp/src/commonMain/kotlin/org/application/shikiapp/shared/models/viewModels/MangaViewModel.kt
+++ composeApp/src/commonMain/kotlin/org/application/shikiapp/shared/models/viewModels/MangaViewModel.kt
@@ -11,6 +11,7 @@ import org.application.shikiapp.shared.models.ui.MangaT
 import org.application.shikiapp.shared.network.client.Network
 import org.application.shikiapp.shared.network.response.AsyncData
 import org.application.shikiapp.shared.network.response.Response
+import org.application.shikiapp.shared.utils.enums.CommentableType
 import org.application.shikiapp.shared.utils.enums.LinkedType
 import org.application.shikiapp.shared.utils.navigation.Screen
 
@@ -21,11 +22,8 @@ class MangaViewModel(saved: SavedStateHandle) : CachedDetailViewModel<MangaT, Ma
     override fun initState() = MangaState()
 
     override fun getSourceFlow(id: Any) = Network.mangaRepository.getMangaRawData(id.toString())
-      //  GraphQL.getManga(id.toString())
 
     override suspend fun transformData(data: MangaT): Manga {
-       // val (main, extra) = data
-
         val (franchise, similar, favoured) = coroutineScope {
             val franchise = async { Network.manga.getFranchise(contentId) }
             val similar = async { Network.manga.getSimilar(contentId) }
@@ -34,7 +32,7 @@ class MangaViewModel(saved: SavedStateHandle) : CachedDetailViewModel<MangaT, Ma
             Triple(franchise.await(), similar.await(), favoured.await())
         }
 
-        setCommentParams(data.topicId)
+        setCommentParams(data.topicId, CommentableType.MANGA)
 
         return Network.mangaRepository.mapToManga(
             raw = data,
@@ -49,38 +47,15 @@ class MangaViewModel(saved: SavedStateHandle) : CachedDetailViewModel<MangaT, Ma
         super.onEvent(event)
 
         when (event) {
-            ContentDetailEvent.ShowComments -> updateState { it.copy(showComments = !it.showComments) }
-            ContentDetailEvent.ShowSheet -> updateState { it.copy(showSheet = !it.showSheet) }
+            ContentDetailEvent.Media.ChangeRate -> with(response.value) {
+                if (this !is Response.Success) return
 
-            is ContentDetailEvent.Media -> when (event) {
-                ContentDetailEvent.Media.ChangeRate -> with(response.value) {
-                    if (this !is Response.Success) return
+                val newData = data.copy(userRate = AsyncData.Loading)
 
-                    val newData = data.copy(userRate = AsyncData.Loading)
+                updateState { it.copy(dialogState = null) }
 
-                    updateState { it.copy(showRate = !it.showRate) }
-
-                    tryEmit(Response.Success(newData))
-                    loadData()
-                }
-
-                ContentDetailEvent.Media.ShowPoster -> updateState { it.copy(showPoster = !it.showPoster) }
-                ContentDetailEvent.Media.ShowAuthors -> updateState { it.copy(showAuthors = !it.showAuthors) }
-                ContentDetailEvent.Media.ShowCharacters -> updateState { it.copy(showCharacters = !it.showCharacters) }
-                ContentDetailEvent.Media.ShowRelated -> updateState { it.copy(showRelated = !it.showRelated) }
-                ContentDetailEvent.Media.ShowSimilar -> updateState { it.copy(showSimilar = !it.showSimilar) }
-                ContentDetailEvent.Media.ShowStats -> updateState { it.copy(showStats = !it.showStats) }
-
-                ContentDetailEvent.Media.ShowLinks -> updateState {
-                    it.copy(
-                        showLinks = !it.showLinks,
-                        showSheet = !it.showSheet
-                    )
-                }
-
-                ContentDetailEvent.Media.ShowRate -> updateState { it.copy(showRate = !it.showRate) }
-
-                else -> Unit
+                tryEmit(Response.Success(newData))
+                loadData()
             }
 
             is ContentDetailEvent.Media.Manga.ToggleFavourite -> with(response.value) {
