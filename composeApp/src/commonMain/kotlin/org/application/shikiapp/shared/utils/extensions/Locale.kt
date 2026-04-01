95e41b53efdb25b9f942ff16c8d763c74014f92d
pav <email@inbox.ru>
2026-04-01 14:14:22 +0300

    — Рефакторинг кода

--- composeApp/src/commonMain/kotlin/org/application/shikiapp/shared/utils/extensions/Locale.kt
+++ composeApp/src/commonMain/kotlin/org/application/shikiapp/shared/utils/extensions/Locale.kt
@@ -2,4 +2,4 @@ package org.application.shikiapp.shared.utils.extensions
 
 import java.util.Locale
 
-fun Locale.getDisplayRegionName() = getDisplayName(this).replaceFirstChar(Char::uppercase)
+fun Locale.getLocalizedName() = getDisplayName(this).replaceFirstChar(Char::uppercase)
