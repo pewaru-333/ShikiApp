72757da778ad9827f56b9c4390a7fa5128b4b922
pav <email@inbox.ru>
2026-04-01 14:19:50 +0300

    — Изменения цвета элементов списка в карточке

--- composeApp/src/commonMain/kotlin/org/application/shikiapp/shared/ui/templates/BottomSheet.kt
+++ composeApp/src/commonMain/kotlin/org/application/shikiapp/shared/ui/templates/BottomSheet.kt
@@ -3,32 +3,27 @@
 package org.application.shikiapp.shared.ui.templates
 
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.WindowInsets
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.systemBars
-import androidx.compose.foundation.layout.windowInsetsBottomHeight
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.BottomSheetDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ListItem
 import androidx.compose.material3.ListItemDefaults
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.Text
+import androidx.compose.material3.contentColorFor
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.FilterQuality
 import androidx.compose.ui.platform.LocalClipboard
 import androidx.compose.ui.platform.LocalUriHandler
 import androidx.compose.ui.unit.dp
-import coil3.compose.AsyncImage
 import kotlinx.coroutines.launch
 import org.application.shikiapp.shared.events.ContentDetailEvent
+import org.application.shikiapp.shared.models.states.BaseDialogState
 import org.application.shikiapp.shared.models.ui.ExternalLink
 import org.application.shikiapp.shared.utils.BLANK
 import org.application.shikiapp.shared.utils.extensions.toClipEntry
@@ -44,53 +39,37 @@ import shikiapp.composeapp.generated.resources.vector_open_in_browser
 import shikiapp.composeapp.generated.resources.vector_website
 
 @Composable
-fun SheetColumn(list: List<String>, label: String, onHide: () -> Unit) =
-    ModalBottomSheet(onHide) {
-        Row(Modifier.fillMaxWidth()) {
-            Text(
-                text = label,
-                modifier = Modifier.padding(start = 12.dp, bottom = 8.dp),
-                style = MaterialTheme.typography.titleLarge
-            )
-        }
-
-        LazyColumn {
-            items(list) { item ->
-                ListItem(
-                    headlineContent = { Text(item) },
-                    colors = ListItemDefaults.colors(
-                        containerColor = MaterialTheme.colorScheme.surfaceContainerLow
-                    )
-                )
-            }
-        }
-
-        Spacer(Modifier.windowInsetsBottomHeight(WindowInsets.systemBars))
-    }
-
-@Composable
 fun BottomSheet(
     url: String,
     website: String = BLANK,
     canShowLinks: Boolean = false,
     onEvent: (ContentDetailEvent) -> Unit
 ) = ModalBottomSheet(
-    onDismissRequest = { onEvent(ContentDetailEvent.ShowSheet) }
+    onDismissRequest = { onEvent(ContentDetailEvent.ToggleDialog(null)) },
+    contentWindowInsets = { WindowInsets.systemBars }
 ) {
     val scope = rememberCoroutineScope()
     val clipboard = LocalClipboard.current
+
+    val colors = ListItemDefaults.colors(
+        containerColor = BottomSheetDefaults.ContainerColor,
+        headlineColor = contentColorFor(BottomSheetDefaults.ContainerColor),
+        leadingIconColor = contentColorFor(BottomSheetDefaults.ContainerColor)
+    )
 
     if (website.isNotEmpty()) {
         val handler = LocalUriHandler.current
 
         ListItem(
+            colors = colors,
             headlineContent = { Text(stringResource(Res.string.text_official_site)) },
-            modifier = Modifier.clickable { handler.openUri(website) },
-            leadingContent = { VectorIcon(Res.drawable.vector_website) }
+            leadingContent = { VectorIcon(Res.drawable.vector_website) },
+            modifier = Modifier.clickable { handler.openUri(website) }
         )
     }
 
     ListItem(
+        colors = colors,
         headlineContent = { Text(stringResource(Res.string.text_copy_link)) },
         leadingContent = { VectorIcon(Res.drawable.vector_copy) },
         modifier = Modifier.clickable {
@@ -100,41 +79,47 @@ fun BottomSheet(
 
     if (canShowLinks) {
         ListItem(
+            colors = colors,
             headlineContent = { Text(stringResource(Res.string.text_external_links)) },
             leadingContent = { VectorIcon(Res.drawable.vector_list) },
-            modifier = Modifier.clickable { onEvent(ContentDetailEvent.Media.ShowLinks) }
+            modifier = Modifier.clickable { onEvent(ContentDetailEvent.ToggleDialog(BaseDialogState.Media.Links)) }
         )
     }
 
     ListItem(
+        colors = colors,
         headlineContent = { Text(stringResource(Res.string.text_open_in_browser)) },
-        modifier = Modifier.clickable { onEvent(ContentDetailEvent.OpenLink) },
-        leadingContent = { VectorIcon(Res.drawable.vector_open_in_browser) }
+        leadingContent = { VectorIcon(Res.drawable.vector_open_in_browser) },
+        modifier = Modifier.clickable { onEvent(ContentDetailEvent.OpenLink) }
     )
 }
 
 @Composable
-fun LinksSheet(list: List<ExternalLink>, hide: () -> Unit) {
+fun LinksSheet(list: List<ExternalLink>, onHide: () -> Unit) {
     val handler = LocalUriHandler.current
 
-    ModalBottomSheet(hide) {
+    ModalBottomSheet(
+        onDismissRequest = onHide,
+        contentWindowInsets = { WindowInsets.systemBars }
+    ) {
         LazyColumn {
-            items(list) {
+            items(list, ExternalLink::url) {
                 ListItem(
                     modifier = Modifier.clickable { handler.openUri(it.url.toString()) },
                     headlineContent = { Text(it.title) },
                     leadingContent = {
-                        AsyncImage(
-                            contentDescription = null,
+                        AnimatedAsyncImage(
                             model = "https://www.google.com/s2/favicons?domain=${it.url.host}&sz=128",
                             modifier = Modifier.size(24.dp),
-                            filterQuality = FilterQuality.High
                         )
-                    }
+                    },
+                    colors = ListItemDefaults.colors(
+                        containerColor = BottomSheetDefaults.ContainerColor,
+                        headlineColor = contentColorFor(BottomSheetDefaults.ContainerColor),
+                        leadingIconColor = contentColorFor(BottomSheetDefaults.ContainerColor)
+                    )
                 )
             }
         }
-
-        Spacer(Modifier.windowInsetsBottomHeight(WindowInsets.systemBars))
     }
 }
