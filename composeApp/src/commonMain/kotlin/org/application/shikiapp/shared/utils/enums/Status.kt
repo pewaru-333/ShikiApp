21626c1e41343cb6600cc6c9e2f840eb1d35a87e
pav <email@inbox.ru>
2026-04-01 14:07:00 +0300

    — Цвета для статуса контента

--- composeApp/src/commonMain/kotlin/org/application/shikiapp/shared/utils/enums/Status.kt
+++ composeApp/src/commonMain/kotlin/org/application/shikiapp/shared/utils/enums/Status.kt
@@ -1,6 +1,9 @@
 package org.application.shikiapp.shared.utils.enums
 
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.luminance
 import org.jetbrains.compose.resources.StringResource
 import shikiapp.composeapp.generated.resources.Res
 import shikiapp.composeapp.generated.resources.status_anons
@@ -48,18 +51,45 @@ enum class Status(
         else mangaTitle
 }
 
-val Status.backgroundColor: Color
-    get() = when (this) {
-        Status.ANONS -> Color(0xFFF28F3B)
-        Status.ONGOING -> Color(0xFFC6E0FF)
-        Status.RELEASED -> Color(0xFF0CF574)
-        Status.PAUSED -> Color(0xFFAEB7AF)
-        Status.DISCONTINUED -> Color(0xFFEF2D56)
-    }
+data class StatusColors(
+    val background: Color,
+    val text: Color
+)
 
-val Status.textColor: Color
-    get() = when (this) {
-        Status.ANONS, Status.RELEASED, Status.PAUSED -> Color.Black
-        Status.ONGOING -> Color(0xFF0E2239)
-        Status.DISCONTINUED -> Color(0xFFEFEFEF)
+val Status.colors: StatusColors
+    @Composable
+    get() {
+        val isDark = MaterialTheme.colorScheme.background.luminance() < 0.5f
+
+        return when (this) {
+            Status.ANONS -> if (isDark) {
+                StatusColors(background = Color(0xFF5C330A), text = Color(0xFFFFDCC1))
+            } else {
+                StatusColors(background = Color(0xFFF28F3B), text = Color.Black)
+            }
+
+            Status.ONGOING -> if (isDark) {
+                StatusColors(background = Color(0xFF0A2B4C), text = Color(0xFFC6E0FF))
+            } else {
+                StatusColors(background = Color(0xFFC6E0FF), text = Color(0xFF0E2239))
+            }
+
+            Status.RELEASED -> if (isDark) {
+                StatusColors(background = Color(0xFF00421F), text = Color(0xFF67DF90))
+            } else {
+                StatusColors(background = Color(0xFF0CF574), text = Color.Black)
+            }
+
+            Status.PAUSED -> if (isDark) {
+                StatusColors(background = Color(0xFF3C403D), text = Color(0xFFDFE4DF))
+            } else {
+                StatusColors(background = Color(0xFFAEB7AF), text = Color.Black)
+            }
+
+            Status.DISCONTINUED -> if (isDark) {
+                StatusColors(background = Color(0xFF7A001D), text = Color(0xFFFFD9E2))
+            } else {
+                StatusColors(background = Color(0xFFEF2D56), text = Color.White)
+            }
+        }
     }
