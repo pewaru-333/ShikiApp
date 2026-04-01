f1417ec50222b9077b6669487a71f523fa756ad4
pav <email@inbox.ru>
2026-04-01 13:56:48 +0300

    — Переменные для хранения конфига приложения

--- composeApp/src/commonMain/kotlin/org/application/shikiapp/shared/AppConfig.kt
+++ composeApp/src/commonMain/kotlin/org/application/shikiapp/shared/AppConfig.kt
@@ -12,12 +12,27 @@ data class AppConfig(
     else listOf("user_rates", /*"email"*/ "messages", "comments", "topics", /*"content"*/ "clubs", "friends", "ignores")
 ) {
     companion object {
-        fun createDesktopConfig() = AppConfig(
+        private val ShikiApp = AppConfig(
             baseUrl = "https://shikimori.io",
             urlMirror = "https://shiki.one",
             userAgent = "ShikiApp",
             clientId = "C0IlIBQYqt9VHjuoayfbBG9ulhBH9XWuTOxSX_6oE6g",
             clientSecret = "0U2MtkFgtGUP9_TFKBw1ORVy6S68KZDz_AdKsoMfnFM",
+            redirectUri = "app://login"
+        )
+        private val DarkShiki = AppConfig(
+            baseUrl = "https://shikimori.fi",
+            urlMirror = "https://shikimori.rip",
+            userAgent = "DarkShiki",
+            clientId = "d8W9rjFLuEZKx_dYXzJ42nGvsUckx4vfhMu5Liyr7MY",
+            clientSecret = "Sog_CyJs19eCuFbIvg06Gb8zu8AMXZE8VI2CKLN1td4",
+            redirectUri = "darkshiki://auth/login"
+        )
+
+        fun createFlavourConfig(userAgent: String) = if (userAgent == "ShikiApp") ShikiApp
+        else DarkShiki
+
+        fun createDesktopConfig() = ShikiApp.copy(
             redirectUri = "app://login/"
         )
     }
