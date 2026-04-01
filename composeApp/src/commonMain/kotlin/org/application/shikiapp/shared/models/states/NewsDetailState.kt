674e0827e99252f86cd2f08ddc2e20ea0d054897
pav <email@inbox.ru>
2026-04-01 14:11:44 +0300

    — Обновление state

--- composeApp/src/commonMain/kotlin/org/application/shikiapp/shared/models/states/NewsDetailState.kt
+++ composeApp/src/commonMain/kotlin/org/application/shikiapp/shared/models/states/NewsDetailState.kt
@@ -1,7 +1,10 @@
 package org.application.shikiapp.shared.models.states
 
 data class NewsDetailState(
-    val showComments: Boolean = false,
-    val showImage: Boolean = false,
-    val image: Int = 0
-)
+    val image: Int = 0,
+    override val isSendingComment: Boolean = false,
+    override val dialogState: BaseDialogState? = null
+) : BaseState<NewsDetailState> {
+    override fun updateSendingState(isSending: Boolean) = copy(isSendingComment = isSending)
+    override fun updateDialogState(dialogState: BaseDialogState?) = copy(dialogState = dialogState)
+}
