7460b3e481f8438a5c86ab2120e4dc2747f914a4
pav <email@inbox.ru>
2026-04-01 14:01:20 +0300

    — Типы для комментария

---
+++ composeApp/src/commonMain/kotlin/org/application/shikiapp/shared/utils/enums/CommentableType.kt
@@ -0,0 +1,16 @@
+package org.application.shikiapp.shared.utils.enums
+
+enum class CommentableType {
+    TOPIC,
+    USER,
+    ANIME,
+    MANGA,
+    CHARACTER,
+    PERSON,
+    ARTICLE,
+    CLUB,
+    CLUBPAGE,
+    COLLECTION,
+    CRITIQUE,
+    REVIEW
+}
