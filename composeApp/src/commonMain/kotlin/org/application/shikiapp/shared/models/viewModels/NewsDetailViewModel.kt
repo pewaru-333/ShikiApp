5c52a8792a0f5e209750230596a5a5a07982e9c5
pav <email@inbox.ru>
2026-04-01 13:59:11 +0300

    — Рефакторинг кода

--- composeApp/src/commonMain/kotlin/org/application/shikiapp/shared/models/viewModels/NewsDetailViewModel.kt
+++ composeApp/src/commonMain/kotlin/org/application/shikiapp/shared/models/viewModels/NewsDetailViewModel.kt
@@ -6,11 +6,13 @@ import androidx.navigation.toRoute
 import kotlinx.coroutines.async
 import kotlinx.coroutines.launch
 import org.application.shikiapp.shared.events.ContentDetailEvent
+import org.application.shikiapp.shared.models.states.BaseDialogState
 import org.application.shikiapp.shared.models.states.NewsDetailState
 import org.application.shikiapp.shared.models.ui.NewsDetail
 import org.application.shikiapp.shared.models.ui.mappers.mapper
 import org.application.shikiapp.shared.network.client.Network
 import org.application.shikiapp.shared.network.response.Response
+import org.application.shikiapp.shared.utils.enums.CommentableType
 import org.application.shikiapp.shared.utils.navigation.Screen
 
 
@@ -27,7 +29,7 @@ class NewsDetailViewModel(saved: SavedStateHandle) : ContentDetailViewModel<News
 
             try {
                 val news = async { Network.topics.getTopic(contentId) }
-                setCommentParams(contentId)
+                setCommentParams(contentId, CommentableType.TOPIC)
 
                 emit(Response.Success(news.await().mapper(comments)))
             } catch (e: Exception) {
@@ -37,17 +39,12 @@ class NewsDetailViewModel(saved: SavedStateHandle) : ContentDetailViewModel<News
     }
 
     override fun onEvent(event: ContentDetailEvent) {
-        when (event) {
-            ContentDetailEvent.ShowComments -> updateState { it.copy(showComments = !it.showComments) }
+        super.onEvent(event)
 
-            is ContentDetailEvent.Media.ShowImage -> updateState {
-                it.copy(
-                    showImage = !it.showImage,
-                    image = event.index
-                )
+        if (event is ContentDetailEvent.ToggleDialog && event.dialogState is BaseDialogState.Media.Image) {
+            updateState {
+                it.copy(image = event.dialogState.index)
             }
-
-            else -> Unit
         }
     }
 }
