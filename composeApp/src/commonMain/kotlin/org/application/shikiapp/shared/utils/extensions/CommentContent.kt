f51322cef723ef4f83453a14f6a8e71c3e7335c3
pav <email@inbox.ru>
2026-04-01 14:13:10 +0300

    — Рефакторинг кода

--- composeApp/src/commonMain/kotlin/org/application/shikiapp/shared/utils/extensions/CommentContent.kt
+++ composeApp/src/commonMain/kotlin/org/application/shikiapp/shared/utils/extensions/CommentContent.kt
@@ -8,15 +8,14 @@ import shikiapp.composeapp.generated.resources.text_image
 import shikiapp.composeapp.generated.resources.text_quote
 import shikiapp.composeapp.generated.resources.text_spoiler_l
 
-fun List<CommentContent>.getLastMessage() =
-    when (val last = lastOrNull { it !is CommentContent.LineBreakContent }) {
-        is CommentContent.ImageContent -> ResourceText.StringResource(Res.string.text_image)
-        is CommentContent.QuoteContent -> ResourceText.StringResource(Res.string.text_quote)
-        is CommentContent.SpoilerContent -> ResourceText.StringResource(Res.string.text_spoiler_l)
-        is CommentContent.TextContent -> ResourceText.StaticString(last.text.text)
+fun List<CommentContent>.getLastMessage() = when (val last = lastOrNull()) {
+    is CommentContent.ImageContent -> ResourceText.StringResource(Res.string.text_image)
+    is CommentContent.QuoteContent -> ResourceText.StringResource(Res.string.text_quote)
+    is CommentContent.SpoilerContent -> ResourceText.StringResource(Res.string.text_spoiler_l)
+    is CommentContent.TextContent -> ResourceText.StaticString(last.text.text)
 
-        else -> ResourceText.StaticString(BLANK)
-    }
+    else -> ResourceText.StaticString(BLANK)
+}
 
 fun List<CommentContent>.flattenImages() = asSequence()
     .flatMap(CommentContent::deepFlatten)
