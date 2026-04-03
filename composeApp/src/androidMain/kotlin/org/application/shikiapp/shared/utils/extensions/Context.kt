fc18e7a56480444f9c9dc619795b6093914ec98e
pav <email@inbox.ru>
2026-04-03 15:15:40 +0300

    — Рефакторинг кода

--- composeApp/src/androidMain/kotlin/org/application/shikiapp/shared/utils/extensions/Context.kt
+++ composeApp/src/androidMain/kotlin/org/application/shikiapp/shared/utils/extensions/Context.kt
@@ -4,21 +4,48 @@ import android.content.Context
 import android.content.Intent
 import android.content.pm.verify.domain.DomainVerificationManager
 import android.content.pm.verify.domain.DomainVerificationUserState
+import android.net.Uri
 import android.os.Build
 import android.provider.Settings
 import android.widget.Toast
-import androidx.core.net.toUri
+import androidx.annotation.RequiresApi
+import kotlin.collections.emptyMap
 
-fun Context.appLinksSettings() = if (Build.VERSION.SDK_INT < Build.VERSION_CODES.S) Intent()
-else Intent(Settings.ACTION_APP_OPEN_BY_DEFAULT_SETTINGS, "package:${packageName}".toUri())
+fun Context.openAppLinksSettings() = if (Build.VERSION.SDK_INT < Build.VERSION_CODES.S) Unit else
+    try {
+        val intent = Intent(Settings.ACTION_APP_OPEN_BY_DEFAULT_SETTINGS).apply {
+            data = Uri.fromParts("package", packageName, null)
+            flags = Intent.FLAG_ACTIVITY_NEW_TASK
+        }
 
-fun Context.isDomainVerified(): Boolean = if (Build.VERSION.SDK_INT < Build.VERSION_CODES.S) true
-else {
-    val manager = getSystemService(DomainVerificationManager::class.java)
-    val userState = manager.getDomainVerificationUserState(packageName) ?: return false
+        startActivity(intent)
+    } catch (_: Exception) {
+        val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS).apply {
+            data = Uri.fromParts("package", packageName, null)
+            flags = Intent.FLAG_ACTIVITY_NEW_TASK
+        }
 
-    userState.isLinkHandlingAllowed && userState.hostToStateMap.all { it.value == DomainVerificationUserState.DOMAIN_STATE_SELECTED }
+        startActivity(intent)
+    }
+
+@get:RequiresApi(Build.VERSION_CODES.S)
+private val Context.domainVerificationState: DomainVerificationUserState?
+    get() = getSystemService(DomainVerificationManager::class.java)
+        ?.getDomainVerificationUserState(packageName)
+
+fun Context.isAllDomainsVerified(): Boolean {
+    if (Build.VERSION.SDK_INT < Build.VERSION_CODES.S) return true
+    val state = domainVerificationState ?: return false
+
+    return state.isLinkHandlingAllowed &&
+            state.hostToStateMap.values.all { it == DomainVerificationUserState.DOMAIN_STATE_SELECTED }
 }
+
+fun Context.getLinkDomains() = if (Build.VERSION.SDK_INT < Build.VERSION_CODES.S) emptyMap()
+else domainVerificationState?.hostToStateMap.orEmpty()
+
+fun Context.isLinkHandlingAllowed() = if (Build.VERSION.SDK_INT < Build.VERSION_CODES.S) true
+else domainVerificationState?.isLinkHandlingAllowed ?: false
 
 fun Context.showToast(text: String, length: Int = Toast.LENGTH_SHORT) =
     Toast.makeText(this, text, length).show()
