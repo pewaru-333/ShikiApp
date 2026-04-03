561779c22f9b06930933d30f50ff7b388fe10ef3
pav <email@inbox.ru>
2026-04-03 15:15:45 +0300

    — Рефакторинг кода

--- composeApp/src/commonMain/kotlin/org/application/shikiapp/shared/utils/extensions/Enum.kt
+++ composeApp/src/commonMain/kotlin/org/application/shikiapp/shared/utils/extensions/Enum.kt
@@ -1,7 +1,11 @@
 package org.application.shikiapp.shared.utils.extensions
 
-inline fun <reified E : Enum<E>> Enum.Companion.safeValueOf(value: String?) =
-    enumValues<E>().firstOrNull { it.name.equals(value, true) } ?: enumValues<E>().first()
+import kotlin.enums.enumEntries
 
-inline fun <reified E : Enum<E>> E.safeEquals(value: String?) = name.equals(value, true)
+inline fun <reified E : Enum<E>> Enum.Companion.safeValueOf(value: String?) =
+    enumEntries<E>().let { entries ->
+        entries.firstOrNull { it.name.equals(value, true) } ?: entries.first()
+    }
+
+fun Enum<*>.safeEquals(value: String?) = name.equals(value, ignoreCase = true)
 inline fun <reified E : Enum<E>> E.toValue() = name.lowercase().replaceFirstChar(Char::uppercaseChar)
