c160783c82fd85f50af930e0d42d3f02968c548f
pav <email@inbox.ru>
2026-04-03 15:16:16 +0300

    — Изменение логики получения полной ссылки

--- composeApp/src/commonMain/kotlin/org/application/shikiapp/shared/utils/extensions/Uri.kt
+++ composeApp/src/commonMain/kotlin/org/application/shikiapp/shared/utils/extensions/Uri.kt
@@ -1,16 +1,17 @@
 package org.application.shikiapp.shared.utils.extensions
 
-import io.ktor.http.URLBuilder
-import io.ktor.http.Url
 import org.application.shikiapp.shared.utils.BASE_URL
 import org.application.shikiapp.shared.utils.URL_MIRROR
 
-fun String.toFullUri(): Url {
-    val fullUri = if (contains(BASE_URL) || contains(URL_MIRROR)) {
+fun String.toFullUrl(): String {
+    val isAbsolute = startsWith("http", ignoreCase = true) ||
+            startsWith("//") ||
+            startsWith(BASE_URL) ||
+            startsWith(URL_MIRROR)
+
+    return if (isAbsolute) {
         this
     } else {
-        BASE_URL + this
+        "${BASE_URL.removeSuffix("/")}/${removePrefix("/")}"
     }
-
-    return URLBuilder(fullUri).build()
 }
