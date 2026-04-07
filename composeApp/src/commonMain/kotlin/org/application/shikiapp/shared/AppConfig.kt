2d4699e414e7c812f17e7250339da8b0b1c11e9a
pav <email@inbox.ru>
2026-04-07 16:23:15 +0300

    — Добавление дополнительных доменов в конфигурацию

--- composeApp/src/commonMain/kotlin/org/application/shikiapp/shared/AppConfig.kt
+++ composeApp/src/commonMain/kotlin/org/application/shikiapp/shared/AppConfig.kt
@@ -2,34 +2,34 @@ package org.application.shikiapp.shared
 
 data class AppConfig(
     val baseUrl: String,
-    val urlMirror: String,
+    val urlMirrors: List<String>,
     val userAgent: String,
     val clientId: String,
     val clientSecret: String,
     val redirectUri: String,
     val isShikimori: Boolean = userAgent == "ShikiApp",
-    val authScopes: List<String> = if (isShikimori) listOf("user_rates", /*"email"*/ "messages", "comments", "topics", /*"content"*/ "clubs", "friends" /*"ignores"*/)
-    else listOf("user_rates", /*"email"*/ "messages", "comments", "topics", /*"content"*/ "clubs", "friends", "ignores")
+    val authScopes: Set<String> = if (isShikimori) setOf("user_rates", /*"email"*/ "messages", "comments", "topics", /*"content"*/ "clubs", "friends" /*"ignores"*/)
+    else setOf("user_rates", /*"email"*/ "messages", "comments", "topics", /*"content"*/ "clubs", "friends", "ignores")
 ) {
     companion object {
         private val ShikiApp = AppConfig(
-            baseUrl = "https://shikimori.io",
-            urlMirror = "https://shiki.one",
+            baseUrl = "https://shikimori.one",
+            urlMirrors = listOf("https://shikimori.io", "https://shiki.one"),
             userAgent = "ShikiApp",
             clientId = "C0IlIBQYqt9VHjuoayfbBG9ulhBH9XWuTOxSX_6oE6g",
             clientSecret = "0U2MtkFgtGUP9_TFKBw1ORVy6S68KZDz_AdKsoMfnFM",
             redirectUri = "app://login"
         )
         private val DarkShiki = AppConfig(
-            baseUrl = "https://shikimori.fi",
-            urlMirror = "https://shikimori.rip",
+            baseUrl = "https://shikimori.rip",
+            urlMirrors = listOf("https://shikimori.fi", "https://shikimori.net"),
             userAgent = "DarkShiki",
             clientId = "d8W9rjFLuEZKx_dYXzJ42nGvsUckx4vfhMu5Liyr7MY",
             clientSecret = "Sog_CyJs19eCuFbIvg06Gb8zu8AMXZE8VI2CKLN1td4",
             redirectUri = "darkshiki://auth/login"
         )
 
-        fun createFlavourConfig(userAgent: String) = if (userAgent == "ShikiApp") ShikiApp
+        fun createFlavorConfig(userAgent: String) = if (userAgent == "ShikiApp") ShikiApp
         else DarkShiki
 
         fun createDesktopConfig() = ShikiApp.copy(
