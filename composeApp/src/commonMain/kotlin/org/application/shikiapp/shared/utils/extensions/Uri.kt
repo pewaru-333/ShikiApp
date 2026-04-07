b5a0c141b636b111d87fbaa094161d1a0b3b5ec5
pav <email@inbox.ru>
2026-04-07 16:24:45 +0300

    — Изменение логики получения полный ссылки

--- composeApp/src/commonMain/kotlin/org/application/shikiapp/shared/utils/extensions/Uri.kt
+++ composeApp/src/commonMain/kotlin/org/application/shikiapp/shared/utils/extensions/Uri.kt
@@ -1,17 +1,9 @@
 package org.application.shikiapp.shared.utils.extensions
 
-import org.application.shikiapp.shared.utils.BASE_URL
-import org.application.shikiapp.shared.utils.URL_MIRROR
+import org.application.shikiapp.shared.network.client.ApiRoutes
 
-fun String.toFullUrl(): String {
-    val isAbsolute = startsWith("http", ignoreCase = true) ||
-            startsWith("//") ||
-            startsWith(BASE_URL) ||
-            startsWith(URL_MIRROR)
-
-    return if (isAbsolute) {
-        this
-    } else {
-        "${BASE_URL.removeSuffix("/")}/${removePrefix("/")}"
-    }
+fun String.toFullUrl() = if (startsWith("http", ignoreCase = true) || startsWith("//")) {
+    this
+} else {
+    "${ApiRoutes.workingBaseUrl.removeSuffix("/")}/${removePrefix("/")}"
 }
