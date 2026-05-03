5b4890f0c1896ef2e3660d0c5c719901380e7396
pav <email@inbox.ru>
2026-05-03 14:31:31 +0300

    — Тип источника

---
+++ composeApp/src/commonMain/kotlin/org/application/shikiapp/shared/utils/enums/VideoSource.kt
@@ -0,0 +1,13 @@
+package org.application.shikiapp.shared.utils.enums
+
+import org.jetbrains.compose.resources.StringResource
+import shikiapp.composeapp.generated.resources.Res
+import shikiapp.composeapp.generated.resources.video_source_collapse
+import shikiapp.composeapp.generated.resources.video_source_kodik
+import shikiapp.composeapp.generated.resources.video_source_videohub
+
+enum class VideoSource(val title: StringResource) {
+    KODIK(Res.string.video_source_kodik),
+    COLLAPS(Res.string.video_source_collapse),
+    CVH(Res.string.video_source_videohub)
+}
