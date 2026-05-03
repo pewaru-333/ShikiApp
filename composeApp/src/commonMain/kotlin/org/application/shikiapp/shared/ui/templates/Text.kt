b0775211f54e6d248d626a1cf09f9444b6c9ca51
pav <email@inbox.ru>
2026-05-03 14:24:42 +0300

    — Шаблон текста для элементов плеера

--- composeApp/src/commonMain/kotlin/org/application/shikiapp/shared/ui/templates/Text.kt
+++ composeApp/src/commonMain/kotlin/org/application/shikiapp/shared/ui/templates/Text.kt
@@ -1,12 +1,14 @@
 package org.application.shikiapp.shared.ui.templates
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 
@@ -26,4 +28,15 @@ fun TextStickyHeader(text: String) = Text(
         .fillMaxWidth()
         .background(MaterialTheme.colorScheme.tertiaryContainer)
         .padding(16.dp, 8.dp)
+)
+
+@Composable
+fun TextLabelVideo(text: String, isSelected: Boolean, onClick: () -> Unit) = Text(
+    text = text,
+    color = if (isSelected) MaterialTheme.colorScheme.primary else Color.White,
+    fontWeight = if (isSelected) FontWeight.Bold else FontWeight.Normal,
+    modifier = Modifier
+        .fillMaxWidth()
+        .clickable(onClick = onClick)
+        .padding(16.dp, 12.dp)
 )
