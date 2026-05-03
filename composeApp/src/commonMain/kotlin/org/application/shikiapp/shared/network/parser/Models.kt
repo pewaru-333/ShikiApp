8f0c9743fb019124a01151120973bc0b330202c5
pav <email@inbox.ru>
2026-05-03 14:30:55 +0300

    — Изменение DTO

--- composeApp/src/commonMain/kotlin/org/application/shikiapp/shared/network/parser/Models.kt
+++ composeApp/src/commonMain/kotlin/org/application/shikiapp/shared/network/parser/Models.kt
@@ -2,6 +2,7 @@ package org.application.shikiapp.shared.network.parser
 
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
+import kotlinx.serialization.json.JsonObject
 
 @Serializable
 data class KodikTokensResponse(
@@ -23,8 +24,12 @@ data class KodikSearchResponse(
 @Serializable
 data class KodikResultItem(
     val id: String,
+    @SerialName("imdb_id")
+    val imdbId: String,
     val link: String,
     val translation: KodikTranslation,
+    @SerialName("last_season")
+    val lastSeason: Int? = null,
     @SerialName("episodes_count")
     val episodesCount: Int? = null,
     val type: String,
@@ -52,4 +57,85 @@ data class KodikTranslation(
     val id: Int,
     val title: String,
     val type: String
+)
+
+// ================================================================================================
+
+@Serializable
+data class CollapsTitleDetails(
+    @SerialName("iframe_url")
+    val iframeUrl: String? = null,
+    val poster: String? = null,
+    val quality: String? = null,
+    val voiceActing: List<String>? = null,
+    val subtitle: List<String>? = null,
+    val seasons: List<CollapsSeason>? = null
+)
+
+@Serializable
+data class CollapsSeason(
+    val season: Int,
+    val episodes: List<CollapsEpisode> = emptyList()
+)
+
+@Serializable
+data class CollapsEpisode(
+    val episode: Int,
+    @SerialName("iframe_url")
+    val iframeUrl: String? = null,
+    @SerialName("iframe_poster")
+    val iframePoster: String? = null,
+    val voiceActing: List<String>? = null,
+    val subtitle: List<String>? = null
+)
+
+@Serializable
+data class CollapsSeasonData(
+    val season: Int,
+    val episodes: List<CollapsEpisodeData> = emptyList()
+)
+
+@Serializable
+data class CollapsEpisodeData(
+    val episode: String? = null,
+    val hls: String? = null,
+    val dash: String? = null,
+    val dasha: String? = null,
+    val cc: List<JsonObject>? = null
+)
+
+// ================================================================================================
+
+@Serializable
+data class CvhPlaylistResponse(
+    val items: List<CvhPlaylistItem> = emptyList()
+)
+
+@Serializable
+data class CvhPlaylistItem(
+    val vkId: String,
+    val voiceStudio: String,
+    val voiceType: String? = null,
+    val season: Int = 1,
+    val episode: Int = 1
+)
+
+@Serializable
+data class CvhVideoResponse(
+    val sources: CvhSources = CvhSources()
+)
+
+@Serializable
+data class CvhSources(
+    val hlsUrl: String? = null,
+    val dashUrl: String? = null,
+    val mpegQhdUrl: String? = null,
+    val mpeg2kUrl: String? = null,
+    val mpeg4kUrl: String? = null,
+    val mpegHighUrl: String? = null,
+    val mpegFullHdUrl: String? = null,
+    val mpegMediumUrl: String? = null,
+    val mpegLowUrl: String? = null,
+    val mpegLowestUrl: String? = null,
+    val mpegTinyUrl: String? = null
 )
