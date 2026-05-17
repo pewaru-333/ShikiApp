59402714a12ef43cf22ac8faf716534760d5da17
pav <email@inbox.ru>
2026-05-17 15:21:14 +0300

    — Оптимизация импорта

--- composeApp/src/commonMain/kotlin/org/application/shikiapp/shared/screens/CalendarScreen.kt
+++ composeApp/src/commonMain/kotlin/org/application/shikiapp/shared/screens/CalendarScreen.kt
@@ -1,5 +1,3 @@
-@file:OptIn(ExperimentalMaterial3Api::class)
-
 package org.application.shikiapp.shared.screens
 
 import androidx.compose.foundation.layout.Arrangement
@@ -20,7 +18,6 @@ import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.PagerState
 import androidx.compose.foundation.pager.rememberPagerState
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.PrimaryTabRow
