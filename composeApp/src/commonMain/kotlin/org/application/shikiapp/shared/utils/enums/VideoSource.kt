ecc5eb910fc163930239f7658b8103813b5e516c
pav <email@inbox.ru>
2026-05-31 13:20:55 +0300

    — Новый источник

--- composeApp/src/commonMain/kotlin/org/application/shikiapp/shared/utils/enums/VideoSource.kt
+++ composeApp/src/commonMain/kotlin/org/application/shikiapp/shared/utils/enums/VideoSource.kt
@@ -2,6 +2,7 @@ package org.application.shikiapp.shared.utils.enums
 
 import org.jetbrains.compose.resources.StringResource
 import shikiapp.composeapp.generated.resources.Res
+import shikiapp.composeapp.generated.resources.video_source_animelib
 import shikiapp.composeapp.generated.resources.video_source_collapse
 import shikiapp.composeapp.generated.resources.video_source_kodik
 import shikiapp.composeapp.generated.resources.video_source_videohub
@@ -9,5 +10,6 @@ import shikiapp.composeapp.generated.resources.video_source_videohub
 enum class VideoSource(val title: StringResource) {
     KODIK(Res.string.video_source_kodik),
     COLLAPS(Res.string.video_source_collapse),
-    CVH(Res.string.video_source_videohub)
+    CVH(Res.string.video_source_videohub),
+    ANIMELIB(Res.string.video_source_animelib)
 }
