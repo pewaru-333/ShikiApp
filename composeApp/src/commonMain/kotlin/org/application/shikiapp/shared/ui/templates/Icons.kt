e9bbe08ae9df69b4e770042d4430b1c3a4fea738
pav <email@inbox.ru>
2026-05-31 13:34:31 +0300

    — Дизайн элементов плеера

--- composeApp/src/commonMain/kotlin/org/application/shikiapp/shared/ui/templates/Icons.kt
+++ composeApp/src/commonMain/kotlin/org/application/shikiapp/shared/ui/templates/Icons.kt
@@ -1,12 +1,16 @@
 package org.application.shikiapp.shared.ui.templates
 
-import androidx.compose.foundation.background
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.interaction.collectIsFocusedAsState
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.IconButtonDefaults
 import androidx.compose.material3.LocalContentColor
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import org.jetbrains.compose.resources.DrawableResource
@@ -16,18 +20,32 @@ import shikiapp.composeapp.generated.resources.vector_arrow_back
 import shikiapp.composeapp.generated.resources.vector_comments
 
 @Composable
-fun IconComment(onEvent: () -> Unit) =
-    IconButton(onEvent) { VectorIcon(Res.drawable.vector_comments) }
+fun IconComment(onClick: () -> Unit) =
+    IconButton(onClick) { VectorIcon(Res.drawable.vector_comments) }
 
 @Composable
 fun NavigationIcon(onClick: () -> Unit) =
     IconButton(onClick) { VectorIcon(Res.drawable.vector_arrow_back) }
 
 @Composable
-fun IconVideoControl(resId: DrawableResource, modifier: Modifier, modifierI: Modifier, onClick: () -> Unit) =
-    IconButton(onClick, modifier.background(Color.White.copy(alpha = 0.2f), CircleShape)) {
-        VectorIcon(resId, modifierI.fillMaxSize(), Color.White)
-    }
+fun IconVideoControl(resId: DrawableResource, modifier: Modifier, modifierI: Modifier, onClick: () -> Unit) {
+    val interactionSource = remember(::MutableInteractionSource)
+    val isFocused by interactionSource.collectIsFocusedAsState()
+
+    val containerColor = if (isFocused) MaterialTheme.colorScheme.primary
+    else Color.White.copy(alpha = 0.2f)
+
+    val contentColor = if (isFocused) MaterialTheme.colorScheme.onPrimary
+    else Color.Transparent
+
+    IconButton(
+        onClick = onClick,
+        modifier = modifier,
+        interactionSource = interactionSource,
+        colors = IconButtonDefaults.iconButtonColors(containerColor, contentColor),
+        content = { VectorIcon(resId, modifierI.fillMaxSize(), Color.White) }
+    )
+}
 
 @Composable
 fun VectorIcon(
