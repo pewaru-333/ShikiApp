bd0de6d458dd713808672307f9da9243fc8f1f85
pav <email@inbox.ru>
2026-05-31 13:32:30 +0300

    — Сравнение строк по возможности

--- composeApp/src/commonMain/kotlin/org/application/shikiapp/shared/utils/ResourceText.kt
+++ composeApp/src/commonMain/kotlin/org/application/shikiapp/shared/utils/ResourceText.kt
@@ -4,21 +4,35 @@ import androidx.compose.runtime.Composable
 import org.jetbrains.compose.resources.getPluralString
 import org.jetbrains.compose.resources.getString
 
-sealed interface ResourceText {
-    data class StaticString(val value: String) : ResourceText
+sealed interface ResourceText : Comparable<ResourceText> {
+    val defaultValue: String
 
-    data class MultiString(val value: List<Any>) : ResourceText
+    data class StaticString(val value: String) : ResourceText {
+        override fun compareTo(other: ResourceText) = value.compareTo(other.defaultValue)
+        override val defaultValue = value
+    }
+
+    data class MultiString(val value: List<Any>) : ResourceText {
+        override fun compareTo(other: ResourceText) = -1
+        override val defaultValue = BLANK
+    }
 
     class StringResource(
         val resourceId: org.jetbrains.compose.resources.StringResource,
         vararg val args: Any
-    ) : ResourceText
+    ) : ResourceText {
+        override fun compareTo(other: ResourceText) = -1
+        override val defaultValue = BLANK
+    }
 
     class PluralStringResource(
         val resourceId: org.jetbrains.compose.resources.PluralStringResource,
         val count: Int,
         vararg val args: Any
-    ) : ResourceText
+    ) : ResourceText {
+        override fun compareTo(other: ResourceText) = -1
+        override val defaultValue = BLANK
+    }
 
     @Composable
     fun asComposableString(): String = when (this) {
