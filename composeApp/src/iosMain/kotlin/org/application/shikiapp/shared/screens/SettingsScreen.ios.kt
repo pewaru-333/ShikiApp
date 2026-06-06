91ea850a83051a6917a5936a64bd6ef73b7b797e
pav <email@inbox.ru>
2026-06-06 11:18:31 +0300

    — Рефакторинг кода

--- composeApp/src/iosMain/kotlin/org/application/shikiapp/shared/screens/SettingsScreen.ios.kt
+++ composeApp/src/iosMain/kotlin/org/application/shikiapp/shared/screens/SettingsScreen.ios.kt
@@ -3,9 +3,7 @@ package org.application.shikiapp.shared.screens
 import androidx.compose.foundation.lazy.LazyListScope
 import androidx.compose.runtime.Composable
 
-actual fun LazyListScope.deeplinkSetting(onClick: () -> Unit) {
-}
+actual fun LazyListScope.deeplinkSetting(onClick: () -> Unit) = Unit
 
 @Composable
-actual fun DeeplinkScreen(isVisible: Boolean, onBack: () -> Unit) {
-}
+actual fun DeeplinkScreen(isVisible: Boolean, onBack: () -> Unit) = Unit
