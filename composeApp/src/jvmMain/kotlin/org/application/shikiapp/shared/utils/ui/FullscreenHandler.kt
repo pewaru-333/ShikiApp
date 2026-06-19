163f4c6268d3324bd37567db942dbc8ecb125b3d
pav <email@inbox.ru>
2026-06-19 10:29:08 +0300

    — Изменение управления полноэкранным режимом на ПК

--- composeApp/src/jvmMain/kotlin/org/application/shikiapp/shared/utils/ui/FullscreenHandler.kt
+++ composeApp/src/jvmMain/kotlin/org/application/shikiapp/shared/utils/ui/FullscreenHandler.kt
@@ -1,13 +1,28 @@
 package org.application.shikiapp.shared.utils.ui
 
 import androidx.compose.runtime.staticCompositionLocalOf
+import androidx.compose.ui.window.WindowPlacement
+import androidx.compose.ui.window.WindowState
 import org.application.shikiapp.shared.utils.BLANK
 
 val LocalFullscreenHandler = staticCompositionLocalOf<FullscreenHandler> {
     error(BLANK)
 }
 
-data class FullscreenHandler(
-    val isFullscreen: Boolean,
-    val toggle: () -> Unit
-)
+class FullscreenHandler(private val windowState: WindowState) {
+    private var size = windowState.size
+    private var position = windowState.position
+
+    val isFullscreen: Boolean
+        get() = windowState.placement == WindowPlacement.Fullscreen
+
+    fun toggle() = if (isFullscreen) {
+        windowState.placement = WindowPlacement.Floating
+        windowState.position = position
+        windowState.size = size
+    } else {
+        size = windowState.size
+        position = windowState.position
+        windowState.placement = WindowPlacement.Fullscreen
+    }
+}
