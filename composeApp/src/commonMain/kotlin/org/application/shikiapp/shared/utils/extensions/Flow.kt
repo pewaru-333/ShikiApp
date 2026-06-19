fae28af0f296c4f82be771938fe7804ce5566362
pav <email@inbox.ru>
2026-06-19 10:18:41 +0300

    — Оптимизация функций расширений для Flow

--- composeApp/src/commonMain/kotlin/org/application/shikiapp/shared/utils/extensions/Flow.kt
+++ composeApp/src/commonMain/kotlin/org/application/shikiapp/shared/utils/extensions/Flow.kt
@@ -1,15 +1,11 @@
 package org.application.shikiapp.shared.utils.extensions
 
-import androidx.paging.PagingData
-import androidx.paging.filter
 import com.apollographql.apollo.api.ApolloResponse
 import com.apollographql.apollo.api.Operation
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.filterNotNull
 import kotlinx.coroutines.flow.flow
-import kotlinx.coroutines.flow.map
-import kotlinx.coroutines.flow.mapLatest
+import kotlinx.coroutines.flow.mapNotNull
 
 fun <T> Flow<T>.pairwise(): Flow<Pair<T, T>> = flow {
     var previous: T? = null
@@ -19,22 +15,9 @@ fun <T> Flow<T>.pairwise(): Flow<Pair<T, T>> = flow {
     }
 }
 
-fun <T : Any, K> Flow<PagingData<T>>.distinctBy(selector: (T) -> K): Flow<PagingData<T>> {
-    val set = hashSetOf<K>()
-
-    return map { pagingData ->
-        pagingData.filter { item ->
-            set.add(selector(item))
-        }
-    }
-}
-
 @OptIn(ExperimentalCoroutinesApi::class)
-fun <T : Operation.Data, D> Flow<ApolloResponse<T>>.mapToResult(transform: (T) -> D) =
-    mapLatest { response ->
-        when {
-            response.hasErrors() -> null
-            response.data == null -> null
-            else -> transform(response.data as T)
-        }
-    }.filterNotNull()
+fun <T : Operation.Data, D> Flow<ApolloResponse<T>>.mapToResult(transform: (T) -> D): Flow<D> =
+    mapNotNull { response ->
+        val data = response.data
+        if (response.hasErrors() || data == null) null else transform(data)
+    }
