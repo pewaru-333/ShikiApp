e723e017b5aa1c636fcc09a3e769fb8df0e29601
pav <email@inbox.ru>
2026-07-29 13:31:42 +0300

    — Новые функции расширения

--- composeApp/src/commonMain/kotlin/org/application/shikiapp/shared/utils/extensions/String.kt
+++ composeApp/src/commonMain/kotlin/org/application/shikiapp/shared/utils/extensions/String.kt
@@ -2,6 +2,10 @@ package org.application.shikiapp.shared.utils.extensions
 
 import androidx.compose.ui.platform.ClipEntry
 
+fun String?.toIntOrDefault(defaultValue: Int = 0) = this?.toIntOrNull() ?: defaultValue
+
+fun String?.toDefaultValue(defaultValue: String = "0") = this ?: defaultValue
+
 expect fun String.getLocaleLocalizedName(): String
 
 expect fun String.toClipEntry(): ClipEntry
