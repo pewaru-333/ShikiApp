551387edcebd1337f04b84c46b0d82c862f6c05e
pav <email@inbox.ru>
2026-08-24 12:41:16 +0300

    — Исправление функций расширения

--- composeApp/src/androidMain/kotlin/org/application/shikiapp/shared/utils/extensions/Context.kt
+++ composeApp/src/androidMain/kotlin/org/application/shikiapp/shared/utils/extensions/Context.kt
@@ -10,11 +10,16 @@ import android.os.Build
 import android.provider.Settings
 import android.widget.Toast
 import androidx.annotation.RequiresApi
-import kotlinx.coroutines.runBlocking
+import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.MainScope
+import kotlinx.coroutines.launch
+import org.jetbrains.compose.resources.getString
 import shikiapp.composeapp.generated.resources.Res
 import shikiapp.composeapp.generated.resources.text_error
 
-fun Context.openAppLinksSettings() = if (Build.VERSION.SDK_INT < Build.VERSION_CODES.S) Unit else
+@RequiresApi(Build.VERSION_CODES.S)
+fun Context.openAppLinksSettings() {
     try {
         val intent = Intent(Settings.ACTION_APP_OPEN_BY_DEFAULT_SETTINGS).apply {
             data = Uri.fromParts("package", packageName, null)
@@ -31,29 +36,34 @@ fun Context.openAppLinksSettings() = if (Build.VERSION.SDK_INT < Build.VERSION_C
         try {
             startActivity(intent)
         } catch (_: ActivityNotFoundException) {
-            val text = runBlocking { org.jetbrains.compose.resources.getString(Res.string.text_error) }
-            showToast(text)
+            asyncScope.launch {
+                showToast(getString(Res.string.text_error))
+            }
         }
     }
+}
 
 @get:RequiresApi(Build.VERSION_CODES.S)
 private val Context.domainVerificationState: DomainVerificationUserState?
     get() = getSystemService(DomainVerificationManager::class.java)
         ?.getDomainVerificationUserState(packageName)
 
+@RequiresApi(Build.VERSION_CODES.S)
 fun Context.isAllDomainsVerified(): Boolean {
-    if (Build.VERSION.SDK_INT < Build.VERSION_CODES.S) return true
     val state = domainVerificationState ?: return false
 
     return state.isLinkHandlingAllowed &&
             state.hostToStateMap.values.all { it == DomainVerificationUserState.DOMAIN_STATE_SELECTED }
 }
 
-fun Context.getLinkDomains() = if (Build.VERSION.SDK_INT < Build.VERSION_CODES.S) emptyMap()
-else domainVerificationState?.hostToStateMap.orEmpty()
+@RequiresApi(Build.VERSION_CODES.S)
+fun Context.getLinkDomains() = domainVerificationState?.hostToStateMap.orEmpty()
 
-fun Context.isLinkHandlingAllowed() = Build.VERSION.SDK_INT < Build.VERSION_CODES.S ||
-        domainVerificationState?.isLinkHandlingAllowed ?: false
+@RequiresApi(Build.VERSION_CODES.S)
+fun Context.isLinkHandlingAllowed() = domainVerificationState?.isLinkHandlingAllowed ?: false
 
 fun Context.showToast(text: String, length: Int = Toast.LENGTH_SHORT) =
     Toast.makeText(this, text, length).show()
+
+inline val Context.asyncScope
+    get() = (this as? LifecycleOwner)?.lifecycleScope ?: MainScope()
